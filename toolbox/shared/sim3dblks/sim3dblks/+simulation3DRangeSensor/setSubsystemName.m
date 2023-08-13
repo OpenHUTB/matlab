bf3792b00f6b1c374d6174251a9454dca2453223
王海东 <wanghaidong@hutb.edu.cn>
2023-08-13 18:27:29 +0800

    更新 setSubsystemName.m

--- toolbox/shared/sim3dblks/sim3dblks/+simulation3DRangeSensor/setSubsystemName.m
+++ toolbox/shared/sim3dblks/sim3dblks/+simulation3DRangeSensor/setSubsystemName.m
@@ -1,9 +1,4 @@
 function setSubsystemName(block)
-
-
-
-
-
     blocks=Simulink.findBlocksOfType(block,'MATLABSystem');
     for i=1:length(blocks)
         if strcmp(get_param(blocks(i),"Name"),"Simulation 3D Range Sensor")
