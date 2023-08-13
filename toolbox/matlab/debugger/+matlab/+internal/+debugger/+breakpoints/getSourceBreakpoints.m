3d762e2de22011f93d67328dac2946184073802e
王海东 <wanghaidong@hutb.edu.cn>
2023-08-13 19:45:06 +0800

    更新 getSourceBreakpoints.m

--- toolbox/matlab/debugger/+matlab/+internal/+debugger/+breakpoints/getSourceBreakpoints.m
+++ toolbox/matlab/debugger/+matlab/+internal/+debugger/+breakpoints/getSourceBreakpoints.m
@@ -1,13 +1,4 @@
 function breakpointInfos=getSourceBreakpoints()
-
-
-
-
-
-
-
-
-
     dbstatusReturn=dbstatus('-completenames');
     breakpointInfos=matlab.internal.debugger.breakpoints.convertDBStatusToBreakpointInfo(dbstatusReturn);
 end
