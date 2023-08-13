fe4a7bfd24b7aaed352be47edde73152abe7680b
王海东 <wanghaidong@hutb.edu.cn>
2023-08-13 17:06:42 +0800

    更新 VehicleToWorld.m

--- toolbox/driving/drivingutilities/+slexportprevious/+preprocess/VehicleToWorld.m
+++ toolbox/driving/drivingutilities/+slexportprevious/+preprocess/VehicleToWorld.m
@@ -1,11 +1,6 @@
 function VehicleToWorld(obj)
 
-
-
-
     if isR2019bOrEarlier(obj.ver)
-
-
         blks=findBlocksWithMaskType(obj,'driving.scenario.internal.VehicleToWorld');
         obj.replaceWithEmptySubsystem(blks);
     end
