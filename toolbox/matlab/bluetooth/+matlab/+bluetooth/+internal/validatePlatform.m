8a40479c76d3dec9cec237f3617fc491ec4abbf3
王海东 <wanghaidong@hutb.edu.cn>
2023-08-13 21:35:19 +0800

    更新 validatePlatform.m

--- toolbox/matlab/bluetooth/+matlab/+bluetooth/+internal/validatePlatform.m
+++ toolbox/matlab/bluetooth/+matlab/+bluetooth/+internal/validatePlatform.m
@@ -1,20 +1,13 @@
 function validatePlatform
 
-
-
-
     if~(ispc||ismac)
         id="MATLAB:bluetooth:common:unsupportedPlatform";
         throwAsCaller(MException(id,getString(message(id))));
     end
 
-
     if ispc
         [status,result]=system("ver");
         if status==0
-
-
-
 
             version=regexp(result,"(?<=.*Version )\d*(?=\..*)","match");
             if~isempty(version)&&(str2double(version)<10)
