70d6fc8f17782941a28cc2de3ab5a6c336b087ff
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 11:04:38 +0800

    更新 Engine.m

--- toolbox/shared/sim3d/sim3d/+sim3d/+engine/Engine.m
+++ toolbox/shared/sim3d/sim3d/+sim3d/+engine/Engine.m
@@ -15,55 +15,78 @@ end
 properties ( Access = private, Hidden = true )
 engineInterface;
 engineFactory;
-end 
+end
+
 
 methods ( Access = private )
-function self = Engine(  )
-self.engineFactory = sim3d.engine.EngineFactory(  );
-self.engineInterface = self.engineFactory.createEngine(  );
-self.engineInterface.start(  );
+function self = Engine()
+self.engineFactory = sim3d.engine.EngineFactory();
+self.engineInterface = self.engineFactory.createEngine();
+self.engineInterface.start();
 end 
-end 
-methods 
-function delete( self )
+end
+
+
+methods
+
+function delete(self)
 if ~isempty( self.engineInterface )
-self.engineInterface.stop(  );
+self.engineInterface.stop();
 end 
-end 
-end 
-methods ( Static = true, Hidden = true )
-function startSimulation( command )
+end
+
+end
+
+
+methods (Static = true, Hidden = true)
+
+function startSimulation(command)
 if ~sim3d.engine.Engine.isPlatformEnabled(  )
 error( message( 'shared_sim3dblks:sim3dblkConfig:blkPrmError_UnsupportedPlatform' ) );
 end 
 sim3d.engine.Engine.engine.engineInterface.startSimulation( command );
-end 
-function setSampleTime( sampleTime )
-SetSimulation3DInterfaceSampleTime( sampleTime );
-end 
-function sampleTime = getSampleTime(  )
-sampleTime = GetSimulation3DInterfaceSampleTime(  );
-end 
+end
+
+
+function setSampleTime(sampleTime)
+SetSimulation3DInterfaceSampleTime(sampleTime);
+end
+
+
+function sampleTime = getSampleTime()
+sampleTime = GetSimulation3DInterfaceSampleTime();
+end
+
 end 
 
-methods ( Static = true, Access = public, Hidden = true )
+
+methods (Static = true, Access = public, Hidden = true)
+
 function ready = isReady( readyTimeout_sec )
 
 ready = sim3d.engine.Engine.engine.engineInterface.isReady( readyTimeout_sec );
-end 
-function stop(  )
+end
 
-sim3d.engine.Engine.engine.engineInterface.stop(  );
-end 
-function start(  )
 
-sim3d.engine.Engine.engine.engineInterface.start(  );
-end 
-function restart(  )
+function stop()
 
-sim3d.engine.Engine.engine.engineInterface.restart(  );
-end 
-function stateOut = getSetState( stateIn, operation )
+sim3d.engine.Engine.engine.engineInterface.stop();
+end
+
+
+function start()
+
+sim3d.engine.Engine.engine.engineInterface.start();
+end
+
+
+function restart()
+
+sim3d.engine.Engine.engine.engineInterface.restart();
+end
+
+
+function stateOut = getSetState(stateIn, operation)
 persistent engineState
 if isempty( engineState )
 engineState = sim3d.engine.EngineCommands.STOP;
@@ -75,16 +98,23 @@ case 'set'
 engineState = stateIn;
 stateOut = [  ];
 end 
+end
+
+
+function state = getState()
+state = sim3d.engine.Engine.getSetState( [], 'get' );
 end 
-function state = getState(  )
-state = sim3d.engine.Engine.getSetState( [  ], 'get' );
-end 
-function setState( state )
+
+
+function setState(state)
 sim3d.engine.Engine.getSetState( state, 'set' );
 end 
-end 
-methods ( Static = true, Access = public, Hidden = true )
-function value = isPlatformEnabled(  )
+end
+
+
+methods (Static = true, Access = public, Hidden = true)
+
+function value = isPlatformEnabled()
 platform = computer( 'arch' );
 if ispref( 'Simulation3D', platform )
 value = getpref( 'Simulation3D', platform );
@@ -99,62 +129,82 @@ case 'maci64'
 value = false;
 end 
 end 
-end 
-function enabledPlatform(  )
+end
+
+
+function enabledPlatform()
 platform = computer( 'arch' );
 setpref( 'Simulation3D', platform, true );
-end 
-function disablePlatform(  )
+end
+
+
+function disablePlatform()
 platform = computer( 'arch' );
 setpref( 'Simulation3D', platform, false );
-end 
-function debug = getDebugLevel(  )
+end
+
+
+function debug = getDebugLevel()
 if ispref( 'Simulation3D', 'debug' )
 debug = uint32( getpref( 'Simulation3D', 'debug' ) );
 else 
 debug = uint32( 0 );
 end 
-end 
-function setDebugLevel( debug )
+end
+
+
+function setDebugLevel(debug)
 R36
 debug( 1, 1 )uint32
 end 
 setpref( 'Simulation3D', 'debug', debug );
-end 
-function warmUpSteps = getWarmUpSteps(  )
+end
+
+
+function warmUpSteps = getWarmUpSteps()
 if ispref( 'Simulation3D', 'warmUpSteps' )
 warmUpSteps = uint32( getpref( 'Simulation3D', 'warmUpSteps' ) );
 else 
 warmUpSteps = sim3d.engine.Engine.NumberOfWarmUpSteps;
 end 
-end 
-function setWarmUpSteps( warmUpSteps )
+end
+
+
+function setWarmUpSteps(warmUpSteps)
 R36
 warmUpSteps( 1, 1 )uint32
 end 
 setpref( 'Simulation3D', 'warmUpSteps', warmUpSteps );
-end 
-function httpPort = getHttpPort(  )
+end
+
+
+function httpPort = getHttpPort()
 if ispref( 'Simulation3D', 'httpPort' )
 httpPort = getpref( 'Simulation3D', 'httpPort' );
 else 
 httpPort = sim3d.engine.Engine.DefHttp;
 end 
-end 
-function setHttpPort( httpPort )
+end
+
+
+function setHttpPort(httpPort)
 R36
 httpPort
 end 
 setpref( 'Simulation3D', 'httpPort', httpPort );
-end 
-function streamerPort = getStreamerPort(  )
+end
+
+
+function streamerPort = getStreamerPort()
 if ispref( 'Simulation3D', 'streamerPort' )
 streamerPort = getpref( 'Simulation3D', 'streamerPort' );
 else 
 streamerPort = sim3d.engine.Engine.DefStreamer;
 end 
-end 
-function setStreamerPort( streamerPort )
+end
+
+
+function setStreamerPort(streamerPort)
 R36
 streamerPort
 end 
@@ -165,6 +215,4 @@ end
 end 
 
 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpKAa7KC.p.
-% Please follow local copyright laws when handling this file.
 
