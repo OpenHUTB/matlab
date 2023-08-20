a3a2e49c87326a1affa974616407d1d54385dea7
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 14:30:59 +0800

    更新 AbstractCameraSensor.m

--- toolbox/shared/sim3d/sim3d/+sim3d/+sensors/AbstractCameraSensor.m
+++ toolbox/shared/sim3d/sim3d/+sim3d/+sensors/AbstractCameraSensor.m
@@ -1,20 +1,16 @@
 classdef(Hidden)AbstractCameraSensor<sim3d.sensors.Sensor
 
     properties(Access=protected)
-
         HorizontalResolution(1,1)uint32=1920;
-
-
         VerticalResolution(1,1)uint32=1080;
-
-
         HorizohorizontalFieldOfView(1,1)single=90;
     end
 
-    properties(Access=public,Hidden=true)
 
+    properties(Access=public,Hidden=true)
         IsEnabled(1,1)logical=true;
     end
+
 
     properties(Access=protected)
         Reader=[];
@@ -22,7 +18,9 @@ classdef(Hidden)AbstractCameraSensor<sim3d.sensors.Sensor
         CameraConfigurationParameters=[];
     end
 
+
     methods
+
         function self=AbstractCameraSensor(sensorID,vehicleID,...
             horizontalResolution,verticalResolution,horizontalFieldOfView,transform)
             sensorName=sim3d.sensors.Sensor.getSensorName('Camera',sensorID);
@@ -33,9 +31,9 @@ classdef(Hidden)AbstractCameraSensor<sim3d.sensors.Sensor
 
         end
 
+
         function setup(self)
             setup@sim3d.sensors.Sensor(self);
-
             self.Reader=sim3d.sensors.CameraReader(self.getTag(),...
             self.getHorizontalResolution(),...
             self.getVerticalResolution());
@@ -45,9 +43,8 @@ classdef(Hidden)AbstractCameraSensor<sim3d.sensors.Sensor
             self.getHorizontalFieldOfView());
         end
 
+
         function reset(self)
-
-
             reset@sim3d.sensors.Sensor(self);
             self.CameraConfigurationParameters=self.createCameraConfigurationParameters();
             if~isempty(self.ConfigurationWriter)
@@ -55,9 +52,8 @@ classdef(Hidden)AbstractCameraSensor<sim3d.sensors.Sensor
             end
         end
 
+
         function image=read(self)
-
-
             if~isempty(self.Reader)
                 image=self.Reader.read();
             else
@@ -65,21 +61,21 @@ classdef(Hidden)AbstractCameraSensor<sim3d.sensors.Sensor
             end
         end
 
+
         function delete(self)
             if~isempty(self.Reader)
                 self.Reader.delete();
                 self.Reader=[];
             end
-
             if~isempty(self.ConfigurationWriter)
                 self.ConfigurationWriter.delete();
                 self.ConfigurationWriter=[];
             end
-
             delete@sim3d.sensors.Sensor(self);
         end
 
     end
+
 
     methods(Static)
         function tagName=getTagName()
@@ -87,37 +83,46 @@ classdef(Hidden)AbstractCameraSensor<sim3d.sensors.Sensor
         end
     end
 
+
     methods(Access=public,Hidden=true)
 
         function actorType=getActorType(~)
             actorType=int32(sim3d.utils.ActorTypes.IdealCamera);
         end
     end
+
+
     methods(Access=protected,Hidden=true)
         function horizontalFieldOfView=getHorizontalFieldOfView(self)
             horizontalFieldOfView=self.HorizohorizontalFieldOfView;
         end
 
+
         function verticalFieldOfView=getVerticalFieldOfView(self)
             verticalFieldOfView=single(self.VerticalResolution)*self.HorizohorizontalFieldOfView/single(self.HorizontalResolution);
         end
+
 
         function horizontalResolution=getHorizontalResolution(self)
             horizontalResolution=self.HorizontalResolution;
         end
 
+
         function verticalResolution=getVerticalResolution(self)
             verticalResolution=self.VerticalResolution;
         end
+
 
         function focalLength=getFocalLength(self)
             focalLength=double(self.HorizontalResolution)/(2*tan(pi*double(self.HorizohorizontalFieldOfView/360)));
             focalLength=uint32([focalLength,focalLength]);
         end
 
+
         function opticalCenter=getOpticalCenter(self)
             opticalCenter=uint32([self.HorizontalResolution/2,self.VerticalResolution/2]);
         end
+
 
         function skew=getSkew(~)
             skew=single(0);
