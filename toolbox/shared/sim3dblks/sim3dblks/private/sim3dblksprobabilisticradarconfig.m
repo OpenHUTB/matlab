524ba7184bdedb95cc769038daa20f7ab55bad49
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 15:41:18 +0800

    更新 sim3dblksprobabilisticradarconfig.m

--- toolbox/shared/sim3dblks/sim3dblks/private/sim3dblksprobabilisticradarconfig.m
+++ toolbox/shared/sim3dblks/sim3dblks/private/sim3dblksprobabilisticradarconfig.m
@@ -13,15 +13,14 @@ function[varargout]=sim3dblksprobabilisticradarconfig(varargin)
     end
 end
 
+
 function Initialization(~)
 end
 
-function IconInfo=DrawCommands(Block)
 
+function IconInfo=DrawCommands(Block)
     AliasNames={};
     IconInfo=autoblksgetportlabels(Block,AliasNames);
-
-
     IconInfo.ImageName='sim3dprobradar_config.png';
     [IconInfo.image,IconInfo.position]=iconImageUpdate(IconInfo.ImageName,1,20,40,'white');
 end
