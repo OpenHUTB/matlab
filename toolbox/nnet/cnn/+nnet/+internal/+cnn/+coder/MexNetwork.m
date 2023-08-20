ff5304f67566244c6a56a67f7b9b55e6ca15403e
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 22:52:06 +0800

    更新 MexNetwork.m

--- toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/MexNetwork.m
+++ toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/MexNetwork.m
@@ -1,128 +1,77 @@
 classdef MexNetwork
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     properties(SetAccess=private)
 
         MexFunctionName char
     end
+
 
     properties(SetAccess=private)
 
 Config
     end
 
+
     properties(Access=private)
 
         RootGenerationDirectory char
 
-
         MexGenerationDirectory char
-
 
         DesignFunction char
 
-
-
         NetworkFilename char
 
-
         Checksum=[]
-
-
 
 CustomLayerFiles
     end
 
+
     properties(Constant,Access=private)
-
-
         NetworkMatFileName='dl_net.mat';
     end
 
+
     properties(Constant)
-
-
         DebugGPUCoderOutput(1,1)logical=false
     end
 
+
     methods
         function this=MexNetwork(network,rootGenerationDirectory,mexNetworkConfig)
-
-
-
-
             this.RootGenerationDirectory=rootGenerationDirectory;
             this.Config=mexNetworkConfig;
-
-
-
-
-
             this.CustomLayerFiles=nnet.internal.cnn.coder.validateCustomLayersAndFindDependencies(network);
-
-
             mexNetworkConfig.mustBeSupportedNetwork(network);
-
-
             this=generateMexNames(this);
-
-
             this=this.generateMEX(network);
         end
 
+
         function out=predict(this,data)
-
-
-
-
             inputArgs=[{this.NetworkFilename},this.Config.ConstantInputs,data];
-
-
             out=cell(1,this.Config.NumOutputs);
-
-
             try
                 [out{:}]=feval(this.MexFunctionName,inputArgs{:});
             catch me
-
-
-
                 clear(this.MexFunctionName);
-
-
                 e=MException(message('nnet_cnn:dlAccel:MEXCallFailed'));
                 e=addCause(e,me);
                 throw(e)
             end
         end
 
+
         function tf=isValid(this)
-
-
             [chksum,mexExist]=this.computeChecksum();
             tf=mexExist&&isequal(this.Checksum,chksum);
         end
 
+
         function removeGeneratedFiles(this)
 
-
-
-
             clear(this.MexFunctionName)
-
-
             mexFile=fullfile(this.RootGenerationDirectory,[this.MexFunctionName,'.',mexext]);
             if exist(mexFile,'file')
                 delete(mexFile);
@@ -133,71 +82,38 @@ CustomLayerFiles
 
 
     methods(Access=private)
+
         function this=generateMexNames(this)
-
-
-
-
             uniqueStr=strrep(tempname,tempdir,'');
-
-
-
             [designFileName,designFilePath]=this.Config.getDesignFileInfo();
-
             this.MexFunctionName=[uniqueStr,'_',designFileName,'_mex'];
             this.DesignFunction=fullfile(designFilePath,designFileName);
-
             this.MexGenerationDirectory=fullfile(this.RootGenerationDirectory,this.MexFunctionName);
             this.NetworkFilename=fullfile(this.RootGenerationDirectory,this.NetworkMatFileName);
         end
 
+
         function this=generateMEX(this,network)
-
-
-
-
             fprintf('%s \n',string(message('nnet_cnn:dlAccel:GeneratingMEX',lower(this.Config.TargetLib))));
-
-
             iValidateMexSetup(false);
-
-
             save(this.NetworkFilename,'network');
-
-
             this.callCodegen();
-
-
             this.Checksum=this.computeChecksum();
         end
 
+
         function callCodegen(this)
-
-
-
-
             codegenArgs=this.Config.getCodegenArguments(this.NetworkFilename,this.MexGenerationDirectory);%#ok<NASGU>
-
-
             [~,coderReport]=evalc('dlcoder_base.internal.generateDlAccelPlugin(this.DesignFunction, codegenArgs{:});');
 
             if this.DebugGPUCoderOutput
                 iDisplayGpuCoderOutput(coderReport)
             end
-
-
             iDiagnosticError(coderReport);
-
-
             cleanGenerationDirectoryAfterCodegen(this);
         end
 
         function[chksum,mexExist]=computeChecksum(this)
-
-
-
-
-
             mexFile=fullfile(this.RootGenerationDirectory,[this.MexFunctionName,'.',mexext]);
             if exist(mexFile,'file')
                 mexInfo=dir(mexFile);
@@ -207,38 +123,25 @@ CustomLayerFiles
                 mexStamp=[];
                 mexExist=false;
             end
-
-
             dataFiles=dir(fullfile(this.MexGenerationDirectory,'cnn*'));
             dataStamps=ones(numel(dataFiles),1);
             for k=1:length(dataStamps)
                 dataStamps(k)=dataFiles(k).datenum;
             end
-
-
             numCustomLayerFiles=numel(this.CustomLayerFiles);
             customLayerStamps=ones(numCustomLayerFiles,1);
             for k=1:customLayerStamps
                 fileInfo=dir(this.CustomLayerFiles{k});
                 customLayerStamps(k)=fileInfo.datenum;
             end
-
-
             chksum=[mexStamp;dataStamps;customLayerStamps];
         end
 
+
         function cleanGenerationDirectoryAfterCodegen(this)
-
-
-
-
-
             if exist(this.NetworkFilename,'file')
                 delete(this.NetworkFilename);
             end
-
-
-
             [~]=rmdir(fullfile(this.MexGenerationDirectory,'build'),'s');
             [~]=rmdir(fullfile(this.MexGenerationDirectory,'interface'),'s');
             [~]=rmdir(fullfile(this.MexGenerationDirectory,'html'),'s');
@@ -251,21 +154,14 @@ CustomLayerFiles
     end
 end
 
+
 function iValidateMexSetup(resetCheck)
-
-
-
-
-
 
     persistent checkStatus;
 
     if isempty(checkStatus)||~(checkStatus.gpu&&checkStatus.deepcodegen&&checkStatus.deepcodeexec)||resetCheck
-
         [checkStatus,errorList]=coder.checkDlAccel;
     end
-
-
     if~(checkStatus.gpu&&checkStatus.deepcodegen&&checkStatus.deepcodeexec)
         if~isempty(errorList)
             error(message('nnet_cnn:dlAccel:InvalidMEXSetup',char(join(string(errorList),newline))));
@@ -275,31 +171,21 @@ function iValidateMexSetup(resetCheck)
     end
 end
 
+
 function iDiagnosticError(errorStruct)
-
-
     if isfield(errorStruct,'summary')
         if~errorStruct.summary.passed
-
-
-
-
             error(message('nnet_cnn:dlAccel:MEXCompilationFailed'));
         end
     else
 
-
         iValidateMexSetup(true);
-
-
         error(message('nnet_cnn:dlAccel:MEXCompilationFailed'));
     end
 end
 
+
 function iDisplayGpuCoderOutput(coderReport)
-
-
-
     e=MException(message('nnet_cnn:dlAccel:MEXCompilationFailed'));
     if isfield(coderReport,'summary')&&~coderReport.summary.passed
         numMsg=numel(coderReport.summary.messageList);
@@ -318,8 +204,8 @@ function iDisplayGpuCoderOutput(coderReport)
     end
 end
 
-function iDeleteIgnoringError(filePath)
 
+function iDeleteIgnoringError(filePath)
     try
         delete(filePath);
     catch
