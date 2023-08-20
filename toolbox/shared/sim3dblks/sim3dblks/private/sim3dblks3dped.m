2468d7e828d0fbd69808f92a98ee3981a123230a
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 15:02:25 +0800

    更新 sim3dblks3dped.m

--- toolbox/shared/sim3dblks/sim3dblks/private/sim3dblks3dped.m
+++ toolbox/shared/sim3dblks/sim3dblks/private/sim3dblks3dped.m
@@ -13,31 +13,22 @@ function[varargout]=sim3dblks3dped(varargin)
     end
 end
 
-function IconInfo=DrawCommands(Block)
 
+function IconInfo=DrawCommands(Block)
     AliasNames={'X','X';'Y','Y';'Yaw','Yaw'};
     IconInfo=autoblksgetportlabels(Block,AliasNames);
-
     IconInfo.ImageName='sim3dpedestrian.png';
     [~,IconInfo.position]=iconImageUpdate(IconInfo.ImageName,1);
     IconInfo.position=IconInfo.position+[20,0,0,0];
     IconInfo.image='sim3dpedestrian.svg';
 end
 
+
 function Initialization(Block)
-
-
-
-
-
     sim3d.utils.SimPool.addActorTag(Block);
-
-
     ParamList={'SampleTime',[1,1],{'st',0};...
     'Scale',[1,3],{'gt',0};};
     autoblkscheckparams(Block,ParamList);
-
-
     TransformOutportEnabled=get_param(Block,'TransformOutportEnabled');
     TranslationOutportOptions={'simulink/Sinks/Terminator','Location Terminator';...
     'simulink/Sinks/Out1','Location'};
@@ -57,8 +48,5 @@ function Initialization(Block)
         autoblksreplaceblock(Block,RotationOutportOptions,2);
         set_param([Block,'/Orientation'],'Unit','rad')
     end
-
-
-
 
 end
