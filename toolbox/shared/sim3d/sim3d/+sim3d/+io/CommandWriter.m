00e8cebadd8edc1a0c8f8274aebdd02bced17af5
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 14:11:28 +0800

    更新 CommandWriter.m

--- toolbox/shared/sim3d/sim3d/+sim3d/+io/CommandWriter.m
+++ toolbox/shared/sim3d/sim3d/+sim3d/+io/CommandWriter.m
@@ -4,18 +4,24 @@ properties
 Writer = [  ];
 SampleTime( 1, 1 )single{ mustBeFinite } = single(  - 1 );
 State = int32(  - 1 );
-end 
+end
+
+
 properties ( Constant = true )
 Topic = 'Simulation3DEngineCommand';
 LeaseDuration = 0
-end 
-methods 
-function self = CommandWriter(  )
-command = struct( 'state', self.getState(  ), 'sampleTime', self.getSampleTime(  ) );
+end
+
+
+methods
+
+function self = CommandWriter()
+command = struct( 'state', self.getState(), 'sampleTime', self.getSampleTime() );
 self.Writer = sim3d.io.Publisher( sim3d.io.CommandWriter.Topic,  ...
 'Packet', command,  ...
 'LeaseDuration', sim3d.io.CommandWriter.LeaseDuration );
-end 
+end
+
 
 function setSampleTime( self, sampleTime )
 R36
@@ -23,39 +29,42 @@ self sim3d.io.CommandWriter
 sampleTime( 1, 1 )single{ mustBePositive }
 end 
 self.SampleTime = sampleTime;
-end 
+end
 
-function sampleTime = getSampleTime( self )
+
+function sampleTime = getSampleTime(self)
 sampleTime = self.SampleTime;
-end 
+end
 
-function setState( self, state )
+
+function setState(self, state)
 R36
 self sim3d.io.CommandWriter
 state( 1, 1 )int32
 end 
 self.State = state;
-end 
+end
 
-function state = getState( self )
+
+function state = getState(self)
 state = self.State;
-end 
+end
 
-function delete( self )
+
+function delete(self)
 if ~isempty( self.Writer )
 self.Writer.delete(  );
 end 
-end 
+end
 
-function write( self )
-if ~isempty( self.Writer )
-command = struct( 'state', self.getState(  ), 'sampleTime', self.getSampleTime(  ) );
+
+function write(self)
+if ~isempty(self.Writer)
+command = struct( 'state', self.getState(), 'sampleTime', self.getSampleTime() );
 self.Writer.send( command );
 end 
 end 
 end 
 end 
 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpj7Hbn1.p.
-% Please follow local copyright laws when handling this file.
 
