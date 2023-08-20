bca73d71ab2dd65fe8ab1495a07054ed004f34f7
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 22:47:27 +0800

    更新 MexDlnetworkLayer.m

--- toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/MexDlnetworkLayer.m
+++ toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/MexDlnetworkLayer.m
@@ -1,48 +1,27 @@
 classdef MexDlnetworkLayer<nnet.internal.cnn.coder.MexNetworkLayer
 
-
-
-
-
     methods
         function obj=MexDlnetworkLayer(name,graph,mexNetwork)
-
-
-
-
-
             requestedOutputs=[mexNetwork.Config.LayerOutputIndices(:),mexNetwork.Config.LayerOutputPortIndices(:)];
             graph=graph.overrideOutputLocations(requestedOutputs);
-
-
             obj=obj@nnet.internal.cnn.coder.MexNetworkLayer(mexNetwork,name,graph);
         end
 
 
         function Z=predict(this,X)
-
             expectedPrecision=this.UnderlyingMexNetwork.Config.Precision;
 
             X=iWrapInCell(X);
 
-
             numInputs=numel(X);
             for i=1:numInputs
                 currentX=X{i};
-
-
-
-
                 data=extractdata(currentX);
 
                 data=gpuArray(data);
 
-
-
                 X{i}=cast(data,expectedPrecision);
             end
-
-
             Z=this.UnderlyingMexNetwork.predict(X);
 
             for i=1:numel(Z)
@@ -52,6 +31,7 @@ classdef MexDlnetworkLayer<nnet.internal.cnn.coder.MexNetworkLayer
         end
     end
 end
+
 
 function X=iWrapInCell(X)
     if~iscell(X)
