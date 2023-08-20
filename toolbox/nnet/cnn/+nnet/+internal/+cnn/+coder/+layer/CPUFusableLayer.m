291c9b230e2fc4282e92cb4e93f02fc1b1fba84a
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 16:39:57 +0800

    更新 CPUFusableLayer.m

--- toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/+layer/CPUFusableLayer.m
+++ toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/+layer/CPUFusableLayer.m
@@ -1,19 +1,10 @@
 %#codegen
 classdef(Abstract)CPUFusableLayer
 
-
-
-
-
-
-
-
-
-
-
     methods(Hidden=true)
         function obj=CPUFusableLayer()
             coder.allowpcode('plain');
         end
     end
+
 end
