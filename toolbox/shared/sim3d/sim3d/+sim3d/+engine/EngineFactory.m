3cb6eb3ddebc953002ebd92844ff17975fa2fb6b
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 11:11:15 +0800

    更新 EngineFactory.m

--- toolbox/shared/sim3d/sim3d/+sim3d/+engine/EngineFactory.m
+++ toolbox/shared/sim3d/sim3d/+sim3d/+engine/EngineFactory.m
@@ -1,16 +1,16 @@
 classdef EngineFactory<handle
-
     properties(Constant=true,Access=private)
         map=containers.Map({'win64','glnxa64','maci64'},{'EngineWin64','EngineGlnxa64',[]});
     end
 
-    methods
-        function self=EngineFactory()
 
+    methods
+
+        function self=EngineFactory()
         end
 
-        function engine=createEngine(self)
 
+        function engine=createEngine(self)
             engineClass=self.map(computer('arch'));
             if isempty(engineClass)
                 error(message('shared_sim3dblks:sim3dblkConfig:blkPrmError_UnsupportedPlatform'));
