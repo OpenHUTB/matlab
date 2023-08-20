2007da80dbddc5a757caa5c138de282263a275aa
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 13:26:18 +0800

    更新 RootObject.m

--- toolbox/shared/sim3d/sim3d/+sim3d/+internal/RootObject.m
+++ toolbox/shared/sim3d/sim3d/+sim3d/+internal/RootObject.m
@@ -1,37 +1,31 @@
 classdef RootObject<sim3d.AbstractActor
 
-
     methods
         function self=RootObject()
-
-
             self@sim3d.AbstractActor('Scene Origin',0,[0,0,0],[0,0,0],[1,1,1]);
         end
     end
+
+
     methods(Hidden)
+
         function actorType=getActorType(~)
             actorType=sim3d.utils.ActorTypes.Custom;
         end
 
         function setupTree(self)
-
-
-
             childList=self.getChildList();
             if~isempty(childList)
                 for i=1:numel(childList)
                     self.Children.(childList{i}).setupTree();
                 end
             else
-
                 warning(message("shared_sim3d:sim3dAbstractActor:NoActorsInScene"));
             end
         end
 
+
         function output(self)
-
-
-
             childList=self.getChildList();
             if~isempty(childList)
                 for i=1:numel(childList)
@@ -40,10 +34,8 @@ classdef RootObject<sim3d.AbstractActor
             end
         end
 
+
         function update(self)
-
-
-
             childList=self.getChildList();
             if~isempty(childList)
                 for i=1:numel(childList)
@@ -52,10 +44,8 @@ classdef RootObject<sim3d.AbstractActor
             end
         end
 
+
         function remove(self)
-
-
-
             childList=self.getChildList();
             if~isempty(childList)
                 for i=1:numel(childList)
@@ -63,6 +53,7 @@ classdef RootObject<sim3d.AbstractActor
                 end
             end
         end
+
 
         function actorS=getAttributes(self)
             actorS=[];
