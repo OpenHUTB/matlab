12e65f32fb3a7feb29f8af06b8c87414add266b8
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 14:37:59 +0800

    更新 LidarSensor.m

--- toolbox/shared/sim3d/sim3d/+sim3d/+sensors/LidarSensor.m
+++ toolbox/shared/sim3d/sim3d/+sim3d/+sensors/LidarSensor.m
@@ -1,17 +1,20 @@
 classdef LidarSensor<sim3d.sensors.Sensor
 
-
     properties(Access=private)
         LidarPublisher=[];
         LidarDataSubscriber=[];
         LidarSemanticSubscriber=[];
     end
+
+
     properties(Constant=true)
         SuffixOut='/Lidar_OUT';
         SuffixSignalIn='/LidarSignal_IN';
         SuffixSemanticSignalIn='/LidarSemanticSignal_IN';
         NumQuadrants=3;
     end
+
+
     properties(SetAccess=protected)
 NumVertPoints
 NumHorzPoints
@@ -19,11 +22,12 @@ QuadrantSize
 Settings
     end
 
+
     methods
+
         function self=LidarSensor(sensorID,vehicleID,lidarProperties,transformProperties)
             sensorName=sim3d.sensors.Sensor.getSensorName('Lidar',sensorID);
             self@sim3d.sensors.Sensor(sensorName,vehicleID,transformProperties);
-
             validateattributes(lidarProperties.MaxRange,{'single'},{'<=',500,'nonnegative'});
             validateattributes(lidarProperties.VertFOV,{'single'},{'<=',90,'nonnegative'});
             validateattributes(lidarProperties.VertAngularResolution,{'single'},{'<=',10,'nonnegative'});
@@ -32,51 +36,45 @@ Settings
             validateattributes(lidarProperties.RangeQuantizationFactor,{'numeric'},{'>=',lidarProperties.MaxRange/pow2(24)});
             validateattributes(transformProperties.getTranslation(),{'single'},{'size',[1,3]});
             validateattributes(transformProperties.getRotation(),{'single'},{'size',[1,3]});
-
             self.Settings.MaxRange=lidarProperties.MaxRange;
             self.Settings.VerticalFOV=lidarProperties.VertFOV;
             self.Settings.VerticalAngularResolution=lidarProperties.VertAngularResolution;
             self.Settings.HorizontalFOV=lidarProperties.HorzFOV/self.NumQuadrants;
             self.Settings.HorizontalAngularResolution=lidarProperties.HorzAngularResolution;
             self.Settings.RangeQuantizationFactor=lidarProperties.RangeQuantizationFactor;
-
             self.NumVertPoints=ceil(lidarProperties.VertFOV/lidarProperties.VertAngularResolution);
             self.NumHorzPoints=ceil(lidarProperties.HorzFOV/(sim3d.sensors.LidarSensor.NumQuadrants*lidarProperties.HorzAngularResolution));
             self.QuadrantSize=uint32(self.NumVertPoints*self.NumHorzPoints);
             self.NumHorzPoints=self.NumHorzPoints*sim3d.sensors.LidarSensor.NumQuadrants;
         end
 
+
         function setup(self)
             setup@sim3d.sensors.Sensor(self)
-
             self.LidarPublisher=sim3d.io.Publisher([self.getTag(),sim3d.sensors.LidarSensor.SuffixOut]);
             self.LidarDataSubscriber=sim3d.io.Subscriber([self.getTag(),sim3d.sensors.LidarSensor.SuffixSignalIn]);
             self.LidarSemanticSubscriber=sim3d.io.Subscriber([self.getTag(),sim3d.sensors.LidarSensor.SuffixSemanticSignalIn]);
-
             sim3d.engine.EngineReturnCode.assertObject(self.LidarPublisher);
             sim3d.engine.EngineReturnCode.assertObject(self.LidarDataSubscriber);
             sim3d.engine.EngineReturnCode.assertObject(self.LidarSemanticSubscriber);
         end
+
 
         function delete(self)
             if~isempty(self.LidarPublisher)
                 self.LidarPublisher.delete();
                 self.LidarPublisher=[];
             end
-
             if~isempty(self.LidarDataSubscriber)
                 self.LidarDataSubscriber.delete();
                 self.LidarDataSubscriber=[];
             end
-
             if~isempty(self.LidarSemanticSubscriber)
                 self.LidarSemanticSubscriber.delete();
                 self.LidarSemanticSubscriber=[];
             end
-
             delete@sim3d.sensors.Sensor(self);
         end
-
         function[range,reflectivity,semantic]=readSignal(self)
             sim3d.engine.EngineReturnCode.assertObject(self.LidarDataSubscriber);
             if self.LidarDataSubscriber.hasMessage()
@@ -92,7 +90,6 @@ Settings
                 range=[];
                 reflectivity=[];
             end
-
             sim3d.engine.EngineReturnCode.assertObject(self.LidarSemanticSubscriber);
             if self.LidarSemanticSubscriber.hasMessage()
                 semantic123=self.LidarSemanticSubscriber.receive();
@@ -103,6 +100,7 @@ Settings
             end
 
         end
+
 
         function reset(self)
             reset@sim3d.sensors.Sensor(self);
@@ -118,13 +116,18 @@ Settings
         end
     end
 
+
     methods(Access=public,Hidden)
+
         function numVertPoints=getNumVertPoints(self)
             numVertPoints=self.NumVertPoints;
         end
+
+
         function numHorzPoints=getNumHorzPoints(self)
             numHorzPoints=self.NumHorzPoints;
         end
+
 
         function actorType=getActorType(~)
             actorType=sim3d.utils.ActorTypes.Lidar;
@@ -135,7 +138,6 @@ Settings
     methods(Access=protected)
         function[range,reflectivity]=decodeAndScaleData(self,D)
 
-
             noHitIndex=D(:)==0;
             reflectivity=single(bitshift(D,-24))/255.0;
             reflectivity(noHitIndex)=NaN;
@@ -143,20 +145,11 @@ Settings
             range(noHitIndex)=NaN;
         end
     end
+
+
     methods(Static)
+
         function sensorProperties=getLidarSensorProperties()
-
-
-
-
-
-
-
-
-
-
-
-
             sensorProperties=struct('MaxRange',single(120),'VertFOV',single(40),...
             'VertAngularResolution',single(1.25),'HorzFOV',single(360),'HorzAngularResolution',single(0.16),...
             'RangeQuantizationFactor',single(0.002));
