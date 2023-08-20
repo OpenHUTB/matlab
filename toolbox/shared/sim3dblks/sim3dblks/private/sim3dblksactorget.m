8cf2d4819c1253f6c70ff24d690cbe6edf04c197
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 15:11:23 +0800

    更新 sim3dblksactorget.m

--- toolbox/shared/sim3dblks/sim3dblks/private/sim3dblksactorget.m
+++ toolbox/shared/sim3dblks/sim3dblks/private/sim3dblksactorget.m
@@ -12,18 +12,17 @@ function[varargout]=sim3dblksactorget(varargin)
     end
 end
 
+
 function IconInfo=DrawCommands(Block)
 
     AliasNames={};
     IconInfo=autoblksgetportlabels(Block,AliasNames);
-
-
     IconInfo.ImageName='sim3dtransform.png';
     [IconInfo.image,IconInfo.position]=iconImageUpdate(IconInfo.ImageName,1,20,200,'white');
 end
 
-function Initialization(Block)
 
+function Initialization(Block)
     ParamList={...
     'CustNumOfParts',[1,1],{'gte',1};...
     'NumberOfParts',[1,1],{'gte',1};...
