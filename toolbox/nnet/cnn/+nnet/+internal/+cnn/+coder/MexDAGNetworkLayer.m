df3325b84e43168afe7ed08c8cb5be820078e812
王海东 <wanghaidong@hutb.edu.cn>
2023-08-20 22:45:07 +0800

    更新 MexDAGNetworkLayer.m

--- toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/MexDAGNetworkLayer.m
+++ toolbox/nnet/cnn/+nnet/+internal/+cnn/+coder/MexDAGNetworkLayer.m
@@ -1,23 +1,15 @@
 classdef MexDAGNetworkLayer<nnet.internal.cnn.coder.MexNetworkLayer
 
-
-
-
-
     properties(Access=private)
-
-
         OutputAs2DRowResponses logical
     end
 
 
     methods
+
         function obj=MexDAGNetworkLayer(name,layerGraph,mexNetwork)
             obj=obj@nnet.internal.cnn.coder.MexNetworkLayer(mexNetwork,name,layerGraph);
-
             numOutputs=mexNetwork.Config.NumOutputs;
-
-
             outputAs2DRowResponses=false(numOutputs,1);
             if mexNetwork.Config.IsCallingPredict
                 networkOutputs=obj.forwardExampleInputs(obj.InputData);
@@ -34,33 +26,22 @@ classdef MexDAGNetworkLayer<nnet.internal.cnn.coder.MexNetworkLayer
     methods
 
         function Z=predict(this,X)
-
             numInputs=numel(this.InputSize);
             requiredBatchSize=this.UnderlyingMexNetwork.Config.MiniBatchSize;
 
             if numInputs>1
                 X=iWrapInCell(X);
-
-
-
                 [padAmount,inputBatchSize]=iGetRequiredPadAmount(X{1},requiredBatchSize);
-
                 X={cellfun(@(x)iPadBatches(x,requiredBatchSize,padAmount,inputBatchSize),X,'UniformOutput',false)};
             else
-
                 [padAmount,inputBatchSize]=iGetRequiredPadAmount(X,requiredBatchSize);
                 X={iPadBatches(X,requiredBatchSize,padAmount,inputBatchSize)};
             end
-
-
             Z=this.UnderlyingMexNetwork.predict(X);
             Z=iWrapInCell(Z);
 
             for i=1:numel(Z)
                 if padAmount>0
-
-
-
                     if this.OutputAs2DRowResponses(i)
                         Z{i}=Z{i}(1:inputBatchSize,:);
                     else
@@ -69,12 +50,6 @@ classdef MexDAGNetworkLayer<nnet.internal.cnn.coder.MexNetworkLayer
                 end
             end
         end
-
-
-
-
-
-
 
 
         function Z=activations(this,X,~,~)
@@ -86,23 +61,15 @@ classdef MexDAGNetworkLayer<nnet.internal.cnn.coder.MexNetworkLayer
 
 end
 
+
 function[padAmount,inputBatchSize]=iGetRequiredPadAmount(X,requiredBatchSize)
 
-
-
     inputBatchSize=size(X,4);
-
     padAmount=requiredBatchSize-inputBatchSize;
 end
 
+
 function Y=iPadBatches(X,requiredBatchSize,padAmount,inputBatchSize)
-
-
-
-
-
-
-
 
     if padAmount>0
         Y=zeros(size(X,1),size(X,2),size(X,3),requiredBatchSize,'like',X);
@@ -112,6 +79,7 @@ function Y=iPadBatches(X,requiredBatchSize,padAmount,inputBatchSize)
     end
 
 end
+
 
 function X=iWrapInCell(X)
     if~iscell(X)
