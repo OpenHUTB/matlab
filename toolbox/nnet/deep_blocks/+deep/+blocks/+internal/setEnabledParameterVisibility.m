00612441faea89bccbf14d74ec7d0894a5264bf6
王海东 <wanghaidong@hutb.edu.cn>
2023-08-21 21:14:15 +0800

    更新 setEnabledParameterVisibility.m

--- toolbox/nnet/deep_blocks/+deep/+blocks/+internal/setEnabledParameterVisibility.m
+++ toolbox/nnet/deep_blocks/+deep/+blocks/+internal/setEnabledParameterVisibility.m
@@ -1,10 +1,5 @@
 function setEnabledParameterVisibility(block,useParamName,paramName)
 
-
-
-
-
-
     mask=Simulink.Mask.get(block);
     useParam=mask.getParameter(useParamName);
     param=mask.getParameter(paramName);
