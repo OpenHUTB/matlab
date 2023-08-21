77fe8ba5560fb8389126bf3567b169c0c58d8771
王海东 <wanghaidong@hutb.edu.cn>
2023-08-21 21:10:05 +0800

    更新 objectDetector.m

--- toolbox/nnet/deep_blocks/+deep/+blocks/+internal/objectDetector.m
+++ toolbox/nnet/deep_blocks/+deep/+blocks/+internal/objectDetector.m
@@ -9,15 +9,9 @@ function[varargout]=objectDetector(...
     labelsEnabled,...
     scoresEnabled)%#codegen
 
-
-
-
-
     coder.inline('always');
     coder.allowpcode('plain');
     coder.extrinsic('coder.internal.getFileInfo');
-
-
     fileName=coder.const(@coder.internal.getFileInfo,detectorToLoad);
     coder.internal.addDependentFile(fileName);
     persistent detector;
@@ -29,14 +23,11 @@ function[varargout]=objectDetector(...
         end
     end
 
-
     dims=ndims(image);
     coder.internal.assert(any(dims==[2,3]),'vision:ObjectDetectorBlock:InvalidImage');
 
     if coder.const(useExtrinsic)
         detectorClass=coder.const(feval('deep.blocks.internal.loadObjectDetector',coder.const(detectorToLoad),'ReturnDetectorClassName',true));
-
-
 
         if coder.const(any(strcmp(detectorClass,{'yolov2ObjectDetector','ssdObjectDetector',...
             'rcnnObjectDetector','fastRCNNObjectDetector','fasterRCNNObjectDetector'})))
@@ -44,19 +35,13 @@ function[varargout]=objectDetector(...
         elseif coder.const(any(strcmp(detectorClass,{'yolov3ObjectDetector','yolov4ObjectDetector'})))
             bboxes=coder.nullcopy(zeros(maxDetections,4,'single'));%#ok
         end
-
-
-
         scores=coder.nullcopy(zeros(maxDetections,1,'single'));%#ok
         labelsIdxs=coder.nullcopy(zeros(maxDetections,1,'single'));%#ok
-
         coder.varsize('bboxes','scores','labelsIdxs');
-
         [bboxes,scores,labelsIdxs]=feval('deep.blocks.internal.detectWrapper',detector,image,detectArgs,ssbmArgs);
     else
         [bboxes,scores,labelsIdxs]=deep.blocks.internal.detectWrapper(detector,image,detectArgs,ssbmArgs);
     end
-
 
     startIndex=0;
     if coder.const(bboxesEnabled)
