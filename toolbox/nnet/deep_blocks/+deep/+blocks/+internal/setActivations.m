87014c70e093f08c03991ebf018b810586bdfd67
王海东 <wanghaidong@hutb.edu.cn>
2023-08-21 21:12:42 +0800

    更新 setActivations.m

--- toolbox/nnet/deep_blocks/+deep/+blocks/+internal/setActivations.m
+++ toolbox/nnet/deep_blocks/+deep/+blocks/+internal/setActivations.m
@@ -1,11 +1,7 @@
 function setActivations(block)
 
-
-
-
     mask=Simulink.Mask.get(block);
     activationsListBox=mask.getParameter('Activations');
-
     networkToLoad=deep.blocks.internal.getSelectedNetwork(block);
 
     try
@@ -14,10 +10,8 @@ function setActivations(block)
     catch
         layerNames={};
     end
-
     currentActivations=eval(activationsListBox.Value);
     newActivations=deep.blocks.internal.cell2str(intersect(layerNames,currentActivations));
-
     activationsListBox.set('TypeOptions',layerNames);
     activationsListBox.set('Value',newActivations);
 
