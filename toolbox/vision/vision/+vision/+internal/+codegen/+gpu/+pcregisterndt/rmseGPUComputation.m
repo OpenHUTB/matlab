8915ecdfbd28b436c8186a436d2737d3f694424d
王海东 <wanghaidong@hutb.edu.cn>
2023-08-23 18:21:11 +0800

    更新 rmseGPUComputation.m

--- toolbox/vision/vision/+vision/+internal/+codegen/+gpu/+pcregisterndt/rmseGPUComputation.m
+++ toolbox/vision/vision/+vision/+internal/+codegen/+gpu/+pcregisterndt/rmseGPUComputation.m
@@ -1,26 +1,11 @@
-
-
-
 function rmseValue=rmseGPUComputation(fixedLocations,movingLocations)
 %#codegen
-
-
-
-
-
-
-
     coder.gpu.internal.kernelfunImpl(false);
     coder.allowpcode('plain');
     coder.inline('never');
 
-
     numFixedPoints=numel(fixedLocations)/3;
     numMovingPoints=numel(movingLocations)/3;
-
-
-
-
 
     distMat=Inf(numMovingPoints,1);
 
@@ -33,10 +18,8 @@ function rmseValue=rmseGPUComputation(fixedLocations,movingLocations)
             diffMat=[movingPt(1)-fixedPt(1),movingPt(2)-fixedPt(2),movingPt(3)-fixedPt(3)];
             distVal=double(diffMat(1)*diffMat(1)+diffMat(2)*diffMat(2)+diffMat(3)*diffMat(3));
 
-
             old=uint64(0);
             old=coder.ceval('-layout:any','-gpudevicefcn','__double_as_longlong',distMat(mPtIter));
-
             while(distVal<distMat(mPtIter))
                 assumed=old;
                 assumedDouble=0;
@@ -51,12 +34,11 @@ function rmseValue=rmseGPUComputation(fixedLocations,movingLocations)
             end
         end
     end
-
-
     distMatSum=gpucoder.reduce(distMat,@funcSum);
     rmseValue=single(distMatSum(1));
     rmseValue=sqrt(rmseValue/numMovingPoints);
 end
+
 
 function c=funcSum(a,b)
     c=a+b;
