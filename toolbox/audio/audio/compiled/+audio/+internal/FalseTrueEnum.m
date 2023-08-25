ff88839641007bc6ceb50017b6eeb9c9c0add5ca
王海东 <wanghaidong@hutb.edu.cn>
2023-08-25 12:12:33 +0800

    更新 FalseTrueEnum.m

--- toolbox/audio/audio/compiled/+audio/+internal/FalseTrueEnum.m
+++ toolbox/audio/audio/compiled/+audio/+internal/FalseTrueEnum.m
@@ -1,13 +1,11 @@
 classdef FalseTrueEnum<int8
 
-
-
-
-
     enumeration
         false(0)
         true(1)
     end
+
+
     methods(Static)
         function yes=addClassNameToEnumNames
             yes=true;
