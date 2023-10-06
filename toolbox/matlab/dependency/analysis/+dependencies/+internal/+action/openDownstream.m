8084deb02ba1d0becf5966d785261b610217b546
王海东 <wanghaidong@hutb.edu.cn>
2023-10-06 16:59:05 +0800

    更新 openDownstream.m

--- toolbox/matlab/dependency/analysis/+dependencies/+internal/+action/openDownstream.m
+++ toolbox/matlab/dependency/analysis/+dependencies/+internal/+action/openDownstream.m
@@ -1,8 +1,4 @@
 function openDownstream(dependency)
-
-
-
-
     manager=dependencies.internal.action.dependency.HiliteManager.Instance;
     manager.hilite(dependency.DownstreamNode,dependency,@openDownstream);
 
