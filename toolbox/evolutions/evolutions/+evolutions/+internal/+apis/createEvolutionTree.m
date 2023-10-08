ec53df9d412f60f57a4b9243fc506bcce580909f
王海东 <wanghaidong@hutb.edu.cn>
2023-10-08 22:26:34 +0800

    更新 createEvolutionTree.m

--- toolbox/evolutions/evolutions/+evolutions/+internal/+apis/createEvolutionTree.m
+++ toolbox/evolutions/evolutions/+evolutions/+internal/+apis/createEvolutionTree.m
@@ -1,8 +1,5 @@
 function evolutionTreeInfoIdentifier = createEvolutionTree( projectId, treeName )
 
-
-
-
 R36
 projectId( 1, : )char
 treeName( 1, : )char
@@ -15,7 +12,4 @@ evolutionTreeInfoIdentifier = evolutionTreeInfo.Id;
 end 
 
 
-
-% Decoded using De-pcode utility v1.2 from file /tmp/tmp9o0Rhp.p.
-% Please follow local copyright laws when handling this file.
 
