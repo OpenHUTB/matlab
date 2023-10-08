1354ac5bdb54487b8ede03824b42d8086a02a639
王海东 <wanghaidong@hutb.edu.cn>
2023-10-08 22:25:19 +0800

    更新 changeEvolutionTreeName.m

--- toolbox/evolutions/evolutions/+evolutions/+internal/+apis/changeEvolutionTreeName.m
+++ toolbox/evolutions/evolutions/+evolutions/+internal/+apis/changeEvolutionTreeName.m
@@ -1,8 +1,6 @@
 function changeEvolutionTreeName( treeId, name )
 
 
-
-
 R36
 treeId( 1, : )char
 name( 1, : )char
@@ -14,6 +12,4 @@ evolutions.internal.changeEvolutionTreeName( treeInfo, evolutionInfo, name );
 
 end 
 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpBheu14.p.
-% Please follow local copyright laws when handling this file.
 
