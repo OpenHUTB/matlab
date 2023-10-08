9a207201e4f28652335078208bd3ef337b48cddf
王海东 <wanghaidong@hutb.edu.cn>
2023-10-08 22:24:52 +0800

    更新 changeEvolutionName.m

--- toolbox/evolutions/evolutions/+evolutions/+internal/+apis/changeEvolutionName.m
+++ toolbox/evolutions/evolutions/+evolutions/+internal/+apis/changeEvolutionName.m
@@ -1,8 +1,6 @@
 function changeEvolutionName( treeId, evolutionId, name )
 
 
-
-
 R36
 treeId( 1, : )char
 evolutionId( 1, : )char
@@ -15,6 +13,4 @@ evolutionInfo = evolutions.internal.getDataObject( evolutionId );
 evolutions.internal.changeEvolutionName( treeInfo, evolutionInfo, name );
 end 
 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpmpJYyD.p.
-% Please follow local copyright laws when handling this file.
 
