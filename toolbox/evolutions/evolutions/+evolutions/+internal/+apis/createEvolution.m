5327918acdfb45ec6e7392844ac821602f0bd6c8
王海东 <wanghaidong@hutb.edu.cn>
2023-10-08 22:26:09 +0800

    更新 createEvolution.m

--- toolbox/evolutions/evolutions/+evolutions/+internal/+apis/createEvolution.m
+++ toolbox/evolutions/evolutions/+evolutions/+internal/+apis/createEvolution.m
@@ -1,8 +1,5 @@
 function [ success, output ] = createEvolution( treeId, evolutionName )
 
-
-
-
 R36
 treeId( 1, : )char
 evolutionName( 1, : )char
@@ -12,7 +9,4 @@ tree = evolutions.internal.getDataObject( treeId );
 [ success, output ] = evolutions.internal.createEvolution( tree, evolutionName );
 
 end 
-
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpe_UepN.p.
-% Please follow local copyright laws when handling this file.
 
