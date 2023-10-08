72486323471439fc097cdd37016cfc177f04925c
王海东 <wanghaidong@hutb.edu.cn>
2023-10-08 22:25:44 +0800

    更新 compareFiles.m

--- toolbox/evolutions/evolutions/+evolutions/+internal/+apis/compareFiles.m
+++ toolbox/evolutions/evolutions/+evolutions/+internal/+apis/compareFiles.m
@@ -1,8 +1,5 @@
 function output = compareFiles( treeId, evolution1Id, evolution2Id, fileName )
 
-
-
-
 R36
 treeId( 1, : )char
 evolution1Id( 1, : )char
@@ -14,6 +11,4 @@ tree = evolutions.internal.getDataObject( treeId );
 output = compareFiles( tree, evolution1Id, evolution2Id, fileName );
 end 
 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpvUtCpA.p.
-% Please follow local copyright laws when handling this file.
 
