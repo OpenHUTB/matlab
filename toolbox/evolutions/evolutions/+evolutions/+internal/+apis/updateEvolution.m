1b89d16ce2b3781b529cd2f8d86876b4767c1770
王海东 <wanghaidong@hutb.edu.cn>
2023-10-08 22:50:00 +0800

    更新 updateEvolution.m

--- toolbox/evolutions/evolutions/+evolutions/+internal/+apis/updateEvolution.m
+++ toolbox/evolutions/evolutions/+evolutions/+internal/+apis/updateEvolution.m
@@ -1,8 +1,5 @@
 function[success,output]=updateEvolution(treeId)
 
-
-
-
     treeInfo=evolutions.internal.getDataObject(treeId);
 
     [success,output]=evolutions.internal.updateEvolution(treeInfo);
