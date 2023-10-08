7c1ea5f9c2acb5ddd1d9d7dee69a05680409ae9f
王海东 <wanghaidong@hutb.edu.cn>
2023-10-08 22:49:44 +0800

    更新 getObjectData.m

--- toolbox/evolutions/evolutions/+evolutions/+internal/+apis/getObjectData.m
+++ toolbox/evolutions/evolutions/+evolutions/+internal/+apis/getObjectData.m
@@ -1,9 +1,6 @@
 function data = getObjectData( objectId )
 
 
-
-
-
 R36
 objectId( 1, : )char
 end 
@@ -12,6 +9,5 @@ object = evolutions.internal.getDataObject( objectId );
 data = evolutions.internal.classhandler.ClassHandler.ReadObject( object );
 end 
 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpGgsdKq.p.
-% Please follow local copyright laws when handling this file.
+
 
