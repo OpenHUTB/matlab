db328cefaab4cae8a1b53342534ad75571a7238d
王海东 <wanghaidong@hutb.edu.cn>
2023-10-10 21:06:22 +0800

    更新 LicensedTemplateLibrary.m

--- toolbox/matlab/testframework/unittest/ext/+matlab/+unittest/+internal/+plugins/+testreport/LicensedTemplateLibrary.m
+++ toolbox/matlab/testframework/unittest/ext/+matlab/+unittest/+internal/+plugins/+testreport/LicensedTemplateLibrary.m
@@ -1,11 +1,5 @@
 classdef(Sealed)LicensedTemplateLibrary<mlreportgen.dom.LockedDocumentPart
 
-
-
-
-
-
-
     properties(Hidden,Constant)
         TemplateRootFolder=fullfile(fileparts(which(...
         'matlab.unittest.internal.plugins.testreport.LicensedTemplateLibrary')),'templates');
@@ -51,18 +45,6 @@ end
 
 
 function key=getLicenseKey()
-
-
-
-
-
-
-
-
-
-
-
-
 
     key='E2Cpo8aUQQVPjNCaQyzQ38crg/5ZW9arbltqhbrHKopv9q3bCCKwj6k8rUNckCVxbvOdd3KbhaxaOIXRrqs4/e4LNfJ98SiZGYIBXYqom4YweDFD0Yg/5x7PVbrr14BUuIfAKWzWmGW5v2ND4vpx4etytnMqNv7NHzGCRCrVGnG+XEn74lAY+wMz8TKUFoW+AZSzkAHV2Q2SgbrUgmHT/iExfOiQMehaZR1V7WVZqpO0l6Y7cKpgAPsYBuTluc1Bq9PfPPWhaM8lpWPOuw3LJU0=';
 end
