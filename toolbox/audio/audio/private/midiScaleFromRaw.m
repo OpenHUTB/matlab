c886311fbc3c7c522644b36027c644b9b93c40ae
王海东 <wanghaidong@hutb.edu.cn>
2023-10-12 22:20:06 +0800

    更新 midiScaleFromRaw.m

--- toolbox/audio/audio/private/midiScaleFromRaw.m
+++ toolbox/audio/audio/private/midiScaleFromRaw.m
@@ -1,17 +1,13 @@
 function x=midiScaleFromRaw(x)
 
-
-
     assert(isa(x,'double')&&isscalar(x)&&isreal(x));
 
 %#codegen
     coder.allowpcode('plain');
 
     if isempty(coder.target)
-
         x=midimexif('midiScaleFromRaw',x);
     else
-
         x=coder.ceval('scaleFromRawCpp',x);
     end
 end
