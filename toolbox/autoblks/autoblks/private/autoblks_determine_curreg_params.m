2164d48d75053a82fa5ec7d3853ec73d4b65d35d
王海东 <wanghaidong@hutb.edu.cn>
2023-10-12 22:26:11 +0800

    更新 autoblks_determine_curreg_params.m

--- toolbox/autoblks/autoblks/private/autoblks_determine_curreg_params.m
+++ toolbox/autoblks/autoblks/private/autoblks_determine_curreg_params.m
@@ -1,13 +1,6 @@
 function[Kp_d,Kp_q,Ki]=autoblks_determine_curreg_params(EV_current,Ld,Lq,Rs)
-
-
-
-
-
-
     Kp_d=2*pi*EV_current*Ld;
     Kp_q=2*pi*EV_current*Lq;
     Ki=2*pi*EV_current*Rs;
-
 end
 
