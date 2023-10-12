6ac2ce8eb3246b76643f54f199645ffe27b47018
王海东 <wanghaidong@hutb.edu.cn>
2023-10-12 22:15:24 +0800

    更新 ecoderutil.m

--- toolbox/audio/audio/compiled/private/ecoderutil.m
+++ toolbox/audio/audio/compiled/private/ecoderutil.m
@@ -1,16 +1,9 @@
 function ok=ecoderutil
 
-
-
-
-
-
     persistent isInstalled;
     if isempty(isInstalled)
         isInstalled=~isempty(ver('embeddedcoder'));
     end
-
-
     b=builtin('license','checkout','RTW_EMBEDDED_CODER');
     ok=b&&isInstalled;
 
