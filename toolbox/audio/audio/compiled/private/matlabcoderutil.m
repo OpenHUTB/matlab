d657491875e3079c968bc7ab42127f003d3a2a43
王海东 <wanghaidong@hutb.edu.cn>
2023-10-12 22:16:41 +0800

    更新 matlabcoderutil.m

--- toolbox/audio/audio/compiled/private/matlabcoderutil.m
+++ toolbox/audio/audio/compiled/private/matlabcoderutil.m
@@ -1,15 +1,9 @@
 function ok=matlabcoderutil
 
-
-
-
-
-
     persistent isInstalled;
     if isempty(isInstalled)
         isInstalled=~isempty(ver('matlabcoder'));
     end
-
 
     b=builtin('license','checkout','MATLAB_CODER');
     ok=b&&isInstalled;
