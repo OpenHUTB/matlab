2fa1c9de2b8f69e43573bbb3bca61d6be01cfe93
王海东 <wanghaidong@hutb.edu.cn>
2023-10-13 13:42:18 +0800

    更新 notifications.m

--- toolbox/classdiagram/app/core/+classdiagram/+app/+core/+feature/notifications.m
+++ toolbox/classdiagram/app/core/+classdiagram/+app/+core/+feature/notifications.m
@@ -1,6 +1,5 @@
 function notifications( val )
 
-
 R36
 val( 1, 1 )logical
 end 
@@ -9,6 +8,5 @@ cdv = s.classviewer;
 cdv.notifications.TemporaryValue = val;
 end 
 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpuxGCtg.p.
-% Please follow local copyright laws when handling this file.
+
 
