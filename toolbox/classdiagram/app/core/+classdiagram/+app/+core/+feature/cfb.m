a9b2ea22e384c23f2ea1cbbff954a1dfd28682a7
王海东 <wanghaidong@hutb.edu.cn>
2023-10-13 13:39:58 +0800

    更新 cfb.m

--- toolbox/classdiagram/app/core/+classdiagram/+app/+core/+feature/cfb.m
+++ toolbox/classdiagram/app/core/+classdiagram/+app/+core/+feature/cfb.m
@@ -1,6 +1,5 @@
 function cfb( val )
 
-
 R36
 val( 1, 1 )logical
 end 
@@ -8,6 +7,5 @@ s = settings;
 cdv = s.classviewer;
 cdv.cfb.JavaOn.TemporaryValue = val;
 end 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpnu_fX3.p.
-% Please follow local copyright laws when handling this file.
+
 
