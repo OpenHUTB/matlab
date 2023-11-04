15854ef0387a7f99558f487bc47d151413e6b626
王海东 <wanghaidong@hutb.edu.cn>
2023-11-04 22:42:10 +0800

    更新 eml_al_cordic_quad_correction_before.m

--- toolbox/shared/algorithmlowering/emlauthoring/intrinsic/eml_al_cordic_quad_correction_before.m
+++ toolbox/shared/algorithmlowering/emlauthoring/intrinsic/eml_al_cordic_quad_correction_before.m
@@ -1,7 +1,5 @@
 function[needToNegate,theta_in_range]=eml_al_cordic_quad_correction_before(theta,K)%#ok<INUSD> 
 
-
-
 %#codegen
     coder.allowpcode('plain')
     [theta_in_range,needToNegate]=fixed.internal.cordiccexpInputQuadrantCorrection(theta);
