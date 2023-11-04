17cba19d14bec7ea640b72df712705f32e5bf029
王海东 <wanghaidong@hutb.edu.cn>
2023-11-04 22:40:09 +0800

    更新 alg_v5_output.m

--- toolbox/shared/algorithmlowering/emlauthoring/intrinsic/alg_v5_output.m
+++ toolbox/shared/algorithmlowering/emlauthoring/intrinsic/alg_v5_output.m
@@ -1,12 +1,10 @@
 function[r]=alg_v5_output(state)
 
-
 %#codegen
 
     coder.allowpcode('plain');
 
     tpm32=2.328306436538696e-10;
-
 
     ui=eml_plus(state(1),state(2),'uint32','wrap');
     r=double(ui)*tpm32;
