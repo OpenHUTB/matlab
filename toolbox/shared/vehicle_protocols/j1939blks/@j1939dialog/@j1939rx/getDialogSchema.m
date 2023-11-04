633e14567a2da95c967768c33d9c2a9f7ce30b00
王海东 <wanghaidong@hutb.edu.cn>
2023-11-04 21:24:27 +0800

    更新 getDialogSchema.m

--- toolbox/shared/vehicle_protocols/j1939blks/@j1939dialog/@j1939rx/getDialogSchema.m
+++ toolbox/shared/vehicle_protocols/j1939blks/@j1939dialog/@j1939rx/getDialogSchema.m
@@ -1,22 +1,8 @@
 function dlgStruct=getDialogSchema(obj,~)
 
-
-
-
-
-
-
-
-
-
-
-
-
     rowSpan=[1,1];
     colSpan=[1,20];
     descPane=tamslgate('privateslwidgetdescgrp',obj,rowSpan,colSpan);
-
-
 
     paramPane=j1939.internal.createBlockPane(obj);
 
@@ -24,7 +10,6 @@ function dlgStruct=getDialogSchema(obj,~)
     dlgItems={descPane,paramPane};
     dlgStruct=tamslgate('privateslpanemaindlg',obj,dlgItems,...
     'j1939.j1939slcbpreapply','closeCallback');
-
 
     [isLibrary,isLocked]=obj.isLibraryBlock(obj.Block);
     if(isLibrary&&isLocked)||any(strcmp(obj.Root.SimulationStatus,{'running','paused','external'}))
