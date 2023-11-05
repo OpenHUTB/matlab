3ab58a9f9fbb095cecb272466df5445ea3884595
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:07:42 +0800

    更新 isHTMLViewer.m

--- toolbox/matlab/htmlviewer/+matlab/+htmlviewer/+internal/isHTMLViewer.m
+++ toolbox/matlab/htmlviewer/+matlab/+htmlviewer/+internal/isHTMLViewer.m
@@ -1,15 +1,6 @@
 function status=isHTMLViewer()
-
-
-
-
-
-
     isJSDorMO=matlab.desktop.editor.internal.useJavaScriptBackEnd;
     forceHTMLViewer=strcmp(getenv('HTML_VIEWER'),'true')||strcmp(getenv('HTML_VIEWER'),'1');
-
-
-
 
     status=isJSDorMO||forceHTMLViewer;
 end
