6834758448477e68800790f5cf066ce2b0b6f5b0
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:55:34 +0800

    更新 CodeGenWidgetStateChangedData.m

--- toolbox/matlab/plottools/+matlab/+graphics/+internal/+codegenwidget/+eventData/CodeGenWidgetStateChangedData.m
+++ toolbox/matlab/plottools/+matlab/+graphics/+internal/+codegenwidget/+eventData/CodeGenWidgetStateChangedData.m
@@ -1,19 +1,11 @@
 classdef CodeGenWidgetStateChangedData<matlab.ui.eventdata.internal.AbstractEventData
 
-
-
-
-
     properties(SetAccess='private')
         Data;
     end
 
     methods
         function obj=CodeGenWidgetStateChangedData(data)
-
-
-
-
             obj=obj@matlab.ui.eventdata.internal.AbstractEventData();
 
             obj.Data=data;
