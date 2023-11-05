d9730d45ce3ff531b0b39d3389c5c3e3bfcaf5b0
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:50:22 +0800

    更新 getPropertyGroups.m

--- toolbox/matlab/graph3d/+matlab/+graphics/+chart/+primitive/@Surface/getPropertyGroups.m
+++ toolbox/matlab/graph3d/+matlab/+graphics/+chart/+primitive/@Surface/getPropertyGroups.m
@@ -1,7 +1,5 @@
 function varargout=getPropertyGroups(~)
 
-
-
     varargout{1}=matlab.mixin.util.PropertyGroup(...
     {'EdgeColor','LineStyle','FaceColor','FaceLighting',...
     'FaceAlpha','XData','YData','ZData','CData'});
