9d2fe6d98a2095feddf5f36603dc9c76316e1317
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 09:21:34 +0800

    更新 diagnostic.m

--- toolbox/shared/coder/coder/+coder/+gpu/+internal/diagnostic.m
+++ toolbox/shared/coder/coder/+coder/+gpu/+internal/diagnostic.m
@@ -1,16 +1,11 @@
 function diagnostic(msgID,varargin)
 
-
 %#codegen
-
-
 
     if~coder.target('MATLAB')
         coder.allowpcode('plain');
 
-
         coder.inline('never');
-
         coder.internal.prefer_const(msgID);
         if coder.internal.targetLang('GPU')
             debugLevel=0;
