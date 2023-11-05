b8f784e71ac94cdc9b2601646c3a12a1afc4428a
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:43:38 +0800

    更新 getTimerMode_C5x.m

--- toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/getTimerMode_C5x.m
+++ toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/getTimerMode_C5x.m
@@ -1,9 +1,4 @@
 function[linkPjtGenInfo,schedPjtGenInfo]=getTimerMode_C5x(linkPjtGenInfo,schedPjtGenInfo)
-
-
-
-
-
     tgtPrefInfo=getTgtPrefInfo(linkPjtGenInfo.modelName);
     if strcmp(tgtPrefInfo.chipInfo.subFamily,'5501')||strcmp(tgtPrefInfo.chipInfo.subFamily,'5502')
         schedPjtGenInfo.timerOpt='64bit-timer';
