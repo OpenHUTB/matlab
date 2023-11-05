6a7b327a52bffa81f61365232021f924c237fbf9
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:07:05 +0800

    更新 getActiveWindow.m

--- toolbox/matlab/htmlviewer/+matlab/+htmlviewer/+internal/getActiveWindow.m
+++ toolbox/matlab/htmlviewer/+matlab/+htmlviewer/+internal/getActiveWindow.m
@@ -1,21 +1,8 @@
 function window=getActiveWindow()
 
-
-
-
-
-
     if matlab.htmlviewer.internal.isHTMLViewer
-
-
-
         window=matlab.htmlviewer.internal.HTMLViewerManager.getInstance().getLastActiveViewer();
     else
-
-
-
-
-
         window=com.mathworks.mde.webbrowser.WebBrowser.getActiveBrowser;
     end
 end
