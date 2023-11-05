4e0345fb07794970398dc04d756b49ea31a59807
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 10:19:57 +0800

    更新 deleteStructValues.m

--- toolbox/comparisons/highlight/matlab/+comparisons/+internal/+highlight/deleteStructValues.m
+++ toolbox/comparisons/highlight/matlab/+comparisons/+internal/+highlight/deleteStructValues.m
@@ -1,8 +1,5 @@
 function deleteStructValues(structVal)
 
-
-
-
     for fieldName=string(fieldnames(structVal))'
         delete(structVal.(fieldName))
         structVal=rmfield(structVal,fieldName);
