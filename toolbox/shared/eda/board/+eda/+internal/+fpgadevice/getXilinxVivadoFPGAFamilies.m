849d581422cccdc41dadf3e3108ead0789357542
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 11:07:46 +0800

    更新 getXilinxVivadoFPGAFamilies.m

--- toolbox/shared/eda/board/+eda/+internal/+fpgadevice/getXilinxVivadoFPGAFamilies.m
+++ toolbox/shared/eda/board/+eda/+internal/+fpgadevice/getXilinxVivadoFPGAFamilies.m
@@ -1,7 +1,5 @@
 function r=getXilinxVivadoFPGAFamilies
 
-
-
     r={'Virtex7','Kintex7','Artix7','Zynq','KintexU','VirtexU',...
     'Kintex UltraScale+','Virtex UltraScale+','Zynq UltraScale+','Spartan7','Zynq UltraScale+ RFSoC'};
 
