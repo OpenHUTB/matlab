c5a83441d13a376c95bce7a4f404a52ec361feb1
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 09:20:02 +0800

    更新 availableSharedMemory.m

--- toolbox/shared/coder/coder/+coder/+gpu/+internal/availableSharedMemory.m
+++ toolbox/shared/coder/coder/+coder/+gpu/+internal/availableSharedMemory.m
@@ -1,8 +1,6 @@
 function[limit]=availableSharedMemory
 %#codegen
 
-
-
     if coder.const(~coder.target('MATLAB'))
         coder.allowpcode('plain')
         limit=coder.const(feval('coder.gpu.getGpuSharedMemory',eml_option('CodegenBuildContext')));
