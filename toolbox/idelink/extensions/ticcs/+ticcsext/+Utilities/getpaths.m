22e691a9e7117be717c82e3a2641ed8e2d1f371c
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:44:18 +0800

    更新 getpaths.m

--- toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/getpaths.m
+++ toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/getpaths.m
@@ -1,8 +1,4 @@
 function[ccsdir,stubpath,proxypath]=getpaths(cc)
-
-
-
-
     ccsdir=cc.invokeIdeModule('GetAppDirectory');
     stubpath=cc.invokeIdeModule('GetStubPath');
     proxypath=cc.invokeIdeModule('GetProxyPath');
