5bebdcf7fcf705e5dd4d625df11b65864466210d
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:35:50 +0800

    更新 ccslinkpref.m

--- toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/ccslinkpref.m
+++ toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/ccslinkpref.m
@@ -1,8 +1,4 @@
 function prefVal=ccslinkpref(action,varargin)
-
-
-
-
     groupName=ticcsext.Utilities.LfCProperty('ccslinkpref-groupname');
     prefVal=linkfoundation.util.linkpref(action,groupName,varargin{:});
 
