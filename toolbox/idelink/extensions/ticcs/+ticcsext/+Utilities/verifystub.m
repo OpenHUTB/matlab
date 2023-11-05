5f386903d974f07905293fd134e0e1ba4a36592d
王海东 <wanghaidong@hutb.edu.cn>
2023-11-05 17:47:40 +0800

    更新 verifystub.m

--- toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/verifystub.m
+++ toolbox/idelink/extensions/ticcs/+ticcsext/+Utilities/verifystub.m
@@ -1,8 +1,4 @@
 function verified=verifystub(cc)
-
-
-
-
     reqdpath=ticcsext.Utilities.LfCProperty('inprocFile-current-client');
     stubpath=cc.invokeIdeModule('GetStubPath');
     verified=(strcmpi(reqdpath,stubpath)==1);
