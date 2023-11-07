dfe91638b6bb09dda87488704c9f13d99c25f4b6
王海东 <wanghaidong@hutb.edu.cn>
2023-11-07 21:54:44 +0800

    更新 schema.m

--- toolbox/mbc/mbcguitools/@xregGui/schema.m
+++ toolbox/mbc/mbcguitools/@xregGui/schema.m
@@ -1,12 +1,6 @@
 function schema
 
-
-
-
-
     pk=schema.package('xregGui');
-
-
 
     if isempty(findtype('xregGui.labelcontrol.SizeModeType'))
         schema.EnumType('xregGui.labelcontrol.SizeModeType',{'relative','absolute'},[0,1]);
@@ -23,12 +17,6 @@ function schema
     if isempty(findtype('xregGui.horz/vert'))
         schema.EnumType('xregGui.horz/vert',{'horizontal','vertical'},[0,1]);
     end
-
-
-
-
-
-
 
 
     p=schema.prop(pk,'TestMode','bool');
