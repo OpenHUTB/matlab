fa1297e8375256724850b40d4aab3fa8af493cf3
王海东 <wanghaidong@hutb.edu.cn>
2023-11-07 21:56:38 +0800

    更新 schema.m

--- toolbox/mbc/mbcguitools/@xregGui/@PointerRepository/schema.m
+++ toolbox/mbc/mbcguitools/@xregGui/@PointerRepository/schema.m
@@ -1,12 +1,7 @@
 function schema
 
-
-
-
-
     pk=findpackage('xregGui');
     c=schema.class(pk,'PointerRepository');
-
 
     p=schema.prop(c,'Pointers','MATLAB array');
     p.AccessFlags.PublicSet='off';
