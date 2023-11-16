db4d66b3640f930b431dd9bdd84267d8bc2646ea
王海东 <wanghaidong@hutb.edu.cn>
2023-11-16 10:24:13 +0800

    更新 getArchitectureFromCurrentContext.m

--- toolbox/sysarch/sysarch/+ZCStudio/getArchitectureFromCurrentContext.m
+++ toolbox/sysarch/sysarch/+ZCStudio/getArchitectureFromCurrentContext.m
@@ -1,8 +1,4 @@
 function topArchMdl=getArchitectureFromCurrentContext(cbinfo)
-
-
-
-
     hdlCurrentArchElem=SLStudio.Utils.getDiagramHandle(cbinfo);
     currentArchElem=systemcomposer.utils.getArchitecturePeer(hdlCurrentArchElem);
     topArchMdl=currentArchElem.getTopLevelArchitecture;
