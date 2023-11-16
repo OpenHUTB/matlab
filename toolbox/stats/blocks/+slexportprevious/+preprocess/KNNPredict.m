a8631694816881dd781fe818d7b91a88eaf58166
王海东 <wanghaidong@hutb.edu.cn>
2023-11-16 10:17:06 +0800

    更新 KNNPredict.m

--- toolbox/stats/blocks/+slexportprevious/+preprocess/KNNPredict.m
+++ toolbox/stats/blocks/+slexportprevious/+preprocess/KNNPredict.m
@@ -1,11 +1,6 @@
 function KNNPredict(obj)
 
-
-
-
     if isR2020aOrEarlier(obj.ver)
-
-
         blks=findBlocksWithMaskType(obj,'ClassificationKNN Predict');
         obj.replaceWithEmptySubsystem(blks);
     end
