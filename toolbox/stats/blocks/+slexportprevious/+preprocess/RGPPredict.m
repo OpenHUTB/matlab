b76a6a128543cb2e8d82d4124636c0c5666f6916
王海东 <wanghaidong@hutb.edu.cn>
2023-11-16 10:18:06 +0800

    更新 RGPPredict.m

--- toolbox/stats/blocks/+slexportprevious/+preprocess/RGPPredict.m
+++ toolbox/stats/blocks/+slexportprevious/+preprocess/RGPPredict.m
@@ -1,12 +1,6 @@
 function RGPPredict(obj)
 
-
-
-
     if isR2021bOrEarlier(obj.ver)
-
-
-
         blks=findBlocksWithMaskType(obj,'RegressionGP Predict');
         obj.replaceWithEmptySubsystem(blks);
     end
