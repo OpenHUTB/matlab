4e5b3c37ae0aa2e72c616830713e2e22a5542477
王海东 <wanghaidong@hutb.edu.cn>
2023-11-16 10:18:23 +0800

    更新 SVMPredict.m

--- toolbox/stats/blocks/+slexportprevious/+preprocess/SVMPredict.m
+++ toolbox/stats/blocks/+slexportprevious/+preprocess/SVMPredict.m
@@ -1,14 +1,8 @@
 function SVMPredict(obj)
 
-
-
-
     if isR2020aOrEarlier(obj.ver)
-
-
         blks=findBlocksWithMaskType(obj,'ClassificationSVM Predict');
         obj.replaceWithEmptySubsystem(blks);
-
         blks=findBlocksWithMaskType(obj,'RegressionSVM Predict');
         obj.replaceWithEmptySubsystem(blks);
     end
