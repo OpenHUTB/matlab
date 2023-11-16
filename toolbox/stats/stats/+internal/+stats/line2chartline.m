2f3e3e9d92561c71018afd898e24669f2073f795
王海东 <wanghaidong@hutb.edu.cn>
2023-11-16 10:15:47 +0800

    更新 line2chartline.m

--- toolbox/stats/stats/+internal/+stats/line2chartline.m
+++ toolbox/stats/stats/+internal/+stats/line2chartline.m
@@ -1,35 +1,11 @@
 function newh=line2chartline(h,varargin)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     narginchk(1,Inf);
     if isempty(h)||~ishghandle(h,'line')
         error(message('stats:line2chartline:InvalidLineHandle'));
     end
-
-
     cls=meta.class.fromName('matlab.graphics.primitive.Line');
     lineprops=cls.Properties;
-
 
     numprops=numel(lineprops);
     hiddenprops(numprops)=false;
@@ -38,22 +14,16 @@ function newh=line2chartline(h,varargin)
     end
     lineprops(hiddenprops)=[];
 
-
     numprops=numel(lineprops);
     fields=cell(numprops,1);
     for i=1:numprops
         fields{i}=lineprops{i}.Name;
     end
 
-
     [~,ia]=intersect(fields,{'BeingDeleted','Children','Type','UIContextMenu','Annotation'});
     fields(ia)=[];
-
-
-
     fields(~ismember(fields,properties(h)))=[];
     vals=get(h,fields);
-
 
     pvpairs=[fields(:).';vals(:).'];
     newh=matlab.graphics.chart.primitive.Line(pvpairs{:},varargin{:});
