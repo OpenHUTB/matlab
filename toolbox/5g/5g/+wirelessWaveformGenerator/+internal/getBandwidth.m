03b170c8ca5b17da31618d651dcdd64d4f847a24
王海东 <wanghaidong@hutb.edu.cn>
2023-11-17 15:03:07 +0800

    更新 getBandwidth.m

--- toolbox/5g/5g/+wirelessWaveformGenerator/+internal/getBandwidth.m
+++ toolbox/5g/5g/+wirelessWaveformGenerator/+internal/getBandwidth.m
@@ -1,16 +1,10 @@
 function bw=getBandwidth(FR,SCS,NRB)
 
-
-
-
-
     if strcmp(FR,'FR1')
         bws=[5,10,15,20,25,30,40,50,60,70,80,90,100];
     else
         bws=[50,100,200,400];
     end
-
-
     fh=str2func(['nr5g.internal.wavegen.get',FR,'BandwidthTable']);
     table=fh();
 
