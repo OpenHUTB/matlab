1bf36e7759b945503e1c44127f57883da70001cd
王海东 <wanghaidong@hutb.edu.cn>
2023-11-17 17:44:48 +0800

    更新 stateVariablesMustMatch.m

--- toolbox/aero/aircraft/+Aero/+FixedWing/@Coefficient/private/stateVariablesMustMatch.m
+++ toolbox/aero/aircraft/+Aero/+FixedWing/@Coefficient/private/stateVariablesMustMatch.m
@@ -1,9 +1,5 @@
 function stateVariablesMustMatch(obj)
 
-
-
-
-
     if size(obj.StateVariables,2)~=size(obj.Values,2)
         error(message("aero:FixedWing:StateVariableDimsMustMatchValues"));
     end
