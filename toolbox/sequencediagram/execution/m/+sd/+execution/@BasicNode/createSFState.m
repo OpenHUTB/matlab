e6f84de7be45c157fce334b47586e8d6beca14de
王海东 <wanghaidong@hutb.edu.cn>
2023-11-17 18:30:15 +0800

    更新 createSFState.m

--- toolbox/sequencediagram/execution/m/+sd/+execution/@BasicNode/createSFState.m
+++ toolbox/sequencediagram/execution/m/+sd/+execution/@BasicNode/createSFState.m
@@ -1,8 +1,5 @@
 function newState=createSFState(obj,parent)
 
-
-
-
     obj.cachedSFState=Stateflow.State(parent);
     newState=obj.cachedSFState;
     obj.cachedSFState.Position=[obj.x,obj.y,obj.width,obj.height];
