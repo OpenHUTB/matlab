9cce9a166a6e45c8933360db1f7cdf4698c69e9d
王海东 <wanghaidong@hutb.edu.cn>
2023-11-17 17:56:48 +0800

    更新 mustBeGettableState.m

--- toolbox/aero/aircraft/+Aero/+Aircraft/+internal/+validation/mustBeGettableState.m
+++ toolbox/aero/aircraft/+Aero/+Aircraft/+internal/+validation/mustBeGettableState.m
@@ -1,18 +1,8 @@
 function mustBeGettableState(stateName,ACState)
 
-
-
-
-
     stateName(stateName=="Zero")=[];
-
-
     mainProps=string(properties(ACState));
-
-
     envProps=string(properties(ACState.Environment));
-
-
     ctrlProps=[ACState.ControlStates.Properties];
 
     if~isempty(ctrlProps)
