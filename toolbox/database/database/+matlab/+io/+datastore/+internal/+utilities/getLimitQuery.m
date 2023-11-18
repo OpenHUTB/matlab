e1dced72212a66d87b6d5f545d0dc7c3e755755c
王海东 <wanghaidong@hutb.edu.cn>
2023-11-18 20:14:15 +0800

    更新 getLimitQuery.m

--- toolbox/database/database/+matlab/+io/+datastore/+internal/+utilities/getLimitQuery.m
+++ toolbox/database/database/+matlab/+io/+datastore/+internal/+utilities/getLimitQuery.m
@@ -1,30 +1,6 @@
 function limitquery=getLimitQuery(dbProductName)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     dbProdName=char(dbProductName);
-
     sqliteconn=sqlite(fullfile(toolboxdir('database'),'database','+matlab','+io','+datastore','+internal','+jdbcodbc','relationalDBInfo.db'));
     limitquery=fetch(sqliteconn,['SELECT LimitQuerySyntax from DBINFO where DatabaseProductName=''',dbProdName,''''],'DataReturnFormat','cellarray');
     close(sqliteconn);
