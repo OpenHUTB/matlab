f3f2b74b85d19d2d4c256011ea9a29b7bf3eeb98
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 15:23:21 +0800

    更新 simrfV2_sparams3d_to_1d.m

--- toolbox/simrf/simrfV2masks/private/simrfV2_sparams3d_to_1d.m
+++ toolbox/simrf/simrfV2masks/private/simrfV2_sparams3d_to_1d.m
@@ -1,20 +1,8 @@
 function s_row = simrfV2_sparams3d_to_1d( sparams )
 
-
-
-
-
-
-
-
-
-
-
-
 [ nport, ~, nfreqs ] = size( sparams );
 validateattributes( sparams, { 'numeric' }, { 'nonempty', 'size',  ...
 [ nport, nport, nfreqs ] }, mfilename, 'S-parameter data' )
-
 
 s_dc = sparams( :, :, 1 );
 validateattributes( abs( imag( s_dc ) ), { 'numeric' }, { '<=', 1e-10 },  ...
@@ -26,6 +14,4 @@ x = x( : ).';
 
 s_row = [ real( x ), imag( x ) ];
 end 
-% Decoded using De-pcode utility v1.2 from file /tmp/tmpbPcA1i.p.
-% Please follow local copyright laws when handling this file.
 
