daa74de7a70a286d1331906c37e75965682a8014
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 17:14:18 +0800

    更新 simrfV2resistor.m

--- toolbox/simrf/simrfV2masks/simrfV2resistor.m
+++ toolbox/simrf/simrfV2masks/simrfV2resistor.m
@@ -1,17 +1,10 @@
 function simrfV2resistor(block,action)
 
-
-
-
-
     top_sys=bdroot(block);
     if strcmpi(get_param(top_sys,'BlockDiagramType'),'library')&&...
         strcmpi(top_sys,'simrfV2elements')
         return
     end
-
-
-
 
     switch(action)
     case 'simrfInit'
@@ -21,8 +14,6 @@ function simrfV2resistor(block,action)
             '^(updating|initializing)$')
 
             MaskWSValues=simrfV2getblockmaskwsvalues(block);
-
-
             validateattributes(MaskWSValues.Resistance,{'numeric'},...
             {'nonempty','scalar','real','nonnegative','finite'},'',...
             'Resistance');
