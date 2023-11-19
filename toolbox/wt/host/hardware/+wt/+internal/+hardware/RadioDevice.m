334b9bd82e205985136fd05f6e0b0fd58f9ab589
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 22:01:45 +0800

    更新 RadioDevice.m

--- toolbox/wt/host/hardware/+wt/+internal/+hardware/RadioDevice.m
+++ toolbox/wt/host/hardware/+wt/+internal/+hardware/RadioDevice.m
@@ -1,31 +1,5 @@
 function varargout=RadioDevice(varargin)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     ds=wt.internal.hardware.DeviceStore;
     if nargin==0
         if nargout==0
@@ -40,21 +14,14 @@ function varargout=RadioDevice(varargin)
             devName=varargin{1};
             devParams=getDeviceParameters(ds,devName);
         elseif isstruct(varargin{1})
-
-
-
-
             devParams=varargin{1};
         else
             error(message('wt:radio:InvalidRadio'));
         end
-
-
         pm=wt.internal.hardware.PluginManager.getInstance();
         plugin=pm.getPlugin(devParams.Type);
 
         devPlugin=eval([plugin.Device,'(plugin)']);
-
 
         devPlugin.SFP0IPAddress=devParams.Network.DeviceIP0;
         devPlugin.SFP1IPAddress=devParams.Network.DeviceIP1;
@@ -65,7 +32,6 @@ function varargout=RadioDevice(varargin)
                 devPlugin.ManagementIPAddress=devPlugin.SFP0IPAddress;
             end
         end
-
 
         varargout{1}=devPlugin;
     end
