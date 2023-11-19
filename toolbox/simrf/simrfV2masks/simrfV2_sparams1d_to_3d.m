0050d7e45e7e5ec952423f6c0106b23b9e5c5de4
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 16:08:45 +0800

    更新 simrfV2_sparams1d_to_3d.m

--- toolbox/simrf/simrfV2masks/simrfV2_sparams1d_to_3d.m
+++ toolbox/simrf/simrfV2masks/simrfV2_sparams1d_to_3d.m
@@ -1,15 +1,10 @@
 function s=simrfV2_sparams1d_to_3d(sparams,nport,nfreqs)
 
-
-
-
     n=nport^2*nfreqs;
     validateattributes(sparams,{'numeric'},{'nonempty','size',...
     [1,2*n]},mfilename,'S-parameter data')
 
-
     s=sparams(1:n)+1j*sparams(n+1:end);
-
 
     s=reshape(s,nport,nport,nfreqs);
     s=permute(s,[2,1,3]);
