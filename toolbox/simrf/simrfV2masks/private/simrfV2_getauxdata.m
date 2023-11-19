126677605a70356b4499c570305e216718a0992d
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 15:18:28 +0800

    更新 simrfV2_getauxdata.m

--- toolbox/simrf/simrfV2masks/private/simrfV2_getauxdata.m
+++ toolbox/simrf/simrfV2masks/private/simrfV2_getauxdata.m
@@ -1,16 +1,10 @@
 function auxData=simrfV2_getauxdata(block)
 
-
-
-
-
     auxData=get_param([block,'/AuxData'],'UserData');
 
     if isempty(auxData)
         auxData.Version=2.0;
         auxData.Ckt=[];
-
-
         simrfV2Constants=simrfV2_constants();
         auxData.Plot=simrfV2Constants.Plot;
 
