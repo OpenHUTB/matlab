bacb09a9b71d3e0e26163d669fc59c5c8f747052
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 16:02:02 +0800

    更新 simrfV2DDGPreApply.m

--- toolbox/simrf/simrfV2masks/simrfV2DDGPreApply.m
+++ toolbox/simrf/simrfV2masks/simrfV2DDGPreApply.m
@@ -1,26 +1,6 @@
 function[status,errMsg]=simrfV2DDGPreApply(blockObj,dlg)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     errMsg=blockObj.validateChanges(dlg);
-
 
     if isempty(errMsg)
         [status,errMsg]=blockObj.preApplyCallback(dlg);
