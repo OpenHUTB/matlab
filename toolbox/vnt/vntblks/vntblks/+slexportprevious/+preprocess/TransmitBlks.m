4127048116518cc4ead475823f816974a00a69df
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 21:31:02 +0800

    更新 TransmitBlks.m

--- toolbox/vnt/vntblks/vntblks/+slexportprevious/+preprocess/TransmitBlks.m
+++ toolbox/vnt/vntblks/vntblks/+slexportprevious/+preprocess/TransmitBlks.m
@@ -1,13 +1,5 @@
 function TransmitBlks(obj)
 
-
-
-
-
-
-
-
-
     exportVer=obj.ver;
 
     if exportVer.isR2019bOrEarlier
@@ -15,19 +7,15 @@ function TransmitBlks(obj)
         blks=obj.findLibraryLinksTo('canlib/CAN Transmit');
 
 
-
-
         if~exportVer.isR2017bOrEarlier
             blks=[blks;obj.findLibraryLinksTo('canfdlib/CAN FD Transmit')];
         end
-
 
         paramName='EnableEventTransmit';
 
         for idx=1:length(blks)
 
             sid=get_param(blks{idx},'SID');
-
 
             obj.appendRule(slexportprevious.rulefactory.removeInstanceParameter(sprintf('<BlockType|"Reference"><SID|"%s">',sid),paramName,exportVer));
         end
