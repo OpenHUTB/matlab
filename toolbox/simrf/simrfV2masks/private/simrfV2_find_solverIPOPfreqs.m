f183093f5c854210715b0af84c05d403a63a88c3
王海东 <wanghaidong@hutb.edu.cn>
2023-11-19 15:15:55 +0800

    更新 simrfV2_find_solverIPOPfreqs.m

--- toolbox/simrf/simrfV2masks/private/simrfV2_find_solverIPOPfreqs.m
+++ toolbox/simrf/simrfV2masks/private/simrfV2_find_solverIPOPfreqs.m
@@ -1,41 +1,22 @@
 function[IPfreq,OPfreq,IPBlkNames]=simrfV2_find_solverIPOPfreqs(solverblock)
-
-
-
-
     connectedBlks=simrfV2_findConnected(solverblock);
-
-
     blks=findBlksMatchingRefSystem(connectedBlks,'simrfV2util1/Inport');
     [IPfreq,IPBlkNames]=simrfV2_read_freqs(solverblock,...
     blks,'CarrierFreq',[]);
 
-
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'simrfV2util1/Outport');
     OPfreq=simrfV2_read_freqs(solverblock,blks,'CarrierFreq',[]);
-
 
     blks=findBlksMatchingRefSystem(connectedBlks,...
     sprintf('simrfV2sources1/Continuous\nWave'));
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,...
     blks,'CarrierFreq',[])];
 
-
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'simrfV2sources1/Sinusoid');
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,...
     blks,'CarrierFreq',[])];
-
-
-
-
-
-
-
-
-
-
 
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'simrfV2systems/IQ Demodulator');
@@ -51,26 +32,21 @@ function[IPfreq,OPfreq,IPBlkNames]=simrfV2_find_solverIPOPfreqs(solverblock)
     'simrfV2systems/Demodulator');
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,blks,'LOFreq',[])];
 
-
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'simrfV2systems/Modulator');
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,blks,'LOFreq',[])];
-
 
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'rfBudgetAnalyzer_lib/Modulator');
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,blks,'LOFreq',[])];
 
-
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'rfBudgetAnalyzer_lib/IQ Modulator');
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,blks,'LOFreq',[])];
 
-
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'rfBudgetAnalyzer_lib/IQ Demodulator');
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,blks,'LOFreq',[])];
-
 
     blks=findBlksMatchingRefSystem(connectedBlks,...
     'simrfV2elements/Antenna');
@@ -81,7 +57,6 @@ function[IPfreq,OPfreq,IPBlkNames]=simrfV2_find_solverIPOPfreqs(solverblock)
     OPfreq=[OPfreq,simrfV2_read_freqs(solverblock,...
     blks,'CarrierFreqRad','OutputRadWave')];
 
-
     blks=findBlksMatchingRefSystem(connectedBlks,...
     sprintf('simrfV2elements/IMT\nMixer'));
     IPfreq=[IPfreq,simrfV2_read_freqs(solverblock,...
@@ -90,8 +65,6 @@ function[IPfreq,OPfreq,IPBlkNames]=simrfV2_find_solverIPOPfreqs(solverblock)
 end
 
 function blks=findBlksMatchingRefSystem(connectedBlks,refsystem)
-
-
 
     idx=strcmp(get_param(connectedBlks,'ReferenceBlock'),refsystem);
     blks=connectedBlks(idx);
