c0d338f0f845eeb7243d4b5a4c2f7aa984d1352e
王海东 <wanghaidong@hutb.edu.cn>
2023-11-20 22:08:10 +0800

    更新 showOptionDialogSync.m

--- toolbox/imaq/imaq/@iatbrowser/@OptionDialog/showOptionDialogSync.m
+++ toolbox/imaq/imaq/@iatbrowser/@OptionDialog/showOptionDialogSync.m
@@ -1,19 +1,4 @@
 function result=showOptionDialogSync(~,parentFrame,optionDialogParameterName)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     params=eval(['com.mathworks.toolbox.imaq.browser.dialogs.OptionDialogParameters.',optionDialogParameterName]);
     dialog=com.mathworks.toolbox.imaq.browser.dialogs.SyncOptionDialog(parentFrame,params);
 
