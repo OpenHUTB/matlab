d93333300bdaf3a039deece44f07132fbfd59fc5
王海东 <wanghaidong@hutb.edu.cn>
2023-11-20 22:01:48 +0800

    更新 schema.m

--- toolbox/imaq/imaq/@iatbrowser/@InfoPanelController/schema.m
+++ toolbox/imaq/imaq/@iatbrowser/@InfoPanelController/schema.m
@@ -1,31 +1,21 @@
 function schema
 
-
-
-
-
     pk=findpackage('iatbrowser');
 
     className='InfoPanelController';
     controllerClass=schema.class(pk,className);
     controllerClass.JavaInterfaces={[pk.JavaPackage,'.',className]};
-
-
     javaPeer=schema.prop(controllerClass,'javaPeer','handle vector');
     javaPeer.AccessFlags.PublicSet='off';
     javaPeer.AccessFlags.PublicGet='on';
-
-
 
     aProp=schema.prop(controllerClass,'treeNodeListeners','MATLAB array');
     aProp.AccessFlags.PublicSet='on';
     aProp.AccessFlags.PublicGet='on';
 
-
     aProp=schema.prop(controllerClass,'sourcePropertyChangedListener','MATLAB array');
     aProp.AccessFlags.PublicSet='on';
     aProp.AccessFlags.PublicGet='on';
-
 
     aProp=schema.prop(controllerClass,'videoinputPropertyChangedListener','MATLAB array');
     aProp.AccessFlags.PublicSet='on';
