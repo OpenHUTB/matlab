37e7a8be86a49e3ee3df9df914d37be469d691dd
王海东 <wanghaidong@hutb.edu.cn>
2023-11-20 22:37:35 +0800

    更新 stopPreview.m

--- toolbox/imaq/imaq/@iatbrowser/@PreviewPanelController/stopPreview.m
+++ toolbox/imaq/imaq/@iatbrowser/@PreviewPanelController/stopPreview.m
@@ -1,9 +1,4 @@
 function stopPreview(this,clearWindow)
-
-
-
-
-
     this.prevPanel.stopPreview(iatbrowser.Browser().currentVideoinputObject,clearWindow);
     send(this,'PreviewStopping',[]);
 end
