cd1930c69cfdd383007ecfddacc25403f0a64999
王海东 <wanghaidong@hutb.edu.cn>
2023-11-20 21:27:14 +0800

    更新 autoblksunitconv.m

--- toolbox/autoblks_utils/autoblksunitconv.m
+++ toolbox/autoblks_utils/autoblksunitconv.m
@@ -1,10 +1,5 @@
 function DestinationData=autoblksunitconv(SourceData,SourceDataUnits,DestinationDataUnits)
 
-
-
-
-
-
     Info=Simulink.UnitUtils.getConversionInfo('',SourceDataUnits,DestinationDataUnits);
     if~Info.isInverted
         DestinationData=SourceData*Info.scaling+Info.offset;
