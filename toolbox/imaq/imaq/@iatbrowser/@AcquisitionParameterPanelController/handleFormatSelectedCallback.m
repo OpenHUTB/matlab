59ba87df0cbd71951706a525ecf74b766ca1edcd
王海东 <wanghaidong@hutb.edu.cn>
2023-11-20 21:35:17 +0800

    更新 handleFormatSelectedCallback.m

--- toolbox/imaq/imaq/@iatbrowser/@AcquisitionParameterPanelController/handleFormatSelectedCallback.m
+++ toolbox/imaq/imaq/@iatbrowser/@AcquisitionParameterPanelController/handleFormatSelectedCallback.m
@@ -1,9 +1,6 @@
 function handleFormatSelectedCallback(~,~,event)
 
 
-
-
-
     browser=iatbrowser.Browser;
     deviceNode=browser.treePanel.currentNode;
     formatNode=deviceNode.getNode(char(event.javaEvent.selectedFormat));
