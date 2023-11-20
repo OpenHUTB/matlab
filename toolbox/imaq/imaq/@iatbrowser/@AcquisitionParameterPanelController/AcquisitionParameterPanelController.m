81319fc0d5ac5913db7aca42af904937c4c22860
王海东 <wanghaidong@hutb.edu.cn>
2023-11-20 21:32:08 +0800

    更新 AcquisitionParameterPanelController.m

--- toolbox/imaq/imaq/@iatbrowser/@AcquisitionParameterPanelController/AcquisitionParameterPanelController.m
+++ toolbox/imaq/imaq/@iatbrowser/@AcquisitionParameterPanelController/AcquisitionParameterPanelController.m
@@ -1,14 +1,4 @@
 function this=AcquisitionParameterPanelController(rootNode,prevPanel)
 
-
-
-
-
-
-
-
-
-
-
     this=iatbrowser.AcquisitionParameterPanelController;
     initialize(this,rootNode,prevPanel);
