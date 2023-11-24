ea6a54e838be633677f78c8eac86e715d5ffc0f3
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 09:45:17 +0800

    更新 getMetaData.m

--- toolbox/alm/project_services/ml/+alm/+internal/+project/getMetaData.m
+++ toolbox/alm/project_services/ml/+alm/+internal/+project/getMetaData.m
@@ -1,24 +1,14 @@
 function value=getMetaData(rootFolder,key)
 
-
-
     value='';
 
     if~isvarname(key)
         return;
     end
 
-
-
     appKey="ALM";
-
-
     proj=alm.internal.project.getProject(rootFolder);
-
-
     fmgr=matlab.internal.project.metadata.FileMetadataManager(proj,appKey);
-
-
 
     try
         metaData=fmgr.getMetadata(proj.RootFolder);
