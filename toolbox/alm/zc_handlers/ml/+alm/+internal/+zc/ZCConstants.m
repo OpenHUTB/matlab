8017b446bcff7655e13f3f56e8b133bb753049bd
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 09:13:39 +0800

    更新 ZCConstants.m

--- toolbox/alm/zc_handlers/ml/+alm/+internal/+zc/ZCConstants.m
+++ toolbox/alm/zc_handlers/ml/+alm/+internal/+zc/ZCConstants.m
@@ -1,15 +1,9 @@
 classdef ZCConstants
 
-
-
-
     properties(Constant)
         ZC_FILE="zc_file";
         ZC_BLOCK_DIAGRAM="zc_block_diagram";
         ZC_COMPONENT="zc_component";
-
-
-
         SIMULINK_SERVICE_ID="alm::services::SLBlockDiagramHandlerService";
         SL_BLOCK_DIAGRAM="sl_block_diagram";
         SL_SUBSYSTEM="sl_subsystem";
