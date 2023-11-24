3c3c1d53207e258ec79ac196489bf189e21f6a14
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 09:36:08 +0800

    更新 closeRequirementFile.m

--- toolbox/alm/slreq_handlers/ml/+alm/+internal/+slreq/closeRequirementFile.m
+++ toolbox/alm/slreq_handlers/ml/+alm/+internal/+slreq/closeRequirementFile.m
@@ -1,11 +1,4 @@
 function closeRequirementFile(absoluteReqFilePath)
-
-
-
-
-
-
-
     [~,name,ext]=fileparts(absoluteReqFilePath);
     slreq.utils.slproject.discardRequirementsFile([name,ext],fullfile(absoluteReqFilePath));
 end
