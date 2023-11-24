41586e28bac36e3bf674c5106dcf5b1eb850088f
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 21:57:03 +0800

    更新 customizationDO178b.m

--- toolbox/slcheck/do178b/+internalcustomization/customizationDO178b.m
+++ toolbox/slcheck/do178b/+internalcustomization/customizationDO178b.m
@@ -1,21 +1,11 @@
 function customizationDO178b()
 
-
-
-
-
-
-
-
-
     cm=DAStudio.CustomizationManager;
 
     cm.addModelAdvisorCheckFcn(@defineDO178bModelAdvisorChecks);
 
     cm.addModelAdvisorTaskFcn(@defineDO178ModelAdvisorTasks);
 
-
     cm.addModelAdvisorTaskFcn(@defineDO254ModelAdvisorTasks);
-
 
 end
