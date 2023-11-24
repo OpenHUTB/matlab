a709cc4befcbb4dbc6bae4fbfb54136214267346
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 09:38:29 +0800

    更新 openReqLinkFile.m

--- toolbox/alm/slreq_handlers/ml/+alm/+internal/+slreq/openReqLinkFile.m
+++ toolbox/alm/slreq_handlers/ml/+alm/+internal/+slreq/openReqLinkFile.m
@@ -1,8 +1,5 @@
 function openReqLinkFile(absoluteFileAddress)
 
-
-
-
     slreq.editor;
     slreq.load(fullfile(absoluteFileAddress));
     ls=slreq.utils.getLinkSet(fullfile(absoluteFileAddress));
