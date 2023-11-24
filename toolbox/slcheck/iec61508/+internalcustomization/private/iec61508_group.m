25f81727b17edc7827d28e64394136af252dbc15
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 22:16:00 +0800

    更新 iec61508_group.m

--- toolbox/slcheck/iec61508/+internalcustomization/private/iec61508_group.m
+++ toolbox/slcheck/iec61508/+internalcustomization/private/iec61508_group.m
@@ -1,7 +1,5 @@
 function str=iec61508_group
 
-
-
     str=['Simulink Check|'...
     ,DAStudio.message('Simulink:tools:ModelingStandards'),'|'...
     ,DAStudio.message('ModelAdvisor:iec61508:IEC61508Checks')];
