5736cb3cf9872aa6b96371ba9958b15c49360b75
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 22:04:42 +0800

    更新 do178b_group.m

--- toolbox/slcheck/highintegrity/+internalcustomization/private/do178b_group.m
+++ toolbox/slcheck/highintegrity/+internalcustomization/private/do178b_group.m
@@ -1,7 +1,5 @@
 function str=do178b_group
 
-
-
     str=['Simulink Check|'...
     ,DAStudio.message('Simulink:tools:ModelingStandards'),'|'...
     ,DAStudio.message('ModelAdvisor:do178b:DO178BChecks')];
