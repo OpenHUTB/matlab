8948fdce49479ac19c4f5bf6d6f2ba732eb8eb07
王海东 <wanghaidong@hutb.edu.cn>
2023-11-24 09:35:49 +0800

    更新 closeLinkFile.m

--- toolbox/alm/slreq_handlers/ml/+alm/+internal/+slreq/closeLinkFile.m
+++ toolbox/alm/slreq_handlers/ml/+alm/+internal/+slreq/closeLinkFile.m
@@ -1,11 +1,4 @@
 function closeLinkFile(absoluteLinkFilePath)
-
-
-
-
-
-
-
     [~,name,ext]=fileparts(absoluteLinkFilePath);
     slreq.utils.slproject.discardRequirementsFile([name,ext],fullfile(absoluteLinkFilePath));
 
