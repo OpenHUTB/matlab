ce549c8a788b276da5adb71d2879fcab31e36fb4
王海东 <wanghaidong@hutb.edu.cn>
2023-11-26 17:13:56 +0800

    更新 EchoUdpServer.m

--- toolbox/shared/networklib/+matlabshared/+network/+internal/EchoUdpServer.m
+++ toolbox/shared/networklib/+matlabshared/+network/+internal/EchoUdpServer.m
@@ -1,44 +1,29 @@
 classdef EchoUdpServer<matlabshared.network.internal.IDestroyable
 
-
-
-
-
     properties(Access=private)
 Channel
     end
+
 
     properties
 PortNumber
     end
 
+
     properties(Constant)
-
-
         EchoServerID=0
-
-
         ProtocolType=1
         RemoteHost='::1'
         LocalHost='::'
-
-
         RemotePort=9090
-
-
         Mode=1
-
-
         SocketType=0
     end
+
+
     methods
         function obj=EchoUdpServer(port)
-
-
-
             obj.PortNumber=port;
-
-
             devicePlugin=fullfile(toolboxdir(fullfile(...
             'shared','networklib','bin',computer('arch'))),'udpdevice');
             converterPlugin=fullfile(toolboxdir(fullfile(...
@@ -53,34 +38,24 @@ PortNumber
             options.Mode=obj.Mode;
             options.SocketType=obj.SocketType;
             options.EnablePortSharing=true;
-
-
             obj.Channel=matlabshared.asyncio.internal.Channel(devicePlugin,...
             converterPlugin,...
             Options=options);
 
-
             obj.Channel.InputStream.Timeout=Inf;
             obj.Channel.OutputStream.Timeout=Inf;
-
-
-
-
 
             obj.Channel.DataEventsDisabled=true;
             obj.Channel.open(options);
         end
 
+
         function delete(obj)
-
-
             destroy(obj);
         end
 
+
         function destroy(obj)
-
-
-
             if~isempty(obj.Channel)
                 obj.Channel.OutputStream.drain();
                 obj.Channel.close();
