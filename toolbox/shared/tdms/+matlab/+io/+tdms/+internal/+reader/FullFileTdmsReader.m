feb5df95bbed7d1ae79e8da3bf53e629630bc9d3
王海东 <wanghaidong@hutb.edu.cn>
2023-11-26 14:56:42 +0800

    更新 FullFileTdmsReader.m

--- toolbox/shared/tdms/+matlab/+io/+tdms/+internal/+reader/FullFileTdmsReader.m
+++ toolbox/shared/tdms/+matlab/+io/+tdms/+internal/+reader/FullFileTdmsReader.m
@@ -1,8 +1,5 @@
 classdef FullFileTdmsReader<matlab.io.tdms.internal.reader.FileOffsetTdmsReader
 
-
-
-
     methods
         function obj=FullFileTdmsReader(location,options,selectedChannelGroup,selectedChannels)
             obj@matlab.io.tdms.internal.reader.FileOffsetTdmsReader(location,options,selectedChannelGroup,selectedChannels,0);
