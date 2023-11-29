7ad1d44b98c1a547027571aac97ef21664d32aad
王海东 <wanghaidong@hutb.edu.cn>
2023-11-30 07:25:48 +0800

    更新 HeightmapTileReader.m

--- toolbox/shared/terrain/+terrain/+internal/HeightmapTileReader.m
+++ toolbox/shared/terrain/+terrain/+internal/HeightmapTileReader.m
@@ -1,8 +1,5 @@
 classdef(Sealed,Hidden)HeightmapTileReader<terrain.internal.TerrainTileReader
 
-
-
-
     properties
 ZoomLevel
     end
@@ -79,18 +76,8 @@ UsingConnector
             latsq=latsq(:);
             lonsq=lonsq(:);
 
-
-
-
-
-
-
             ny=int32(180/reader.TileLatitudeExtents);
             nx=int32(360/reader.TileLongitudeExtents);
-
-
-
-
             w=reader.TileLongitudeExtents;
             tileX=int32(floor((lonsq+180)/w));
             tileX=min(tileX,nx-1);
@@ -99,32 +86,21 @@ UsingConnector
             tileY=int32(floor((latsq+90)/h));
             tileY=min(tileY,ny-1);
 
-
-
             [tileCoordinates,~,ind]=unique([tileX,tileY],'rows');
         end
 
         function[terrainData,latvs,lonvs]=readTiles(reader,tileCoordinates)
 
-
-
             zoomLevel=reader.ZoomLevel;
             tileX=tileCoordinates(:,1);
             tileY=tileCoordinates(:,2);
             tileTokens="z"+zoomLevel+"x"+tileX+"y"+tileY;
-
-
             ext=reader.TerrainTileExtension;
             tdir=tempname;
             mkdir(tdir);
             cleanupTempDir=onCleanup(@()deleteFolder(tdir));
             unzippedFileNames=fullfile(tdir,tileTokens);
             fullFileNames=unzippedFileNames+ext;
-
-
-
-
-
 
             timeout=30;
 
@@ -197,12 +173,8 @@ UsingConnector
                 waitForFilesExist(tdir,fullFileNames,timeout);
             end
 
-
-
             gunzip(tdir);
             waitForFilesExist(tdir,unzippedFileNames,timeout);
-
-
             numTiles=size(unzippedFileNames,1);
             terrainData=cell(numTiles,1);
             latvs=cell(numTiles,1);
@@ -233,17 +205,12 @@ UsingConnector
             tileX=double(tileCoordinates(:,1));
             tileY=double(tileCoordinates(:,2));
 
-
-
             w=reader.TileLongitudeExtents;
             h=reader.TileLatitudeExtents;
             tileSouth=-90+tileY*h;
             tileNorth=tileSouth+h;
             tileWest=-180+tileX*w;
             tileEast=tileWest+w;
-
-
-
             latv=linspace(tileSouth,tileNorth,tileSize);
             lonv=linspace(tileWest,tileEast,tileSize);
         end
@@ -252,14 +219,9 @@ UsingConnector
     methods(Static)
         function[heightData,childrenByte]=readHeightmapTile(file)
 
-
-
-
             fid=fopen(file);
             cesiumTerrainData=fread(fid,inf,'uint16',0,'l');
             fclose(fid);
-
-
             childrenByte=cesiumTerrainData(end);
             cesiumTerrainData(end)=[];
 
@@ -272,13 +234,9 @@ UsingConnector
         function writeHeightmapTile(file,terrainData,childrenByte)
 
 
-
-
             fileData=(terrainData+1000)/.2;
             fileData=fileData';
             fileData=uint16(fileData(:));
-
-
             fileData=[fileData;childrenByte];
 
 
@@ -289,17 +247,11 @@ UsingConnector
 
         function[nx,ny]=numXYTiles(zoomLevel)
 
-
-
-
             nx=2^(zoomLevel+1);
             ny=2^zoomLevel;
         end
 
         function[w,h]=tileExtents(zoomLevel)
-
-
-
             [nx,ny]=terrain.internal.HeightmapTileReader.numXYTiles(zoomLevel);
             w=360/nx;
             h=180/ny;
@@ -324,9 +276,6 @@ function[URLs,fileNames]=getMissingFileNames(downloadDir,desiredfiles,...
 
     filesDownloaded=dir(downloadDir);
     fileIndsNeeded=~ismember(desiredfiles,fullfile(downloadDir,string({filesDownloaded.name})));
-
-
-
 
     if(usingconnector)
         urlsNeeded=arrayfun(@(x)string(connector.getUrl(x)),baseurls);
