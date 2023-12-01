d4fb5666feaee864fcca8f540554d0d7190582ab
王海东 <wanghaidong@hutb.edu.cn>
2023-12-01 23:43:02 +0800

    更新 PreferencesConfig.m

--- toolbox/multisim/+simmanager/+designview/+internal/PreferencesConfig.m
+++ toolbox/multisim/+simmanager/+designview/+internal/PreferencesConfig.m
@@ -1,18 +1,12 @@
 classdef PreferencesConfig<handle
 
 
-
-
-
-
-
     properties
         Group='Simulink'
         Preference='SimManagerCustomPlots'
         JSONSerializer=mf.zero.io.JSONSerializer
         JSONParser=mf.zero.io.JSONParser
     end
-
 
 
 end
