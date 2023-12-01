9a8c0063c250004d430e2efb0008096682343100
王海东 <wanghaidong@hutb.edu.cn>
2023-12-01 23:46:57 +0800

    更新 getSurfDataFromScatteredData.m

--- toolbox/multisim/+simmanager/+designview/+internal/getSurfDataFromScatteredData.m
+++ toolbox/multisim/+simmanager/+designview/+internal/getSurfDataFromScatteredData.m
@@ -1,9 +1,6 @@
 function[X,Y,Z]=getSurfDataFromScatteredData(scatterX,scatterY,scatterZ)
 
 
-
-
-
     finiteDataIndices=isfinite(scatterX)&isfinite(scatterY)&isfinite(scatterZ);
     numFinitePoints=sum(finiteDataIndices);
 
@@ -14,27 +11,21 @@ function[X,Y,Z]=getSurfDataFromScatteredData(scatterX,scatterY,scatterZ)
         Z=[];
     else
 
-
-
         numPointsAlongEachAxis=2*ceil(sqrt(numFinitePoints));
 
         scatterX=scatterX(finiteDataIndices);
         scatterY=scatterY(finiteDataIndices);
         scatterZ=scatterZ(finiteDataIndices);
 
-
         xlin=linspace(min(scatterX),max(scatterX),numPointsAlongEachAxis);
         ylin=linspace(min(scatterY),max(scatterY),numPointsAlongEachAxis);
         [X,Y]=meshgrid(xlin,ylin);
-
-
 
 
         origState=warning('off','all');
         oc=onCleanup(@()warning(origState));
 
         Z=griddata(scatterX,scatterY,scatterZ,X,Y,'cubic');
-
 
 
         if isempty(Z)
