209d6e59c7c744f98d316fbe90c6fc6fb07b95e6
王海东 <wanghaidong@hutb.edu.cn>
2023-12-03 15:04:45 +0800

    更新 addFunctionHandle.m

--- toolbox/coder/targetreg/+coder/+targetreg/+internal/@TargetRegistry/addFunctionHandle.m
+++ toolbox/coder/targetreg/+coder/+targetreg/+internal/@TargetRegistry/addFunctionHandle.m
@@ -1,19 +1,5 @@
 function addFunctionHandle(h,fh)
-
-
-
-
-
-
-
-
-
-
-
-
-
     validateattributes(fh,'function_handle',{})
-
 
     if~any(cellfun(@(f)isequal(f,fh),h.TargetInfoFcns))
         h.TargetInfoFcns{end+1}=fh;
