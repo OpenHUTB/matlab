0dda4ace776e46ef7d2441e9bb93f294de82744b
王海东 <wanghaidong@hutb.edu.cn>
2023-12-03 15:08:14 +0800

    更新 getRequiredInterfaceSupportFlags.m

--- toolbox/coder/wizard/+coder/+internal/getRequiredInterfaceSupportFlags.m
+++ toolbox/coder/wizard/+coder/+internal/getRequiredInterfaceSupportFlags.m
@@ -1,17 +1,6 @@
 function out=getRequiredInterfaceSupportFlags(model)
-
-
-
-
-
-
-
-
-
-
     modelInterface=get_param(model,'Object');
     assert(isa(modelInterface,'Simulink.BlockDiagram'));
-
 
     sess=Simulink.CMI.EIAdapter(Simulink.EngineInterfaceVal.embeddedCoder);
     oc=onCleanup(@()Simulink.CMI.EIAdapter(sess.oldFeatureValue));
