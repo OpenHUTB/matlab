11d116453a67b93dd736100405f761fe1543dc49
王海东 <wanghaidong@hutb.edu.cn>
2023-12-04 18:56:28 +0800

    更新 protectCode.m

--- toolbox/shared/toolstrip_coder_app/plugin/m/+coder/+internal/+toolstrip/+callback/protectCode.m
+++ toolbox/shared/toolstrip_coder_app/plugin/m/+coder/+internal/+toolstrip/+callback/protectCode.m
@@ -1,8 +1,4 @@
 function protectCode(userdata,cbinfo)
-
-
-
-
     if~coder.internal.toolstrip.license.isMATLABCoder
 
         DAStudio.error('SimulinkCoderApp:toolstrip:licenseForActionNotFound','MATLAB Coder');
