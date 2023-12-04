9c92c79bba38a104294dff9390b472643eb7d2fd
王海东 <wanghaidong@hutb.edu.cn>
2023-12-04 18:53:56 +0800

    更新 launchCodePerspective.m

--- toolbox/shared/toolstrip_coder_app/plugin/m/+coder/+internal/+toolstrip/+callback/launchCodePerspective.m
+++ toolbox/shared/toolstrip_coder_app/plugin/m/+coder/+internal/+toolstrip/+callback/launchCodePerspective.m
@@ -1,16 +1,11 @@
 function launchCodePerspective(cbinfo)
 
-
-
-
-
     if~coder.internal.toolstrip.license.isSimulinkCoder&&~coder.internal.toolstrip.license.isEmbeddedCoder
 
         return;
     end
 
     mdlH=cbinfo.model.Handle;
-
 
     if~simulinkcoder.internal.CodePerspective.isInPerspective(mdlH)
         cp=simulinkcoder.internal.CodePerspective.getInstance;
