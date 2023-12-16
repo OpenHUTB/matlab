0eecbf902210bd25f25ec5ecb134842deb867dc7
王海东 <wanghaidong@hutb.edu.cn>
2023-12-16 14:36:07 +0800

    更新 actx_demopreload.m

--- toolbox/shared/reqmgt/+rmicom/actx_demopreload.m
+++ toolbox/shared/reqmgt/+rmicom/actx_demopreload.m
@@ -1,9 +1,5 @@
 function actx_demopreload
 
-
-
-
-
     if ispc&&~rmicom.actx_installed('SLRefButton')
         msg=getString(message('Slvnv:reqmgt:actx_installed:NavFromWordWontWork'));
         msgbox(msg,getString(message('Slvnv:reqmgt:actx_installed:RequiredActiveXNotRegistered')));
