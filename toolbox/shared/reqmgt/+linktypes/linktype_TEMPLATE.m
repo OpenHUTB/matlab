7801531ab04f06c2d2102fd01f3f8b7c13b740f6
王海东 <wanghaidong@hutb.edu.cn>
2023-12-16 14:13:23 +0800

    更新 linktype_TEMPLATE.m

--- toolbox/shared/reqmgt/+linktypes/linktype_TEMPLATE.m
+++ toolbox/shared/reqmgt/+linktypes/linktype_TEMPLATE.m
@@ -1,100 +1,18 @@
 function linkType=linktype_TEMPLATE
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     linkType=ReqMgr.LinkType;
-
-
-
-
-
-
-
 
     linkType.Registration=mfilename;
 
-
     linkType.Label='MY CUSTOM DOC TYPE';
-
-
 
     linkType.IsFile=1;
 
-
-
-
-
-
     linkType.Extensions={};
-
-
-
-
-
-
-
-
-
-
-
 
     linkType.LocDelimiters='@#?';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     linkType.NavigateFcn=@NavigateFcn;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 end
@@ -104,36 +22,18 @@ end
 function NavigateFcn(document,location)
     disp([mfilename,': Navigating to ',location,' in ',document]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
 
+
 function[LABELS,DEPTHS,LOCATIONS]=ContentsFcn(DOCUMENT)%#ok<*DEFNU>
-
-
-
-
-
 
     LABELS={['Dummy Header in',DOCUMENT];'Dummy sub-header'};
     DEPTHS=[0;1];
     LOCATIONS={'dummy_location_1';'dummy_location_2'};
 end
 
+
 function DOCUMENT=BrowseFcn()
-
-
 
     [filename,pathname]=uigetfile({...
     '.ext1','My Custom Doc sub-type1';...
@@ -142,8 +42,8 @@ function DOCUMENT=BrowseFcn()
     DOCUMENT=fullfile(pathname,filename);
 end
 
-function URL=CreateURLFcn(DOCPATH,DOCURL,LOCATION)
 
+function URL=CreateURLFcn(DOCPATH,DOCURL,LOCATION)
 
     if~isempty(DOCURL)
         URL=[DOCURL,'#',LOCATION(2:end)];
@@ -152,83 +52,57 @@ function URL=CreateURLFcn(DOCPATH,DOCURL,LOCATION)
     end
 end
 
+
 function SUCCESS=IsValidDocFcn(DOCUMENT,REFPATH)
-
-
-
     SUCCESS=exist(DOCUMENT,'file')==2||...
     exist(fullfile(REFPATH,DOCUMENT),'file')==2;
 end
 
+
 function SUCCESS=IsValidIdFcn(DOCUMENT,LOCATION)
 
-
-
-
     SUCCESS=true;
 end
+
 
 function[SUCCESS,DOC_DESCRIPTION]=IsValidDescFcn(DOCUMENT,LOCATION,LINK_DESCRIPTION)
-
-
-
-
-
-
     SUCCESS=true;
 end
 
+
 function[DEPTHS,ITEMS]=DetailsFcn(DOCUMENT,LOCATION,LEVEL)
-
-
-
-
-
-
-
-
     ITEMS={'DetailsFcn not implemented','Need to query the document'};
     DEPTHS=[0,1];
 end
 
+
 function REQ=SelectionLinkFcn(OBJECT,MAKE2WAY)
-
-
-
 
     REQ=rmi('createempty');
     REQ.description='SelectionLinkFcn not implemented';
     REQ.doc='CurrentDoc';
 end
 
+
 function HTML=HtmlViewFcn(DOC,LOCATION,OPT_DETAILS_LEVEL)
 
     HTML='<p><b>Bold parag</b></p><p><i>Italic parag</i></p>';
 end
+
 
 function VALUE=GetAttributeFcn(DOC,LOCATION,ATTRIBUTENAME)
 
     VALUE=app.API.getAttribute(DOC,LOCATION,ATTRIBUTENAME);
 end
 
-function text=TextViewFcn(DOC,LOCATION)
 
+function text=TextViewFcn(DOC,LOCATION)
 
     text=app.API.getText(DOC,LOCATION);
 end
 
+
 function RESULT=GetResultFcn(LINK)
-
-
-
-
-
-
-
-
-
-
-
     RESULT.status=slreq.verification.Status.Pass;
     RESULT.timestamp=datetime(now,'ConvertFrom','datenum','TimeZone','Local');
 end
