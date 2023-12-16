5744651d2b820129a72ed0331d6fde52ce1b5706
王海东 <wanghaidong@hutb.edu.cn>
2023-12-16 14:11:03 +0800

    更新 isVersionedResource.m

--- toolbox/shared/reqmgt/+linktypes/isVersionedResource.m
+++ toolbox/shared/reqmgt/+linktypes/isVersionedResource.m
@@ -1,14 +1,9 @@
 function tf=isVersionedResource(regName)
 
-
-
-
-
     if~rmi.isInstalled()
         tf=false;
         return;
     end
-
 
     tf=any(strcmp(regName,{'linktype_rmi_doors','doors'}));
 
