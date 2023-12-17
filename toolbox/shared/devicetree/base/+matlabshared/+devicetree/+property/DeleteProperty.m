6a603ae082d7c61ce4275db083829b3656855cfd
王海东 <wanghaidong@hutb.edu.cn>
2023-12-17 20:38:00 +0800

    更新 DeleteProperty.m

--- toolbox/shared/devicetree/base/+matlabshared/+devicetree/+property/DeleteProperty.m
+++ toolbox/shared/devicetree/base/+matlabshared/+devicetree/+property/DeleteProperty.m
@@ -1,6 +1,5 @@
 classdef DeleteProperty<matlabshared.devicetree.property.PropertyBase
 
-
     methods
         function obj=DeleteProperty(name)
             obj=obj@matlabshared.devicetree.property.PropertyBase(name);
@@ -10,7 +9,6 @@ classdef DeleteProperty<matlabshared.devicetree.property.PropertyBase
 
     methods(Access=protected)
         function printBody(obj,hDTPrinter,~,~)
-
 
             hDTPrinter.addLine("/delete-property/ "+obj.Name+";");
         end
