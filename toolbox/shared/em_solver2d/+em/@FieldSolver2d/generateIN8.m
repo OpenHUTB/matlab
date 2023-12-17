cf533038384c8703aca48f7718230008353a6ef7
王海东 <wanghaidong@hutb.edu.cn>
2023-12-17 19:45:26 +0800

    更新 generateIN8.m

--- toolbox/shared/em_solver2d/+em/@FieldSolver2d/generateIN8.m
+++ toolbox/shared/em_solver2d/+em/@FieldSolver2d/generateIN8.m
@@ -2,51 +2,35 @@ function fileName=generateIN8(obj)
 
     fileName='linpar.in8';
 
-
     fileID=fopen(fileName,'w');
-
 
     fprintf(fileID,'8\n');
 
-
     fprintf(fileID,'3\n');
-
-
     fprintf(fileID,'%d\n',obj.numSubLayer);
-
 
     fprintf(fileID,'%d\n',obj.codeSub);
 
-
     fprintf(fileID,'         0.0000000\n');
-
 
     for iSub=1:obj.numSubLayer
 
         fprintf(fileID,'         %.7f\n',obj.thickSub(iSub));
 
-
         fprintf(fileID,'         %.7f\n',obj.epsilonRSub(iSub));
 
-
         fprintf(fileID,'         %.7f\n',obj.lossTangentSub(iSub));
-
-
         fprintf(fileID,'%d\n',obj.numTrace(iSub));
-
 
         for iTrace=1:obj.numTrace(iSub)
             fprintf(fileID,'         %.7f\n',obj.widthTrace{iSub}(iTrace));
         end
 
-
         for iTrace=1:obj.numTrace(iSub)
             fprintf(fileID,'         %.7f\n',obj.thickTrace{iSub}(iTrace));
         end
 
-
         fprintf(fileID,'         %.7f\n',obj.offsetTrace(iSub));
-
 
         for iSep=1:obj.numTrace(iSub)-1
             fprintf(fileID,'         %.7f\n',obj.separationTrace{iSub}(iSep));
@@ -57,18 +41,14 @@ function fileName=generateIN8(obj)
 
         fprintf(fileID,'         %.7f\n',obj.thickCover);
 
-
         fprintf(fileID,'         %.7f\n',obj.epsilonRCover);
-
 
         fprintf(fileID,'         %.7f\n',obj.lossTangentCover);
     end
 
-
     for i=1:40
         fprintf(fileID,'0\n');
     end
-
 
     fclose(fileID);
 
