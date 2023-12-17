f608f6aa49505aef7e5007ea168fc3c59fa056ea
王海东 <wanghaidong@hutb.edu.cn>
2023-12-17 21:07:58 +0800

    更新 CAD2DDesignerTool.m

--- toolbox/shared/em_cad/+cad/CAD2DDesignerTool.m
+++ toolbox/shared/em_cad/+cad/CAD2DDesignerTool.m
@@ -1,15 +1,11 @@
 classdef CAD2DDesignerTool<handle
 
-
-
-
     properties
 App
 
 View
 Model
 Controller
-
 
 TabGroup
 CadDesignTab
@@ -48,8 +44,6 @@ PropertiesDocument
 
             generateNewSessionView(self)
 
-
-
             self.App.Visible=true;
             addViewModelAndController(self)
         end
@@ -57,33 +51,27 @@ PropertiesDocument
         function generateNewSessionView(self)
             import matlab.ui.internal.*;
 
-
-
             documentOptions.Title="Tree";
             documentOptions.Tag="TreeView";
-
             documentOptions.DocumentGroupTag="FigureDocumentGroup";
             document=FigureDocument(documentOptions);
             self.App.add(document);
             self.TreeDocument=document;
-
             documentOptions.Title="Canvas";
             documentOptions.Tag="Canvas";
-
             documentOptions.DocumentGroupTag="FigureDocumentGroup";
             document=FigureDocument(documentOptions);
             self.App.add(document);
             self.CanvasDocument=document;
-
             documentOptions.Title="Properties";
             documentOptions.Tag="Properties";
-
             documentOptions.DocumentGroupTag="FigureDocumentGroup";
             document=FigureDocument(documentOptions);
             self.App.add(document);
             self.PropertiesDocument=document;
 
         end
+
 
         function addViewModelAndController(self)
             self.View=[cad.Cad2DCanvas(self.CanvasDocument.Figure),...
