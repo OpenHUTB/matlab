696f459f8f6eef8340dfb562a528b9c7a7d209f8
王海东 <wanghaidong@hutb.edu.cn>
2023-12-17 20:30:57 +0800

    更新 Node.m

--- toolbox/shared/devicetree/base/+matlabshared/+devicetree/+node/Node.m
+++ toolbox/shared/devicetree/base/+matlabshared/+devicetree/+node/Node.m
@@ -7,10 +7,6 @@ classdef Node<matlabshared.devicetree.node.NonTerminalNode&matlabshared.devicetr
         end
 
         function hRefNode=getReferenceNode(obj)
-
-
-
-
             name=obj.getReferenceName;
             hRefNode=matlabshared.devicetree.node.ReferenceNode(name);
         end
@@ -19,20 +15,15 @@ classdef Node<matlabshared.devicetree.node.NonTerminalNode&matlabshared.devicetr
     methods(Access=protected)
         function hTargetNode=getOverlayTargetNode(obj)
 
-
-
             hTargetNode=obj.ParentNode;
-
-
-
 
             if isempty(hTargetNode)
                 error(message('devicetree:base:OverlayParentNodeMissing',obj.Name));
             end
         end
 
-        function labelPrefix=getSourceLabelPrefix(obj)
 
+        function labelPrefix=getSourceLabelPrefix(obj)
 
             if~isempty(obj.Label)
                 labelPrefix=obj.Label+": ";
