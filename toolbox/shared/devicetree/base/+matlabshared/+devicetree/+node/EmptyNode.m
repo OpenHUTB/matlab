098181c3b44c6b298da3d33e94b038f9b7f727eb
王海东 <wanghaidong@hutb.edu.cn>
2023-12-17 20:29:22 +0800

    更新 EmptyNode.m

--- toolbox/shared/devicetree/base/+matlabshared/+devicetree/+node/EmptyNode.m
+++ toolbox/shared/devicetree/base/+matlabshared/+devicetree/+node/EmptyNode.m
@@ -1,10 +1,5 @@
 classdef EmptyNode<matlabshared.devicetree.node.TerminalNode&matlabshared.devicetree.node.ParentableNode
 
-
-
-
-
-
     methods
         function obj=EmptyNode()
             obj=obj@matlabshared.devicetree.node.TerminalNode(string.empty);
