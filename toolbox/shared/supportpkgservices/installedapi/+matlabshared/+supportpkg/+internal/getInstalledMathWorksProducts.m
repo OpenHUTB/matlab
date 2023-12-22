b32928a99bf6207577df01e0ce853066dabfaff7
王海东 <wanghaidong@hutb.edu.cn>
2023-12-22 13:24:05 +0800

    更新 getInstalledMathWorksProducts.m

--- toolbox/shared/supportpkgservices/installedapi/+matlabshared/+supportpkg/+internal/getInstalledMathWorksProducts.m
+++ toolbox/shared/supportpkgservices/installedapi/+matlabshared/+supportpkg/+internal/getInstalledMathWorksProducts.m
@@ -1,13 +1,9 @@
 function out=getInstalledMathWorksProducts(root)
 
-
-
     out=[];
     if isempty(root)
         return;
     end
-
-
 
     out=matlab.internal.getInstalledMathWorksProducts(root);
 end
