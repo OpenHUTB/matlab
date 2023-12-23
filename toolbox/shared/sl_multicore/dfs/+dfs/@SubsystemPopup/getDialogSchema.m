fd303ca43dd3944fa1e8d284cc87e46819295bd6
王海东 <wanghaidong@hutb.edu.cn>
2023-12-23 19:44:56 +0800

    更新 getDialogSchema.m

--- toolbox/shared/sl_multicore/dfs/+dfs/@SubsystemPopup/getDialogSchema.m
+++ toolbox/shared/sl_multicore/dfs/+dfs/@SubsystemPopup/getDialogSchema.m
@@ -1,18 +1,14 @@
 function dlgstruct=getDialogSchema(this,~)
 
-
-
-
     items=getItems(this);
     dlgstruct=getDlgStruct(items,this);
 
 end
 
+
 function panel=getChildPanel(this)
 
     items={};
-
-
     childLabel.Name=DAStudio.message('dataflow:UI:SubsystemPopupChildLabel');
     childLabel.Type='text';
     childLabel.Tag='childLabel';
@@ -22,8 +18,6 @@ function panel=getChildPanel(this)
     childLabel.Alignment=1;
 
     items{end+1}=childLabel;
-
-
     findHLink.Name=getfullname(this.HConfigBlock);
     findHLink.Type='hyperlink';
     findHLink.Tag='findConfigBlkHLink';
@@ -37,7 +31,6 @@ function panel=getChildPanel(this)
 
     items{end+1}=findHLink;
 
-
     panel.Type='panel';
     panel.Tag='childPanel';
     panel.Flat=false;
@@ -48,12 +41,9 @@ function panel=getChildPanel(this)
 
 end
 
+
 function panel=getInfoPanel(this)
-
-
     items={};
-
-
     threadsLabel.Name=DAStudio.message('dataflow:UI:SubsystemPopupThreadsLabel',this.NumThreadsStr);
     threadsLabel.Type='text';
     threadsLabel.Tag='threadsLabel';
@@ -92,8 +82,6 @@ function panel=getInfoPanel(this)
         latencyLabel.Alignment=1;
 
         items{end+1}=latencyLabel;
-
-
         openDialog.Name=DAStudio.message('dataflow:UI:SubsystemPopupConfigureHyperlinkLabel');
         openDialog.Type='hyperlink';
         openDialog.Tag='openConfigBlkDialogHLink';
@@ -110,7 +98,6 @@ function panel=getInfoPanel(this)
     end
 
     if this.ShowSuggested
-
         latencyLabel.Name=DAStudio.message('dataflow:UI:SubsystemPopupSuggestedLatencyLabel',this.OptimumLatency);
         latencyLabel.Type='text';
         latencyLabel.Tag='suggestedLatencyLabel';
@@ -122,7 +109,6 @@ function panel=getInfoPanel(this)
         items{end+1}=latencyLabel;
 
         if this.ShowAccept
-
             acceptHLink.Name=DAStudio.message('dataflow:UI:SubsystemPopupAcceptHyperlinkLabel');
             acceptHLink.Type='hyperlink';
             acceptHLink.Tag='acceptHLink';
@@ -140,8 +126,6 @@ function panel=getInfoPanel(this)
         end
     end
 
-
-
     panel.Type='panel';
     panel.Tag='infoPanel';
     panel.Flat=false;
@@ -152,12 +136,9 @@ function panel=getInfoPanel(this)
 
 end
 
+
 function items=getItems(this)
-
-
     items={};
-
-
     popupLabel.Name=DAStudio.message('dataflow:UI:ConcurrentSubsystemPopupTitleLabel');
     popupLabel.Type='text';
     popupLabel.Tag='popupLabel';
@@ -168,8 +149,6 @@ function items=getItems(this)
     popupLabel.Bold=true;
 
     items{end+1}=popupLabel;
-
-
     openHelp.Name=DAStudio.message('dataflow:UI:SubsystemPopupHelpLabel');
     openHelp.Type='hyperlink';
     openHelp.Tag='helpHLink';
@@ -184,13 +163,11 @@ function items=getItems(this)
 
     items{end+1}=openHelp;
 
-
     if strcmp(this.PopupType,'Child')
         items{end+1}=getChildPanel(this);
     else
         items{end+1}=getInfoPanel(this);
     end
-
 
     mainPanel.Type='panel';
     mainPanel.Tag='mainPanel';
@@ -201,6 +178,7 @@ function items=getItems(this)
     mainPanel.Items=items;
     items={mainPanel};
 end
+
 
 function dlgstruct=getDlgStruct(items,~)
     dlgstruct.DialogTitle=DAStudio.message('dataflow:UI:ConcurrentSubsystemPopupTitle');
