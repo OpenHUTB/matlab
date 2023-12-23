7e1cf86187d8d054651ffd142a613cc3764c22b2
王海东 <wanghaidong@hutb.edu.cn>
2023-12-23 19:35:43 +0800

    更新 disableMultirateForSubsystem.m

--- toolbox/shared/sl_multicore/dfs/+dfs/disableMultirateForSubsystem.m
+++ toolbox/shared/sl_multicore/dfs/+dfs/disableMultirateForSubsystem.m
@@ -1,7 +1,4 @@
 function str=disableMultirateForSubsystem(subsystem)
-
-
-
     set_param(subsystem,'AutoFrameSizeCalculation','off');
     str=getString(message('dataflow:Multirate:MultirateDisabled',subsystem));
 end
