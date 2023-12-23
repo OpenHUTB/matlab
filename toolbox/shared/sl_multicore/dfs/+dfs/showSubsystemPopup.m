5fa1eb7ed28aab0c55b74322937fa5061132dcac
王海东 <wanghaidong@hutb.edu.cn>
2023-12-23 19:39:20 +0800

    更新 showSubsystemPopup.m

--- toolbox/shared/sl_multicore/dfs/+dfs/showSubsystemPopup.m
+++ toolbox/shared/sl_multicore/dfs/+dfs/showSubsystemPopup.m
@@ -1,8 +1,4 @@
 function showSubsystemPopup(badgeType,isGraph,hConfigBlock,evalLatency)
-
-
-
-
     popup=dfs.SubsystemPopup(badgeType,isGraph,hConfigBlock,evalLatency);
     dlg=DAStudio.Dialog(popup);
     popup.show(dlg);
