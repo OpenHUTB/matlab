288dcd8da69ad7fde796a8aa79b844a20ce889a8
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:12:35 +0800

    更新 convertToAperiodicPartition.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/+partition/convertToAperiodicPartition.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/+partition/convertToAperiodicPartition.m
@@ -1,12 +1,8 @@
 function convertToAperiodicPartition(subsystemH)
 
-
-
     obsModel=bdroot(subsystemH);
     aperiodicSubsys=Simulink.findBlocks(obsModel,...
     "BlockType","SubSystem","ScheduleAs","Aperiodic partition");
-
-
     obsModel=get_param(bdroot(subsystemH),"Name");
     if get_param(obsModel,"SolverType")=="Fixed-step"
         set_param(obsModel,"EnableMultiTasking","on");
@@ -19,21 +15,18 @@ function convertToAperiodicPartition(subsystemH)
     setPartitionPosition(subsystemH);
 end
 
+
 function setPartitionPosition(aperiodicH)
     pos=get_param(aperiodicH,"Position");
     pos=pos+[0,0,110,35];
     set_param(aperiodicH,"Position",pos);
 end
 
+
 function newName=getUniquePartitionName(aperiodicSubsys)
-
     existingNames=string(get_param(aperiodicSubsys,"PartitionName"));
-
-
     TEMPLATE="ConditionalObserverPartition";
     existingNames=[existingNames;TEMPLATE];
-
-
     existingNames=matlab.lang.makeUniqueStrings(existingNames);
     newName=existingNames(end);
 end
