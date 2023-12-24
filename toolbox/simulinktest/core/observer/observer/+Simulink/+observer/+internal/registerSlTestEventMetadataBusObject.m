6316a2d853f9d0ebda678500ab0f87407022f23c
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:09:27 +0800

    更新 registerSlTestEventMetadataBusObject.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/registerSlTestEventMetadataBusObject.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/registerSlTestEventMetadataBusObject.m
@@ -1,7 +1,4 @@
 function registerSlTestEventMetadataBusObject
-
-
-
     slTestEventMetadata=load(fullfile(matlabroot,"toolbox","simulinktest",...
     "core","observer","observer","slTestEventMetadata.mat")).slTestEventMetadata;
     Simulink.Bus.register('slTestEventMetadata',slTestEventMetadata,'forceregister',true);
