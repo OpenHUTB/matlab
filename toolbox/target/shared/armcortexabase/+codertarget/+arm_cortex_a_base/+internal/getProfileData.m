d9d84164ee7f206b5b044f22f2157156aef5abc6
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 19:00:30 +0800

    更新 getProfileData.m

--- toolbox/target/shared/armcortexabase/+codertarget/+arm_cortex_a_base/+internal/getProfileData.m
+++ toolbox/target/shared/armcortexabase/+codertarget/+arm_cortex_a_base/+internal/getProfileData.m
@@ -1,25 +1,12 @@
 function[sectionIds,timerValues,coreNums]=getProfileData(modelName)
 
-
-
-
-
-
-
-
-
-
-
     hCS=getActiveConfigSet(modelName);
     hardwareName=codertarget.targethardware.getTargetHardwareName(hCS);
     [ipAddress,userName,password,~,portNumber]=codertarget.arm_cortex_a.internal.getConnectionInfo(hardwareName);
-
-
     ssh=matlabshared.internal.ssh2client(ipAddress,userName,password,portNumber);
     [~,name,~]=fileparts(modelName);
     filename=[name,'.txt'];
     scpGetFile(ssh,filename,filename);
-
 
     d=importdata(filename);
     sectionIds=uint32(d(:,1));
