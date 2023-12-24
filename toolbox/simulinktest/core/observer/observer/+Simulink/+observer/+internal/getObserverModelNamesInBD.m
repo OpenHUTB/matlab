00d763f21e485eb4ce427091108c3b862ad34aa4
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:08:44 +0800

    更新 getObserverModelNamesInBD.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/getObserverModelNamesInBD.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/getObserverModelNamesInBD.m
@@ -1,11 +1,4 @@
 function mdls=getObserverModelNamesInBD(topmdlH)
-
-
-
-
-
-
-
     obsMdlHdls=find_system(topmdlH,'IncludeCommented','On',...
     'MatchFilter',@Simulink.match.internal.filterOutInactiveVariantSubsystemChoices,...
     'BlockType','ObserverReference');
