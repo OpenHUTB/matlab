38a1ac260be35374160aea073a4bf3fe6b4104cf
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:09:45 +0800

    更新 rtwgenForSLDV.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/rtwgenForSLDV.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/rtwgenForSLDV.m
@@ -1,14 +1,8 @@
 function isObsSupportON=rtwgenForSLDV(topBdHandle,obsBdHandle)
-
-
-
     topSession=sldvprivate('sldvGetActiveSession',topBdHandle);
     if isempty(topSession)
-
-
         designMdlName=get_param(topBdHandle,'DVDesignModelName');
         topSession=sldvprivate('sldvGetActiveSession',designMdlName);
     end
-
     isObsSupportON=topSession.generateIR(obsBdHandle);
 end
