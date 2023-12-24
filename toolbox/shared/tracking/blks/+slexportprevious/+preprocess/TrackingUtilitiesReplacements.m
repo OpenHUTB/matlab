c289cb727d3bcae931f88ea074b0aa733e5a9446
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 09:32:53 +0800

    更新 TrackingUtilitiesReplacements.m

--- toolbox/shared/tracking/blks/+slexportprevious/+preprocess/TrackingUtilitiesReplacements.m
+++ toolbox/shared/tracking/blks/+slexportprevious/+preprocess/TrackingUtilitiesReplacements.m
@@ -1,14 +1,8 @@
 function TrackingUtilitiesReplacements(obj)
-
-
-
     if isR2020bOrEarlier(obj.ver)
         obj.removeLibraryLinksTo('trackingutilitieslib/Track Concatenation');
     end
 
-    if isR2017aOrEarlier(obj.ver)
-
-
-        obj.removeLibraryLinksTo('trackingutilitieslib/Detection Concatenation');
+    if isR2017aOrEarlier(obj.ver)        obj.removeLibraryLinksTo('trackingutilitieslib/Detection Concatenation');
     end
 end
