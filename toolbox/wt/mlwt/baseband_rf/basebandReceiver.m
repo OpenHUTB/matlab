3f1c3f97893e3b25d355200b46f6803dffb0af54
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 18:46:52 +0800

    更新 basebandReceiver.m

--- toolbox/wt/mlwt/baseband_rf/basebandReceiver.m
+++ toolbox/wt/mlwt/baseband_rf/basebandReceiver.m
@@ -1,114 +1,18 @@
 classdef basebandReceiver<handle&matlab.mixin.SetGet
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     properties(Dependent)
-
-
-
-
-
 
         RadioGain double{mustBeFinite,mustBeVector}
 
-
-
-
-
-
         CenterFrequency double{mustBeFinite,mustBePositive,mustBeVector}
 
-
-
-
-
         SampleRate(1,1)double{mustBeFinite,mustBePositive}
-
-
-
-
-
-
-
         Antennas{mustBeText}
-
-
-
         DroppedSamplesAction{mustBeTextScalar}
-
-
-
-
 
         CaptureDataType{mustBeTextScalar}
     end
+
 
     properties(Access=protected)
 sysObj
@@ -116,42 +20,62 @@ propHelper
         nonTunableErrorMessage=@(propName)message("wt:baseband_rf:StopTxThenTune",propName)
     end
 
+
     methods
         function set.CaptureDataType(obj,val)
             val=validatestring(val,["int16","double","single"],"","CaptureDataType");
             obj.propHelper.setNontunable("CaptureDataType",val,canRelease(obj),obj.nonTunableErrorMessage("CaptureDataType"));
         end
+
+
         function val=get.CaptureDataType(obj)
             val=obj.sysObj.CaptureDataType;
         end
+
+
         function set.DroppedSamplesAction(obj,val)
             val=validatestring(val,["error","warning","none"],"","DroppedSamplesAction");
             obj.sysObj.DroppedSamplesAction=val;
         end
+
+
         function val=get.DroppedSamplesAction(obj)
             val=obj.sysObj.DroppedSamplesAction;
         end
+
+
         function set.RadioGain(obj,val)
             obj.propHelper.applyVectorTunable("ReceiveGain",val,length(obj.Antennas),canRelease(obj),obj.nonTunableErrorMessage("RadioGain"))
         end
+
+
         function val=get.RadioGain(obj)
             val=obj.sysObj.ReceiveGain;
         end
+
+
         function set.CenterFrequency(obj,val)
             obj.propHelper.applyVectorTunable("ReceiveCenterFrequency",val,length(obj.Antennas),canRelease(obj),obj.nonTunableErrorMessage("CenterFrequency"))
         end
+
+
         function val=get.CenterFrequency(obj)
             val=obj.sysObj.ReceiveCenterFrequency;
         end
+
 
         function set.Antennas(obj,val)
             val=convertCharsToStrings(val);
             mustBeMember(val,obj.sysObj.AvailableReceiveAntennas);
             obj.propHelper.setNontunable("ReceiveAntennas",val,canRelease(obj),obj.nonTunableErrorMessage("Antennas"));
         end
+
+
         function val=get.Antennas(obj)
             val=obj.sysObj.ReceiveAntennas;
         end
+
+
         function set.SampleRate(obj,val)
             obj.propHelper.setNontunable("SampleRate",val,canRelease(obj),obj.nonTunableErrorMessage("SampleRate"));
         end
@@ -160,16 +84,14 @@ propHelper
         end
     end
 
+
     methods
         function obj=basebandReceiver(radioID,varargin)
             radioID=convertCharsToStrings(radioID);
             obj.sysObj=wt.internal.basebandTransceiver(radioID);
             obj.propHelper=wt.internal.app.PropertyHelper(obj.sysObj);
-
-
             obj.sysObj.TransmitAntennas=-1;
             obj.sysObj.ReceiveAntennas=obj.sysObj.ReceiveAntennas(1);
-
 
             if~isempty(varargin)
                 set(obj,varargin{:});
@@ -177,37 +99,10 @@ propHelper
         end
 
         function[data,timestamp,droppedSamples]=capture(obj,length)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             [data,timestamp,droppedSamples]=capture(obj.sysObj,length,1);
         end
     end
+
 
     methods(Access=public,Hidden=true)
         function setHardwareSetupCompleted(obj,value)
