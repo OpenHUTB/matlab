b5b1de2a9db589ea9101a0a722205d433a53a166
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 18:57:20 +0800

    更新 DeviceSetup.m

--- toolbox/wt/mlwt/baseband_rf/+wt/+internal/+baseband_rf/+rfnoc/DeviceSetup.m
+++ toolbox/wt/mlwt/baseband_rf/+wt/+internal/+baseband_rf/+rfnoc/DeviceSetup.m
@@ -1,37 +1,22 @@
 classdef DeviceSetup<wt.internal.rfnoc.DeviceSetup
 
-
-
-
-
     properties(Constant)
         BitstreamFolder=fullfile(matlabroot,'toolbox','wt','bitstreams','baseband_rf','rfnoc','bitstreams')
         Application="baseband_rf";
     end
+
 
     methods
         function flag=canRadioRunApplication(obj,driver)
             flag=true;
             hasBlocks=canRadioRunApplication@wt.internal.rfnoc.DeviceSetup(obj,driver);
             if hasBlocks
-
-
-
-
-
-
-
                 if strcmp(wt.internal.hardware.rfnoc.feature("MCOSDriver"),"off")
                     replayControl=driver.driverImpl.getControl("0/Replay#0");
-
-
                     numPorts=replayControl.get_num_input_ports;
-
 
                     clibRelease(replayControl);
                 else
-
-
                     numPorts=driver.driverImpl.getBlock("0/Replay#0").getNumInputPorts;
                 end
 
