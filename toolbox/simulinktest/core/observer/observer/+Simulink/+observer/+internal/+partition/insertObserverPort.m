8f72aee78cd3a048cbcbd421302fcf5296739c3c
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:13:30 +0800

    更新 insertObserverPort.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/+partition/insertObserverPort.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/+partition/insertObserverPort.m
@@ -1,28 +1,17 @@
 function insertObserverPort(obsPortH)
-
-
-
-
-
     if~isempty(Simulink.observer.internal.partition.getPartitionFromObsPort(obsPortH))
         return;
     end
-
-
     Simulink.BlockDiagram.createSubsystem(obsPortH,"Name","Aperiodic",...
     "MakeNameUnique","on");
-
     subsystem=get_param(obsPortH,"Parent");
     subsystemH=get_param(subsystem,"Handle");
-
     Simulink.observer.internal.partition.convertToAperiodicPartition(subsystemH);
     terminateObsPortOutput(subsystemH);
 end
 
+
 function terminateObsPortOutput(aperiodicH)
-
-
-
     replBlks=replace_block(aperiodicH,"Outport","Terminator","noprompt");
     assert(isscalar(replBlks));
     set_param(replBlks{1},"Name","Terminator");
