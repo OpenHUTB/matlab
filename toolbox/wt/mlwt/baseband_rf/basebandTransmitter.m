798713c80ebd398d21eb11b478f7e9e0a22262d4
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 18:54:45 +0800

    更新 basebandTransmitter.m

--- toolbox/wt/mlwt/baseband_rf/basebandTransmitter.m
+++ toolbox/wt/mlwt/baseband_rf/basebandTransmitter.m
@@ -1,106 +1,21 @@
 classdef basebandTransmitter < handle & matlab.mixin.SetGet
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     properties ( Dependent )
-
-
-
-
-
-
         RadioGain double{ mustBeFinite, mustBeVector }
-
-
-
-
-
 
         CenterFrequency double{ mustBeFinite, mustBePositive, mustBeVector }
 
-
-
-
-
-
-
         SampleRate( 1, 1 )double{ mustBeFinite, mustBePositive }
-
-
-
-
-
-
 
         Antennas{ mustBeText }
     end
 
-    properties ( Hidden, Dependent )
 
+    properties ( Hidden, Dependent )
 
         TransmitDataType( 1, 1 ){ mustBeMember( TransmitDataType, [ "int16", "double", "single" ] ) }
     end
+
 
     properties ( Access = protected )
 
@@ -110,41 +25,60 @@ classdef basebandTransmitter < handle & matlab.mixin.SetGet
         currentlyTransmitting = false;
     end
 
+
     methods
         function set.RadioGain( obj, val )
             obj.propHelper.applyVectorTunable( "TransmitGain", val, length( obj.Antennas ), canRelease( obj ), obj.nonTunableErrorMessage( "RadioGain" ) )
         end
+
+
         function val = get.RadioGain( obj )
             val = obj.sysObj.TransmitGain;
         end
+
+
         function set.CenterFrequency( obj, val )
             obj.propHelper.applyVectorTunable( "TransmitCenterFrequency", val, length( obj.Antennas ), canRelease( obj ), obj.nonTunableErrorMessage( "CenterFrequency" ) )
         end
+
+
         function val = get.CenterFrequency( obj )
             val = obj.sysObj.TransmitCenterFrequency;
         end
+
 
         function set.Antennas( obj, val )
             mustBeMember( val, obj.sysObj.AvailableTransmitAntennas );
             val = convertCharsToStrings( val );
             obj.propHelper.setNontunable( "TransmitAntennas", val, canRelease( obj ), obj.nonTunableErrorMessage( "Antennas" ) );
         end
+
+
         function val = get.Antennas( obj )
             val = obj.sysObj.TransmitAntennas;
         end
+
+
         function set.SampleRate( obj, val )
             obj.propHelper.setNontunable( "SampleRate", val, canRelease( obj ), obj.nonTunableErrorMessage( "SampleRate" ) );
         end
+
+
         function val = get.SampleRate( obj )
             val = obj.sysObj.SampleRate;
         end
+
+
         function set.TransmitDataType( obj, val )
             obj.propHelper.setNontunable( "TransmitDataType", val, canRelease( obj ), obj.nonTunableErrorMessage( "TransmitDataType" ) );
         end
+
+
         function val = get.TransmitDataType( obj )
             val = obj.sysObj.TransmitDataType;
         end
     end
+
 
     methods
         function obj = basebandTransmitter( radioID, varargin )
@@ -156,15 +90,13 @@ classdef basebandTransmitter < handle & matlab.mixin.SetGet
             obj.sysObj.TransmitAntennas = obj.sysObj.TransmitAntennas( 1 );
             obj.sysObj.ReceiveAntennas =  - 1;
 
-
             if ~isempty( varargin )
                 set( obj, varargin{ : } );
             end
         end
 
+
         function transmit( obj, waveform, mode )
-
-
             arguments
                 obj( 1, 1 )
                 waveform
@@ -175,16 +107,13 @@ classdef basebandTransmitter < handle & matlab.mixin.SetGet
                 error( message( "wt:baseband_rf:StopTxThenTx" ) )
             end
 
-
             if ~ismember( class( waveform ), [ "int16", "double", "single" ] )
                 error( message( "wt:baseband_rf:WrongTxDataType" ) )
             end
             obj.TransmitDataType = string( class( waveform ) );
-
             obj.propHelper.applyAllProperties(  )
 
             transmit( obj.sysObj, waveform, mode );
-
 
             if mode ~= wt.internal.TransmitModes.once
                 obj.currentlyTransmitting = true;
@@ -192,33 +121,34 @@ classdef basebandTransmitter < handle & matlab.mixin.SetGet
         end
 
         function stopTransmission( obj )
-
-
-
-
-
-
             obj.sysObj.stopTransmitRepeat(  );
-
 
             obj.currentlyTransmitting = false;
         end
     end
 
+
     methods ( Access = public, Hidden = true )
         function setHardwareSetupCompleted( obj, value )
             obj.sysObj.HardwareSetupCompleted = value;
         end
+
+
         function value = getHardwareSetupCompleted( obj )
             value = obj.sysObj.HardwareSetupCompleted;
         end
+
+
         function setSystemObjectProperty( obj, name, value )
             obj.sysObj.( name ) = value;
         end
+
+
         function value = getSystemObjectProperty( obj, name )
             value = obj.sysObj.( name );
         end
     end
+
 
     methods ( Access = protected )
         function releasable = canRelease( obj )
