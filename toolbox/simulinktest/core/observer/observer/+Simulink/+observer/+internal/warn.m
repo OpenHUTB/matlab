56363ac95da68c742b2a3170dd3add1cc53c1111
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:11:53 +0800

    更新 warn.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/warn.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/warn.m
@@ -1,60 +1,31 @@
 function warn(msg,varargin)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     assert(isa(msg,'MException')||...
     iscell(msg)||...
     ischar(msg));
 
     if(ischar(msg)||isa(msg,'MException'))
-
-
         msg={msg};
     end
 
-
     if nargin>1
         narginchk(4,4);
-
-
-
         uiMode=varargin{1};
-
         stage=DAStudio.message(varargin{2});
         modelName=varargin{3};
 
         if ishandle(modelName)
             modelName=get_param(modelName,'Name');
         end
-
         observerStage=Simulink.output.Stage(stage,...
         'ModelName',modelName,...
         'UIMode',uiMode);%#ok
     end
-
-
     origSetting=warning('off','backtrace');
-
-
     if uiMode
         MSLDiagnostic(msg{:},'COMPONENT','Simulink Test','CATEGORY','Observer').reportAsWarning;
     else
         MSLDiagnostic(msg{:}).reportAsWarning;
     end
-
-
     warning(origSetting.state,origSetting.identifier);
 end
