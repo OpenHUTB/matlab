57a61467a417134af04020f99eb2ddfc252ead02
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 09:33:22 +0800

    更新 AbstractTracker.m

--- toolbox/shared/tracking/fusionlib/+matlabshared/+tracking/+internal/+fusion/AbstractTracker.m
+++ toolbox/shared/tracking/fusionlib/+matlabshared/+tracking/+internal/+fusion/AbstractTracker.m
@@ -1,17 +1,13 @@
 classdef AbstractTracker<matlab.System...
     &matlabshared.tracking.internal.AbstractContainsFilters
 
-
-
-
 %#codegen
 
     methods(Access=protected)
         function obj=AbstractTracker
-
-
             coder.allowpcode('plain');
         end
+
 
         function flag=isSimulinkBlock(obj)
             flag=obj.getExecPlatformIndex();
