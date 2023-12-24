7133cb6e0bb80cf721c5ade397077ee1f1095ae3
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:06:08 +0800

    更新 determineObservableSrcAndResidualBusPath.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/determineObservableSrcAndResidualBusPath.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/determineObservableSrcAndResidualBusPath.m
@@ -1,71 +1,9 @@
 function ret=determineObservableSrcAndResidualBusPath(phObj,elementString)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     nvBus=strcmp(phObj.CompiledBusType,'NON_VIRTUAL_BUS');
     notBus=strcmp(phObj.CompiledBusType,'NOT_BUS');
     vBus=strcmp(phObj.CompiledBusType,'VIRTUAL_BUS');
 
     if notBus
-
-
-
-
 
         if strcmp(phObj.PortType,'inport')
 
@@ -89,28 +27,17 @@ function ret=determineObservableSrcAndResidualBusPath(phObj,elementString)
         end
         elems=[];
         if~isempty(elementString)
-
-
-
             elems=Simulink.observer.internal.extractSignalNames(elementString);
         end
         ret.elems=elems;
     else
         assert(vBus);
 
-
         assert(~isempty(elementString));
         elems=Simulink.observer.internal.extractSignalNames(elementString);
-
-
-
-
-
         sess=Simulink.CMI.EIAdapter(Simulink.EngineInterfaceVal.byFiat);
 
         if strcmp(phObj.PortType,'outport')
-
-
 
             dst=phObj.getGraphicalDst;
             if isempty(dst)
@@ -131,27 +58,17 @@ end
 
 function ret=findNVBoundary(busSrcInfo,elems,phObj,origSel)
     assert(isa(busSrcInfo,'containers.Map'));
-
     assert(busSrcInfo.isKey(elems(1).name),"Could not find the element in the bus src map");
     elemSrcInfo=busSrcInfo(elems(1).name);
 
     if isa(elemSrcInfo,'containers.Map')
-
-
-
         assert(isscalar(elems(1).index)&&elems(1).index==-1);
 
-
-
         elems(1)=[];
-
         ret=findNVBoundary(elemSrcInfo,elems,phObj,origSel);
     else
 
         if elemSrcInfo(1,4)~=-1
-
-
-
             DAStudio.error('Simulink:SltBlkMap:partialSliceObservationNotSupported',...
             origSel,phObj.PortNumber,phObj.Parent);
         end
@@ -161,19 +78,8 @@ function ret=findNVBoundary(busSrcInfo,elems,phObj,origSel)
 
         if strcmp(busType,'NON_VIRTUAL_BUS')
             if elems(1).index~=-1
-
-
-
-
-
-
                 elems(1).name='';
             else
-
-
-
-
-
                 elems(1)=[];
             end
             ret.elems=elems;
