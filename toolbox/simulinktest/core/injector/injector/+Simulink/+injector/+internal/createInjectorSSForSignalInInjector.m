7509eb0d8fb49feb0bfc59b5488ae0f7cc54b6aa
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 09:56:16 +0800

    更新 createInjectorSSForSignalInInjector.m

--- toolbox/simulinktest/core/injector/injector/+Simulink/+injector/+internal/createInjectorSSForSignalInInjector.m
+++ toolbox/simulinktest/core/injector/injector/+Simulink/+injector/+internal/createInjectorSSForSignalInInjector.m
@@ -1,15 +1,10 @@
 function[injSSHdl,injInHdl,injOutHdl]=createInjectorSSForSignalInInjector(prtHdl,inj,showAndSelect,uiMode,varargin)
 
-
-
-
-
     blockPath=[];
     if iscell(prtHdl)
         blockPath=prtHdl{1};
         prtHdl=prtHdl{2};
     end
-
     [sysBounds(1),sysBounds(2),sysBounds(3),sysBounds(4)]=Simulink.injector.internal.getSystemBounds(inj);
     pos=[sysBounds(1)+5,sysBounds(4)+20,sysBounds(1)+90,sysBounds(4)+72];
 
@@ -26,8 +21,6 @@ function[injSSHdl,injInHdl,injOutHdl]=createInjectorSSForSignalInInjector(prtHdl
     if~isempty(varargin)
         set_param(injSSHdl,varargin{:});
     end
-
-
     Simulink.injector.internal.notifyFaultHasBeenAdded(blkH,injSpec);
 end
 
