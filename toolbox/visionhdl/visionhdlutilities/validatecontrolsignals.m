714652b14c65c368481108fc1ddbc32dbdb5f719
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 17:36:52 +0800

    更新 validatecontrolsignals.m

--- toolbox/visionhdl/visionhdlutilities/validatecontrolsignals.m
+++ toolbox/visionhdl/visionhdlutilities/validatecontrolsignals.m
@@ -1,41 +1,15 @@
 function valid=validatecontrolsignals(Ctrl)
 
-
-
-
 %#codegen
     coder.allowpcode('plain');
 
     coder.internal.errorIf(~isstruct(Ctrl),'visionhdl:controlsignals:FieldNameOrOrder');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     actualField=fieldnames(Ctrl);
     coder.internal.errorIf(numel(actualField)~=5,'visionhdl:controlsignals:FieldNameOrOrder');
-
     expectedField={'hStart';'hEnd';'vStart';'vEnd';'valid'};%#ok<EMCA>
     for ii=1:5
         coder.internal.errorIf(~strcmp(actualField{ii},expectedField{ii}),'visionhdl:controlsignals:FieldNameOrOrder');%#ok<EMCA>
     end
-
 
     validateattributes(Ctrl.hStart,{'logical'},{'scalar'},'','hStart');%#ok<EMCA>
     validateattributes(Ctrl.hEnd,{'logical'},{'scalar'},'','hEnd');%#ok<EMCA>
