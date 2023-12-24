9a37e77cbbb5b1eb4cbef50624e14ba8a2f3731a
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 09:45:57 +0800

    更新 macroTemplate.m

--- toolbox/simulinktest/core/assessments/expressions/+sltest/+expressions/macroTemplate.m
+++ toolbox/simulinktest/core/assessments/expressions/+sltest/+expressions/macroTemplate.m
@@ -1,8 +1,4 @@
 function templateHandle=macroTemplate(name,args,definition)
-
-
-
-
     import sltest.expressions.*
     templateHandle=TemplateHandle.makeMoveFrom(mi.macroTemplate(name,args,definition));
 end
