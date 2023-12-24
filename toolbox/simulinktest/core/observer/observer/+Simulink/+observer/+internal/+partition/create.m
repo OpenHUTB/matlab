e7b4f4ccdb63e1c78f7d9c0ec8959cd6c54834a3
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:13:02 +0800

    更新 create.m

--- toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/+partition/create.m
+++ toolbox/simulinktest/core/observer/observer/+Simulink/+observer/+internal/+partition/create.m
@@ -1,7 +1,5 @@
 function aperiodicH=create(obsPort)
 
-
-
     obsModel=bdroot(obsPort);
     obsModel=string(get_param(obsModel,"Name"));
     aperiodicH=createSubsystem(obsModel);
@@ -9,16 +7,15 @@ function aperiodicH=create(obsPort)
     Simulink.observer.internal.partition.convertToAperiodicPartition(aperiodicH);
 end
 
+
 function subsystemH=createSubsystem(obsModel)
     pos=getMaxPos(obsModel);
 
-
     subsystemH=add_block("simulink/Commonly Used Blocks/Subsystem",obsModel+"/Partition",...
     "MakeNameUnique","on","Position",pos);
-
-
     Simulink.SubSystem.deleteContents(subsystemH);
 end
+
 
 function pos=getMaxPos(obsModel)
 
