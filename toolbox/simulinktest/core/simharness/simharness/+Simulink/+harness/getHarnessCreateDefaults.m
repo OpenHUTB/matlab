fc30a0e5f4cca6fd5bdeb9f0d6a0295e215e61b7
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 10:24:13 +0800

    更新 getHarnessCreateDefaults.m

--- toolbox/simulinktest/core/simharness/simharness/+Simulink/+harness/getHarnessCreateDefaults.m
+++ toolbox/simulinktest/core/simharness/simharness/+Simulink/+harness/getHarnessCreateDefaults.m
@@ -1,22 +1,13 @@
 function defaults=getHarnessCreateDefaults
 
-
     if slfeature('SLT_HarnessCustomizationRegistration')==0
-
         DAStudio.error('Simulink:Harness:DefaultRegistrationsDisallowed');
     end
-
-
-
-
-
-
 
     cm=DAStudio.CustomizationManager;
     defaultsObj=cm.SimulinkTestCustomizer.createHarnessDefaultsObj;
 
     properties=fieldnames(defaultsObj);
-
 
     for idx=1:length(properties)
         propName=properties{idx};
