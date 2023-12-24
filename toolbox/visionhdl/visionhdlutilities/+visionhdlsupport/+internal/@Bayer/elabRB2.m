787128e134e97307c49a2835d22e941e98afa239
王海东 <wanghaidong@hutb.edu.cn>
2023-12-24 17:52:54 +0800

    更新 elabRB2.m

--- toolbox/visionhdl/visionhdlutilities/+visionhdlsupport/+internal/@Bayer/elabRB2.m
+++ toolbox/visionhdl/visionhdlutilities/+visionhdlsupport/+internal/@Bayer/elabRB2.m
@@ -1,9 +1,5 @@
 function interpRB2Net=elabRB2(~,coreNet,blockInfo,sigInfo,dataRate)%#ok<INUSL>
 
-
-
-
-
     inType=sigInfo.inType.BaseType;
     inputWL=sigInfo.inputWL;
     inputFL=sigInfo.inputFL;
@@ -22,8 +18,6 @@ function interpRB2Net=elabRB2(~,coreNet,blockInfo,sigInfo,dataRate)%#ok<INUSL>
     ADDT8=pir_sfixpt_t(inputWL+8,-(inputFL+1));
     ADDT9=pir_sfixpt_t(inputWL+9,-(inputFL+1));
     ADDT10=pir_ufixpt_t(inputWL+12,-(inputFL+4));
-
-
     inPortNames={'REG2IN','REG3IN','REG4IN','REG5IN','DATA3IN','REG7IN'...
     ,'REG9IN','REG10IN','REG11IN','REG12IN','REG14IN'};
     inPortTypes=[inType,inType,inType,inType,inType,inType,inType,inType,inType,inType,inType];
@@ -53,11 +47,7 @@ function interpRB2Net=elabRB2(~,coreNet,blockInfo,sigInfo,dataRate)%#ok<INUSL>
     REG11IN=inSignals(9);
     REG12IN=inSignals(10);
     REG14IN=inSignals(11);
-
-
     outSignal=interpRB2Net.PirOutputSignals;
-
-
     Gain1Out=interpRB2Net.addSignal2('Type',twosCompT,'Name','GAIN1OUT');
     Gain2Out=interpRB2Net.addSignal2('Type',twosCompT,'Name','GAIN2OUT');
     Gain3CAST=interpRB2Net.addSignal2('Type',shiftTwoT,'Name','Gain3CAST');
@@ -77,7 +67,6 @@ function interpRB2Net=elabRB2(~,coreNet,blockInfo,sigInfo,dataRate)%#ok<INUSL>
     Gain11OutD=interpRB2Net.addSignal2('Type',twosCompT,'Name','GAIN11OUTD');
     Gain12Out=interpRB2Net.addSignal2('Type',ADDT10,'Name','GAIN12OUT');
     Gain12OutD=interpRB2Net.addSignal2('Type',ADDT10,'Name','GAIN12OUTD');
-
     pirelab.getGainComp(interpRB2Net,REG2IN,Gain1Out,fi(-1,1,2,0));
     pirelab.getGainComp(interpRB2Net,REG3IN,Gain2Out,fi(-1,1,2,0));
     pirelab.getDTCComp(interpRB2Net,REG4IN,Gain3CAST);
@@ -93,8 +82,6 @@ function interpRB2Net=elabRB2(~,coreNet,blockInfo,sigInfo,dataRate)%#ok<INUSL>
     pirelab.getBitShiftComp(interpRB2Net,Gain9CAST,Gain9Out,'sll',2);
     pirelab.getGainComp(interpRB2Net,REG12IN,Gain10Out,fi(-1,1,2,0));
     pirelab.getGainComp(interpRB2Net,REG14IN,Gain11Out,fi(-1,1,2,0));
-
-
     ADD1=interpRB2Net.addSignal2('Type',ADDT1,'Name','ADD1OUT');
     ADD2=interpRB2Net.addSignal2('Type',ADDT2,'Name','ADD2OUT');
     ADD3=interpRB2Net.addSignal2('Type',ADDT3,'Name','ADD3OUT');
@@ -121,9 +108,6 @@ function interpRB2Net=elabRB2(~,coreNet,blockInfo,sigInfo,dataRate)%#ok<INUSL>
     pirelab.getAddComp(interpRB2Net,[Gain5Out,Gain6Out],ADD3,'Floor','Wrap');
     pirelab.getAddComp(interpRB2Net,[Gain7Out,Gain8Out],ADD4,'Floor','Wrap');
     pirelab.getAddComp(interpRB2Net,[Gain9Out,Gain10Out],ADD5,'Floor','Wrap');
-
-
-
     pirelab.getUnitDelayComp(interpRB2Net,ADD1,ADD1D);
     pirelab.getUnitDelayComp(interpRB2Net,ADD2,ADD2D);
     pirelab.getUnitDelayComp(interpRB2Net,ADD3,ADD3D);
@@ -135,30 +119,16 @@ function interpRB2Net=elabRB2(~,coreNet,blockInfo,sigInfo,dataRate)%#ok<INUSL>
     pirelab.getAddComp(interpRB2Net,[ADD1D,ADD2D],ADD6,'Floor','Wrap');
     pirelab.getAddComp(interpRB2Net,[ADD3D,ADD4D],ADD7,'Floor','Wrap');
     pirelab.getAddComp(interpRB2Net,[ADD5D,Gain11OutD],ADD8,'Floor','Wrap');
-
-
-
-
     pirelab.getUnitDelayComp(interpRB2Net,ADD6,ADD6D,'REG6');
     pirelab.getUnitDelayComp(interpRB2Net,ADD7,ADD7D,'REG7');
     pirelab.getUnitDelayComp(interpRB2Net,ADD8,ADD8D,'REG8');
 
-
-
     pirelab.getAddComp(interpRB2Net,[ADD6D,ADD7D],ADD9);
 
-
     pirelab.getAddComp(interpRB2Net,[ADD9,ADD8D],ADD10,'Floor','Wrap');
-
-
-
-
     pirelab.getDTCComp(interpRB2Net,ADD10,ADD11,'Nearest','Saturate');
     pirelab.getBitShiftComp(interpRB2Net,ADD11,Gain12Out,'srl',3);
-
-
     pirelab.getUnitDelayComp(interpRB2Net,Gain12Out,Gain12OutD);
-
     pirelab.getDTCComp(interpRB2Net,Gain12OutD,outSignal(1),'Nearest','Saturate');
 
 
