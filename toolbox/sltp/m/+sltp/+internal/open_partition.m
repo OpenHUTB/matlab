f034bad910b6c70fef8e442816d9c629d2ec84c8
王海东 <wanghaidong@hutb.edu.cn>
2024-01-06 21:19:24 +0800

    更新 open_partition.m

--- toolbox/sltp/m/+sltp/+internal/open_partition.m
+++ toolbox/sltp/m/+sltp/+internal/open_partition.m
@@ -1,8 +1,5 @@
 function open_partition(mdl,tasks)
 
-
-
-
     handle=get_param(mdl,'Handle');
     editor=sltp.internal.ScheduleEditorManager.getEditor(handle);
     editor.show();
@@ -10,7 +7,6 @@ function open_partition(mdl,tasks)
     if~iscellstr(tasks)
         tasks={tasks};
     end
-
 
     ge=sltp.GraphEditor(handle);
     ge.setTaskHilite(tasks,true);
