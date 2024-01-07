da4d64fbee432ebde50fc78360dc3aff82e03469
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 18:05:07 +0800

    更新 verifyUint32Value.m

--- toolbox/shared/cgir_fe/+cgfe/+util/verifyUint32Value.m
+++ toolbox/shared/cgir_fe/+cgfe/+util/verifyUint32Value.m
@@ -1,7 +1,5 @@
 function aValue=verifyUint32Value(aPropName,aValue)
 
-
-
     if~isnumeric(aValue)||(double(uint32(aValue))~=double(aValue))
         me=MException('Simulink:tools:CGFEPropertyValueNotUInt32',...
         message('Simulink:tools:CGFEPropertyValueNotUInt32',...
