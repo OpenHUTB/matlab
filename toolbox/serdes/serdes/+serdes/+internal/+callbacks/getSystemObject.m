9163beeddef3dc5b2addf615a884fd17199d7f6f
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:47:57 +0800

    更新 getSystemObject.m

--- toolbox/serdes/serdes/+serdes/+internal/+callbacks/getSystemObject.m
+++ toolbox/serdes/serdes/+serdes/+internal/+callbacks/getSystemObject.m
@@ -1,19 +1,9 @@
-
-
-
-
-
-
-
 function systemObject=getSystemObject(rootBlock)
     systemObject=[];
-
-
     blocks=find_system(rootBlock,'FindAll','on',...
     'MatchFilter',@Simulink.match.internal.filterOutInactiveVariantSubsystemChoices,...
     'LookUnderMasks','on','FollowLinks','on',...
     'Type','block','BlockType','MATLABSystem');
-
 
     if length(blocks)==1
         systemName=get_param(blocks(1),'System');
