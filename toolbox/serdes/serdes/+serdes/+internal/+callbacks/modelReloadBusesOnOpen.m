1e59672e5b743f169789fd22415f94e1a22b2a93
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:56:26 +0800

    更新 modelReloadBusesOnOpen.m

--- toolbox/serdes/serdes/+serdes/+internal/+callbacks/modelReloadBusesOnOpen.m
+++ toolbox/serdes/serdes/+serdes/+internal/+callbacks/modelReloadBusesOnOpen.m
@@ -1,19 +1,11 @@
-
-
-
-
-
 function modelReloadBusesOnOpen(model)
     mws=get_param(model,'ModelWorkspace');
     requiredMWSElements=["TxTree","RxTree"];
     if~isempty(mws)&&all(arrayfun(@(x)mws.hasVariable(x),requiredMWSElements))
-
         preserveDirtyFlag=feval('Simulink.PreserveDirtyFlag',bdroot(model),'blockDiagram');%#ok<NASGU,FVAL>
-
         modelHandle=get_param(model,'handle');
         TxTree=mws.getVariable('TxTree');
         RxTree=mws.getVariable('RxTree');
-
 
         TxTree.modelHandle=modelHandle;
         RxTree.modelHandle=modelHandle;
@@ -23,7 +15,6 @@ function modelReloadBusesOnOpen(model)
         RxTree.createStructsAndParameters();
         RxTree.clearUndoStack
     end
-
 
     if mws.hasVariable('SerdesIBIS')
         SerdesIBIS=mws.getVariable('SerdesIBIS');
@@ -37,23 +28,16 @@ function modelReloadBusesOnOpen(model)
     RxBlockFound=false;
     currentTopLevelBlocks=find_system(model,'SearchDepth',1,'BlockType','SubSystem');
     for idxBlocks=1:size(currentTopLevelBlocks,1)
-
         blockOrigLibraryAndName=get_param(currentTopLevelBlocks{idxBlocks},'ReferenceBlock');
         if strcmp(blockOrigLibraryAndName,'serdesUtilities/Analog Channel')
-
             serdes.internal.callbacks.analogChannelUpdate(currentTopLevelBlocks{idxBlocks},"Initialization");
         end
-
-
-
         blockName=get_param(currentTopLevelBlocks{idxBlocks},'Name');
         if strcmp(blockName,'Tx')
             priorPriority=get_param(currentTopLevelBlocks{idxBlocks},'Priority');
             if isempty(priorPriority)
-
                 set_param(currentTopLevelBlocks{idxBlocks},'Priority','1');
             elseif~strcmp(priorPriority,'1')
-
                 warning(message('serdes:callbacks:PriorityPropertyValueNotRecognized',...
                 'Tx',num2str(priorPriority),'1'));
             end
@@ -61,10 +45,8 @@ function modelReloadBusesOnOpen(model)
         elseif strcmp(blockName,'Rx')
             priorPriority=get_param(currentTopLevelBlocks{idxBlocks},'Priority');
             if isempty(priorPriority)
-
                 set_param(currentTopLevelBlocks{idxBlocks},'Priority','2');
             elseif~strcmp(priorPriority,'2')
-
                 warning(message('serdes:callbacks:PriorityPropertyValueNotRecognized',...
                 'Rx',num2str(priorPriority),'2'));
             end
@@ -72,8 +54,6 @@ function modelReloadBusesOnOpen(model)
         end
     end
     if~TxBlockFound||~RxBlockFound
-
-
         warning(message('serdes:callbacks:TxorRxNotFound'))
     end
 end
