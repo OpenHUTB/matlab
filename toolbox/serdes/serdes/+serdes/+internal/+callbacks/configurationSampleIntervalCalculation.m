61705602758eced88a914846da8f4cd11c0b2e38
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:40:13 +0800

    更新 configurationSampleIntervalCalculation.m

--- toolbox/serdes/serdes/+serdes/+internal/+callbacks/configurationSampleIntervalCalculation.m
+++ toolbox/serdes/serdes/+serdes/+internal/+callbacks/configurationSampleIntervalCalculation.m
@@ -1,17 +1,9 @@
-
-
-
-
-
 function configurationSampleIntervalCalculation(block)
-
     simStatus=get_param(bdroot(block),'SimulationStatus');
     if strcmp(simStatus,'stopped')
-
         if~strcmp(get_param(block,'SymbolTime'),'NaN')
             symbolTime=slResolve(get_param(block,'SymbolTime'),bdroot(block));
             samplesPerSymbol=get_param(block,'SamplesPerSymbol');
-
             calculatedSampleInterval=symbolTime/str2double(samplesPerSymbol);
             set_param(block,'SampleInterval',serdes.internal.callbacks.numberToEngString(calculatedSampleInterval));
         else
