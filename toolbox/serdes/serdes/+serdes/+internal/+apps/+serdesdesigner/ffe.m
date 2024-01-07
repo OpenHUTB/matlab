a5619fbb4d4d7cbdb88048f9220981868c9ea366
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:29:10 +0800

    更新 ffe.m

--- toolbox/serdes/serdes/+serdes/+internal/+apps/+serdesdesigner/ffe.m
+++ toolbox/serdes/serdes/+serdes/+internal/+apps/+serdesdesigner/ffe.m
@@ -1,20 +1,21 @@
 classdef ffe<serdes.FFE&serdes.internal.serdesquicksimulation.SERDESElement
 
-
-
-
     methods
         function obj=ffe(varargin)
             obj@serdes.internal.serdesquicksimulation.SERDESElement(varargin{:});
         end
     end
 
+
     properties(Constant,Access=protected)
         HeaderDescription=getString(message('serdes:serdesdesigner:FfeHdrDesc'));
     end
+
+
     properties(Constant,Hidden)
         DefaultName='FFE';
     end
+
 
     methods(Hidden,Access=protected)
         function out=localClone(in)
@@ -22,4 +23,5 @@ classdef ffe<serdes.FFE&serdes.internal.serdesquicksimulation.SERDESElement
             copyProperties(in,out)
         end
     end
+
 end
