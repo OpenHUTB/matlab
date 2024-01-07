23fde4ac1871ec69d6d40212ff5a5f6c157c2e51
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 10:37:19 +0800

    更新 open.m

--- toolbox/sequencediagram/sl/m/+sequencediagram/+internal/open.m
+++ toolbox/sequencediagram/sl/m/+sequencediagram/+internal/open.m
@@ -1,11 +1,6 @@
 function open(modelName,sequencediagramName,debugMode)
-
-
-
-
     if nargin<3
         debugMode=false;
     end
-
     builtin('_open_sequence_diagram',modelName,sequencediagramName,debugMode);
 end
