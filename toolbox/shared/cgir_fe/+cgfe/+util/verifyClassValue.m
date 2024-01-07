e85ec1f2bb8811f3d7a3a6b4f5272e1c23a7b703
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 18:04:04 +0800

    更新 verifyClassValue.m

--- toolbox/shared/cgir_fe/+cgfe/+util/verifyClassValue.m
+++ toolbox/shared/cgir_fe/+cgfe/+util/verifyClassValue.m
@@ -1,7 +1,5 @@
 function aValue=verifyClassValue(aPropName,aClassname,aValue)
 
-
-
     if~isa(aValue,aClassname)
         me=MException('Simulink:tools:CGFEPropertyValueNotClassOf',...
         message('Simulink:tools:CGFEPropertyValueNotClassOf',...
