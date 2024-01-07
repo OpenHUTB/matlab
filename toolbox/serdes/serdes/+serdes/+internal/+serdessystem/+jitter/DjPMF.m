0b58a7b6204dd2c35077ab83fa5c3df7c83d4b7a
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 12:22:37 +0800

    更新 DjPMF.m

--- toolbox/serdes/serdes/+serdes/+internal/+serdessystem/+jitter/DjPMF.m
+++ toolbox/serdes/serdes/+serdes/+internal/+serdessystem/+jitter/DjPMF.m
@@ -1,23 +1,10 @@
 function pmf=DjPMF(t,tDj)
 
-
-
-
-
-
-
-
-
-
-
-
-
     narginchk(2,2)
     nargoutchk(0,1)
     fcnName='DjPMF';
     validateattributes(t,{'numeric'},{'vector','finite','real'},fcnName,'t',1);
     validateattributes(tDj,{'numeric'},{'scalar','finite'},fcnName,'tDj',2);
-
 
     pmf=double(abs(t)<tDj);
     vsum=sum(pmf);
