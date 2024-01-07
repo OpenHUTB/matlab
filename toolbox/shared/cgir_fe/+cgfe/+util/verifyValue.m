d48074995f7352865d00edf712e314f22aabaad4
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 18:05:23 +0800

    更新 verifyValue.m

--- toolbox/shared/cgir_fe/+cgfe/+util/verifyValue.m
+++ toolbox/shared/cgir_fe/+cgfe/+util/verifyValue.m
@@ -1,7 +1,4 @@
 function aValue=verifyValue(aPropName,aSetOfAllowedValue,aValue)
-
-
-
     if~all(ismember(aValue,aSetOfAllowedValue))
         me=MException('Simulink:tools:CGFEPropertyValueNotInSet',...
         message('Simulink:tools:CGFEPropertyValueNotInSet',...
