bfeeec3742e6bb7424187fc51fa636357795bb85
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:28:52 +0800

    更新 dfeCdr.m

--- toolbox/serdes/serdes/+serdes/+internal/+apps/+serdesdesigner/dfeCdr.m
+++ toolbox/serdes/serdes/+serdes/+internal/+apps/+serdesdesigner/dfeCdr.m
@@ -1,20 +1,21 @@
 classdef dfeCdr<serdes.DFECDR&serdes.internal.serdesquicksimulation.SERDESElement
 
-
-
-
     methods
         function obj=dfeCdr(varargin)
             obj@serdes.internal.serdesquicksimulation.SERDESElement(varargin{:});
         end
     end
 
+
     properties(Constant,Access=protected)
         HeaderDescription=getString(message('serdes:serdesdesigner:DfeCdrHdrDesc'));
     end
+
+
     properties(Constant,Hidden)
         DefaultName='DFECDR';
     end
+
 
     methods(Hidden,Access=protected)
         function out=localClone(in)
@@ -22,4 +23,5 @@ classdef dfeCdr<serdes.DFECDR&serdes.internal.serdesquicksimulation.SERDESElemen
             copyProperties(in,out)
         end
     end
+
 end
