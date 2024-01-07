94eb6d1a285aa60352b1bf6f99708b76e054e67d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:47:43 +0800

    更新 getSerDesTree.m

--- toolbox/serdes/serdes/+serdes/+internal/+callbacks/getSerDesTree.m
+++ toolbox/serdes/serdes/+serdes/+internal/+callbacks/getSerDesTree.m
@@ -1,23 +1,11 @@
-
-
-
-
-
-
-
 function varargout=getSerDesTree(block)
     varargout{1}=[];
     if nargout>1
         varargout{2}='';
     end
 
-
-
     blockParts=strsplit(block,'/');
     if size(blockParts,2)>=2
-
-
-
         subsystem=blockParts{2};
         if any(strcmp(subsystem,["Tx","Rx"]))
             treeName=[char(blockParts{2}),'Tree'];
