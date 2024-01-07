0c0d0b614f60b2292bac9c768fc30f57649f5e48
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 10:39:11 +0800

    更新 enable.m

--- toolbox/sequencediagram/quasi_annotation/enable.m
+++ toolbox/sequencediagram/quasi_annotation/enable.m
@@ -2,10 +2,12 @@ addToPath();
 setupBlockDiagramListeners();
 setupSimulinkRequirements();
 
+
 function addToPath()
     qaPath=getQAPath();
     addpath(qaPath);
 end
+
 
 function qaPath=getQAPath()
     thisScript=mfilename('fullpath');
@@ -13,10 +15,12 @@ function qaPath=getQAPath()
     qaPath=fullfile(thisDir,'m');
 end
 
+
 function setupBlockDiagramListeners()
     app=sequencediagram.quasiannotation.App.getInstance();
     app.addBlockDiagramCreatedCallback()
 end
+
 
 function setupSimulinkRequirements()
     rmi('register','linktype_rmi_sequenceDiagramQuasiAnnotation');
