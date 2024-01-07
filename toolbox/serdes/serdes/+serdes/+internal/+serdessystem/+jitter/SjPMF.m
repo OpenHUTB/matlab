60ccaf8f0b1875495f59f31f9f63a5d55a637be1
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 12:24:13 +0800

    更新 SjPMF.m

--- toolbox/serdes/serdes/+serdes/+internal/+serdessystem/+jitter/SjPMF.m
+++ toolbox/serdes/serdes/+serdes/+internal/+serdessystem/+jitter/SjPMF.m
@@ -1,34 +1,13 @@
 function pmf=SjPMF(t,tSj)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     narginchk(2,2)
     nargoutchk(0,1)
     fcnName='SjPMF';
     validateattributes(t,{'numeric'},{'vector','finite','real'},fcnName,'t',1);
     validateattributes(tSj,{'numeric'},{'scalar','finite'},fcnName,'tSj',2);
 
-
     pmf=real(1./(pi*sqrt(tSj^2-t.^2)));
 
-
     pmf(abs(t)==tSj)=0;
-
 
     vsum=sum(pmf);
     if vsum~=0
