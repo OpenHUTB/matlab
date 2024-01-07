e173cee77d70aacdc67f6695b7cf79a1d7964212
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:43:56 +0800

    更新 datapathDelete.m

--- toolbox/serdes/serdes/+serdes/+internal/+callbacks/datapathDelete.m
+++ toolbox/serdes/serdes/+serdes/+internal/+callbacks/datapathDelete.m
@@ -1,16 +1,6 @@
-
-
-
-
-
-
-
 function datapathDelete(block)
     tree=serdes.internal.callbacks.getSerDesTree(block);
     if~isempty(tree)
-
-
-
         blockInstanceName=get_param(block,'Name');
         tree.removeBlock(blockInstanceName,true);
 
