2f37e1cdd03a463010ea1474aa169ae0e788e213
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 10:36:08 +0800

    更新 destroySequenceDiagram.m

--- toolbox/sequencediagram/sl/m/+sequencediagram/+internal/destroySequenceDiagram.m
+++ toolbox/sequencediagram/sl/m/+sequencediagram/+internal/destroySequenceDiagram.m
@@ -1,15 +1,11 @@
 function destroySequenceDiagram(modelName,sequenceDiagramName)
 
-
-
-
     narginchk(2,2);
 
 
     if~bdIsLoaded(modelName)
         load_system(modelName);
     end
-
     sequencediagram.internal.validateSubdomain(modelName);
     builtin('_destroy_sequence_diagram',modelName,sequenceDiagramName);
 
