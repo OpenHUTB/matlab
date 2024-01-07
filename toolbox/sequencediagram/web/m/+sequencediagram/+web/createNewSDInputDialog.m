ce5e0c2a211e5b794668a17c295751c350c6e055
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 10:34:11 +0800

    更新 createNewSDInputDialog.m

--- toolbox/sequencediagram/web/m/+sequencediagram/+web/createNewSDInputDialog.m
+++ toolbox/sequencediagram/web/m/+sequencediagram/+web/createNewSDInputDialog.m
@@ -1,14 +1,10 @@
 function createNewSDInputDialog(rootUri)
-
-
     [mdlHandle,~]=builtin('_get_sl_object_instance_handle_from_sequence_diagram_uri','',rootUri);
     mdlName=get_param(mdlHandle,'Name');
 
     dp=DAStudio.DialogProvider;
     inputdlg=dp.inputdlg('Enter sequence diagram name:','New Sequence Diagram','',...
     @(newSDName)(sequencediagram.web.createSequenceDiagramAndOpen(mdlName,newSDName)));
-
-
     imd=DAStudio.imDialog.getIMWidgets(inputdlg);
     inputBox=imd.find('-isa','DAStudio.imEdit');
     inputBox.setFocus();
