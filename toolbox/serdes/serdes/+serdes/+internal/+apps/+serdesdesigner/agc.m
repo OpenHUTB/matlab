5109ae9bda865b5e67ab99c420a221aa2e3417b5
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 14:24:54 +0800

    更新 agc.m

--- toolbox/serdes/serdes/+serdes/+internal/+apps/+serdesdesigner/agc.m
+++ toolbox/serdes/serdes/+serdes/+internal/+apps/+serdesdesigner/agc.m
@@ -1,12 +1,11 @@
 classdef agc<serdes.AGC&serdes.internal.serdesquicksimulation.SERDESElement
 
-
-
     methods
         function obj=agc(varargin)
             obj@serdes.internal.serdesquicksimulation.SERDESElement(varargin{:});
         end
     end
+
 
     properties(Constant,Access=protected)
         HeaderDescription=getString(message('serdes:serdesdesigner:AgcHdrDesc'));
@@ -14,6 +13,7 @@ classdef agc<serdes.AGC&serdes.internal.serdesquicksimulation.SERDESElement
     properties(Constant,Hidden)
         DefaultName='AGC';
     end
+
 
     methods(Hidden,Access=protected)
         function out=localClone(in)
