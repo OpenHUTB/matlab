068d2ecdfe6466ea359c9239bdcfb2f0eb5daa13
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 10:35:55 +0800

    更新 createSequenceDiagram.m

--- toolbox/sequencediagram/sl/m/+sequencediagram/+internal/createSequenceDiagram.m
+++ toolbox/sequencediagram/sl/m/+sequencediagram/+internal/createSequenceDiagram.m
@@ -1,8 +1,5 @@
 function createSequenceDiagram(modelName,sequenceDiagramName)
 
-
-
-
     if nargin<1
         error('Not enough parameters!');
     end
@@ -15,7 +12,6 @@ function createSequenceDiagram(modelName,sequenceDiagramName)
             error(e.message);
         end
     end
-
     sequencediagram.internal.validateSubdomain(modelName);
     builtin('_create_sequence_diagram',modelName,sequenceDiagramName);
 
