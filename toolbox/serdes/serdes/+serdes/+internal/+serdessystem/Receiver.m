a12e82cfdd57320146668c43914f177c6959a7a3
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 12:17:10 +0800

    更新 Receiver.m

--- toolbox/serdes/serdes/+serdes/+internal/+serdessystem/Receiver.m
+++ toolbox/serdes/serdes/+serdes/+internal/+serdessystem/Receiver.m
@@ -1,13 +1,7 @@
 classdef Receiver<serdes.internal.serdessystem.Transceiver
 
-
-
-
-
     methods
-        function rx=Receiver(varargin)
-
-            rx=rx@serdes.internal.serdessystem.Transceiver(varargin{:});
+        function rx=Receiver(varargin)            rx=rx@serdes.internal.serdessystem.Transceiver(varargin{:});
             rx.Name='RX';
         end
     end
