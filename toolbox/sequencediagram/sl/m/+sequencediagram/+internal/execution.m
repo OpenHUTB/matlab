42614cb3bb6836eac36110f36b1dedbf6c2d0f59
王海东 <wanghaidong@hutb.edu.cn>
2024-01-07 10:36:31 +0800

    更新 execution.m

--- toolbox/sequencediagram/sl/m/+sequencediagram/+internal/execution.m
+++ toolbox/sequencediagram/sl/m/+sequencediagram/+internal/execution.m
@@ -1,19 +1,14 @@
 classdef(Hidden)execution
 
-
-
-
     methods(Hidden,Static)
         function enable()
-
-
-
             sequencediagram.internal.execution.loadLibraries();
             slfeature('SequenceDiagramExecutionToolstrip',1);
             slfeature('ObserverMessageSupport',1);
             slfeature('SequenceDiagramZCIntegExecutionToolstrip',1);
             slfeature('SDMessageOperandOrphanHighlighting',1);
         end
+
 
         function disable()
             sequencediagram.internal.execution.loadLibraries();
@@ -24,20 +19,9 @@ classdef(Hidden)execution
         end
     end
 
+
     methods(Access=private,Static)
         function loadLibraries()
-
-
-
-
-
-
-
-
-
-
-
-
             sequencediagram.internal.sl.kernel.getSimulinkApp();
 
         end
