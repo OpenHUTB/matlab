7e302a1add72d2af33c5d38a99fee4a187d7ad0e
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 21:39:51 +0800

    更新 createArchitectureIterator.m

--- toolbox/sysarch/sysarch_api/+systemcomposer/createArchitectureIterator.m
+++ toolbox/sysarch/sysarch_api/+systemcomposer/createArchitectureIterator.m
@@ -1,13 +1,8 @@
 function iterator=createArchitectureIterator(traversalOrder)
 
-
-
-
-
     if nargin<1
         traversalOrder=systemcomposer.IteratorDirection.PreOrder;
     end
-
     iterator=internal.systemcomposer.ArchitectureIterator(traversalOrder);
 
 end
