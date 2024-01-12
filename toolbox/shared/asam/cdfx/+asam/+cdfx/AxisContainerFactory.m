b741b9de210c491314e1410cf3923428d82f5f11
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 21:49:59 +0800

    更新 AxisContainerFactory.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/AxisContainerFactory.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/AxisContainerFactory.m
@@ -1,25 +1,16 @@
 function instance=AxisContainerFactory(root,sys,inst)
 
-
-
-
-
-
     category=asam.cdfx.mf0.getCategory(inst);
-
 
     switch category
     case "FIX_AXIS"
-
         instance=asam.cdfx.axis.FixedAxis(inst);
     case "STD_AXIS"
-
         instance=asam.cdfx.axis.StandardAxis(inst);
     case "COM_AXIS"
 
         instance=asam.cdfx.axis.CommonAxis(inst);
     case "CURVE_AXIS"
-
         instance=asam.cdfx.axis.CurveAxis(inst);
     case "RES_AXIS"
 
