63a66a1f7ac6044968899881e2ffcd2f5e35429e
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 23:03:37 +0800

    更新 CommonAxis.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/+instance/CommonAxis.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/+instance/CommonAxis.m
@@ -1,44 +1,27 @@
 classdef CommonAxis<asam.cdfx.instance.Instance
 
-
-
-
     properties
 ArrayDims
         ReferencingInstances asam.cdfx.instance.Instance
 ReferencingAxisIndices
     end
 
+
     methods
         function obj=CommonAxis(root,sys,inst)
-
-
-
             obj=obj@asam.cdfx.instance.Instance(root,sys,inst);
-
-
             obj.PhysicalValue=obj.getPhysicalValues();
             obj.Value=obj.PhysicalValue;
-
-
             obj.InstanceElement=obj.ParameterSet.SW_VALUE_CONT.SW_VALUES_PHYS.V.toArray;
-
-
             obj.ArrayDims=size(obj.PhysicalValue);
         end
 
+
         function setValue(obj,value,~)
-
-
-
-
             value=convertCharsToStrings(value);
-
-
             if~isnumeric(value)&&~isstring(value)&&~ischar(value)
                 error(message('asam_cdfx:CDFX:CategoryValueTypeMismatch',obj.Category));
             end
-
             if~all(size(value)==size(obj.PhysicalValue))
                 error(message('asam_cdfx:CDFX:CategoryValueSizeMismatch',obj.Category));
             end
@@ -51,43 +34,25 @@ ReferencingAxisIndices
                 error(message('asam_cdfx:CDFX:TextValueElementMismatch',obj.ShortName));
             end
 
-
             obj.Value=value;
             obj.PhysicalValue=value;
-
 
             for idx=1:numel(obj.InstanceElement)
                 obj.InstanceElement(idx).elementValue=string(value(idx));
             end
 
-
-
-
             for idx=1:numel(obj.ReferencingInstances)
-
                 instanceValue=getValue(obj.Root,obj.ReferencingInstances(idx).ShortName,obj.ParentSystem.ShortName);
-
-
-
                 axisPropertyName="Axis"+num2str(obj.ReferencingAxisIndices(idx));
 
-
                 instanceValue.(axisPropertyName).PhysicalValue=value;
-
-
-
                 setValueInternal(obj.Root,obj.ReferencingInstances(idx).ShortName,obj.ParentSystem.ShortName,instanceValue);
             end
 
         end
 
+
         function physVals=getPhysicalValues(obj)
-
-
-
-
-
-
 
             switch obj.ValueType
             case "V"
@@ -102,8 +67,6 @@ ReferencingAxisIndices
                 closeString="}";
             end
 
-
-
             value=openString;
             for vIndex=1:V.Size
                 if vIndex>1
@@ -113,36 +76,24 @@ ReferencingAxisIndices
             end
             value=value+closeString;
 
-
             switch obj.ValueType
             case "V"
 
                 physVals=eval(value);
             case "VT"
-
-
                 physVals=eval(convertCharsToStrings(value));
-
-
-
                 physVals=convertCharsToStrings(physVals);
             end
         end
 
+
         function addReferencingInstance(obj,refInstance,axisIndex)
-
-
-
-
             obj.ReferencingInstances(end+1)=refInstance;
             obj.ReferencingAxisIndices(end+1)=axisIndex;
         end
 
+
         function valType=getValueType(~,valueContainer)
-
-
-
-
             if~isequal(valueContainer.SW_VALUES_PHYS.V.Size,0)
                 valType="V";
             else
