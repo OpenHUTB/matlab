2c707e5867a3c04ba0f9b3c9a967dc6390ba3d15
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 21:52:39 +0800

    更新 AxisContainer.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/AxisContainer.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/AxisContainer.m
@@ -1,27 +1,17 @@
 classdef(Abstract)AxisContainer<matlab.mixin.Heterogeneous
 
-
-
-
     properties
-
-
-
         Category string
-
         Units string
-
 ArrayDims
 
-
-
         IsReferencedAxis logical
-
 
 PhysicalValue
 
 AxisContElement
     end
+
 
     methods
         function isRefAxis=isReferencedAxis(obj)
