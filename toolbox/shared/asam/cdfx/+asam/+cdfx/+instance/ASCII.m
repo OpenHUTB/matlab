30c8758967f8209a7dfe11ed4a826fc3387a9cb6
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 22:00:24 +0800

    更新 ASCII.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/+instance/ASCII.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/+instance/ASCII.m
@@ -1,29 +1,16 @@
 classdef ASCII<asam.cdfx.instance.Instance
 
-
-
-
     methods
         function obj=ASCII(root,sys,inst)
-
-
-
             obj=obj@asam.cdfx.instance.Instance(root,sys,inst);
-
-
             obj.InstanceElement=obj.ParameterSet.SW_VALUE_CONT.SW_VALUES_PHYS.VT;
-
-
             obj.PhysicalValue=obj.getPhysicalValues();
             obj.Value=obj.PhysicalValue;
 
         end
 
+
         function setValue(obj,value,~)
-
-
-
-
             if~isstring(value)&&~ischar(value)
                 error(message('asam_cdfx:CDFX:CategoryValueTypeMismatch',obj.Category));
             end
@@ -32,25 +19,18 @@ classdef ASCII<asam.cdfx.instance.Instance
                 error(message('asam_cdfx:CDFX:CategoryValueSizeMismatch',obj.Category));
             end
 
-
             obj.Value=value;
             obj.PhysicalValue=value;
-
-
             obj.InstanceElement(1).elementValue=string(value);
         end
 
+
         function physVals=getPhysicalValues(obj)
-
-
-
             physVals=string(obj.InstanceElement(1).elementValue);
         end
 
+
         function valType=getValueType(~,~)
-
-
-
             valType="VT";
         end
     end
