f0eb3fb4f8abbd3e266dd362b24af008054e1f6d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 21:55:14 +0800

    更新 FixedAxis.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/FixedAxis.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/FixedAxis.m
@@ -1,34 +1,16 @@
 classdef FixedAxis<asam.cdfx.axis.AxisContainer
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     methods
         function obj=FixedAxis(axisCont)
-
 
             obj.Category="FIX_AXIS";
             obj.IsReferencedAxis=false;
 
             obj.AxisContElement=axisCont;
-
             obj.Units=string(obj.AxisContElement.UNIT_DISPLAY_NAME.elementValue);
-
             obj.ArrayDims=zeros(1,ndims(obj.AxisContElement.SW_VALUES_PHYS.V.toArray));
             elementArray=obj.AxisContElement.SW_VALUES_PHYS.V.toArray;
             obj.ArrayDims(:)=size(elementArray);
-
 
             for idx=1:numel(elementArray)
                 obj.PhysicalValue=[obj.PhysicalValue,str2double(elementArray(idx).elementValue)];
