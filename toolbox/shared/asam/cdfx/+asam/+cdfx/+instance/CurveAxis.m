722075f5e3b076a7966df6bf2e3493fa5481cb23
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 23:12:26 +0800

    更新 CurveAxis.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/+instance/CurveAxis.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/+instance/CurveAxis.m
@@ -1,48 +1,33 @@
 classdef CurveAxis<asam.cdfx.instance.Instance
 
-
-
-
     properties
 
 SWAxisContainer
 
 HasVG
 
-
 ArrayDims
         ReferencingInstances asam.cdfx.instance.Instance
 ReferencingAxisIndices
     end
 
+
     methods
         function obj=CurveAxis(root,sys,inst)
-
-
-
             obj=obj@asam.cdfx.instance.Instance(root,sys,inst);
-
-
             obj.PhysicalValue=obj.getPhysicalValues();
-
-
             obj.ArrayDims=size(obj.PhysicalValue);
-
-
             contElementArray=obj.ParameterSet.SW_AXIS_CONTS.SW_AXIS_CONT.toArray;
 
-
             obj.InstanceElement=obj.ParameterSet.SW_VALUE_CONT.SW_VALUES_PHYS.V.toArray;
-
 
             for idx=1:numel(contElementArray)
                 obj.SWAxisContainer=[obj.SWAxisContainer,asam.cdfx.AxisContainerFactory(root,sys,contElementArray(idx))];
             end
         end
+
+
         function setValue(obj,value,isInternalModify)
-
-
-
 
             if~isstruct(value)
                 error(message('asam_cdfx:CDFX:CategoryValueTypeMismatch',obj.Category));
@@ -60,15 +45,11 @@ ReferencingAxisIndices
                 error(message('asam_cdfx:CDFX:TextValueElementMismatch',obj.ShortName));
             end
 
-
             for idx=1:numel(obj.InstanceElement)
                 obj.InstanceElement(idx).elementValue=string(value.PhysicalValue(idx));
             end
-
-
             if~obj.SWAxisContainer.isReferencedAxis()
                 elementArray=obj.SWAxisContainer.AxisContElement.SW_VALUES_PHYS.V.toArray;
-
 
                 for idx=1:numel(elementArray)
                     elementArray(idx).elementValue=string(value.Axis1.PhysicalValue(idx));
@@ -78,41 +59,24 @@ ReferencingAxisIndices
                 error(message('asam_cdfx:CDFX:ModifyReferencedAxis',obj.ShortName,obj.SWAxisContainer.InstanceReference));
             end
 
-
             obj.Value=value;
             obj.PhysicalValue=value.PhysicalValue;
-
-
-
 
             for idx=1:numel(obj.ReferencingInstances)
 
                 instanceValue=getValue(obj.Root,obj.ReferencingInstances(idx).ShortName,obj.ParentSystem.ShortName);
 
-
-
                 axisPropertyName="Axis"+num2str(obj.ReferencingAxisIndices(idx));
-
 
                 instanceValue.(axisPropertyName).PhysicalValue=value.PhysicalValue;
                 instanceValue.(axisPropertyName).Axis1=value.Axis1;
-
-
-
-
                 setValueInternal(obj.Root,obj.ReferencingInstances(idx).ShortName,obj.ParentSystem.ShortName,instanceValue);
             end
 
         end
 
+
         function physVals=getPhysicalValues(obj)
-
-
-
-
-
-
-
 
             switch obj.ValueType
             case "V"
@@ -125,15 +89,12 @@ ReferencingAxisIndices
                 closeString="}";
             end
 
-
             switch obj.ValueType
             case "V"
                 V=obj.ValueContainerElement.SW_VALUES_PHYS.V;
             case "VT"
                 V=obj.ValueContainerElement.SW_VALUES_PHYS.VT;
             end
-
-
 
             value=openString;
             for vIndex=1:V.Size
@@ -144,36 +105,24 @@ ReferencingAxisIndices
             end
             value=value+closeString;
 
-
             switch obj.ValueType
             case "V"
 
                 physVals=eval(value);
             case "VT"
 
-
                 physVals=eval(convertCharsToStrings(value));
-
-
-
                 physVals=convertCharsToStrings(physVals);
             end
         end
 
+
         function resolveAxisValues(obj)
-
-
-
-
             if(~obj.SWAxisContainer.isReferencedAxis())
                 axisVals=struct("ReferenceName","","Category",obj.SWAxisContainer.Category,"PhysicalValue",obj.SWAxisContainer.PhysicalValue,"IsReferenced",false);
             else
-
-                axisInstanceName=obj.SWAxisContainer.InstanceReference;
-
-
+               axisInstanceName=obj.SWAxisContainer.InstanceReference;
                 referencedInstance=obj.ParentSystem.SystemInstanceTable(strcmp(obj.ParentSystem.SystemInstanceTable.ShortName,axisInstanceName),:);
-
 
                 actualReferenceSize=size(referencedInstance.ObjectHandles(1).PhysicalValue,1);
                 containerSize=actualReferenceSize;
@@ -183,38 +132,25 @@ ReferencingAxisIndices
                         error(message('asam_cdfx:CDFX:ContainerReferenceTooLarge',1,obj.ShortName,obj.SWAxisContainer.InstanceReference));
                     end
                 end
-
-
                 axisVals=struct("ReferenceName",referencedInstance.ObjectHandles(1).ShortName,"Category",referencedInstance.ObjectHandles(1).Category,"PhysicalValue",referencedInstance.ObjectHandles(1).PhysicalValue(1:containerSize,:),"IsReferenced",true);
-
-
 
                 if axisVals.Category=="CURVE_AXIS"
                     referencedInstance.ObjectHandles(1).resolveAxisValues();
                     axisVals.Axis1=referencedInstance.ObjectHandles(1).Value.Axis1;
                 end
-
-
                 referencedInstance.ObjectHandles(1).addReferencingInstance(obj,1);
             end
-
             obj.Value=struct("PhysicalValue",obj.PhysicalValue,"Axis1",axisVals);
         end
 
+
         function addReferencingInstance(obj,refInstance,axisIndex)
-
-
-
-
             obj.ReferencingInstances(end+1)=refInstance;
             obj.ReferencingAxisIndices(end+1)=axisIndex;
         end
 
+
         function valType=getValueType(~,valueContainer)
-
-
-
-
             if~isequal(valueContainer.SW_VALUES_PHYS.V.Size,0)
                 valType="V";
             else
