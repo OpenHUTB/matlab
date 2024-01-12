8d4ca04969a677322c969bd78d353543a0329d78
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 21:53:36 +0800

    更新 CommonAxis.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/CommonAxis.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/CommonAxis.m
@@ -1,46 +1,24 @@
 classdef CommonAxis<asam.cdfx.axis.AxisContainer
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     properties
 
 InstanceReference
 ReferenceSize
     end
 
+
     methods
         function obj=CommonAxis(axisCont)
 
-
             obj.Category="COM_AXIS";
 
-
             obj.AxisContElement=axisCont;
-
-
-
             obj.InstanceReference=string(obj.AxisContElement.SW_INSTANCE_REF.elementValue);
-
 
             obj.ReferenceSize=[];
             if~isempty(obj.AxisContElement.SW_ARRAYSIZE)
                 obj.ReferenceSize=str2double(obj.AxisContElement.SW_ARRAYSIZE.V(1).elementValue);
             end
-
-
-
 
             obj.IsReferencedAxis=true;
         end
