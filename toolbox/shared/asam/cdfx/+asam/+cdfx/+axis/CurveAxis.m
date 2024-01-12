9c1756ac5095fe7c1c9f786f68e9f0023f5559b0
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 21:54:32 +0800

    更新 CurveAxis.m

--- toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/CurveAxis.m
+++ toolbox/shared/asam/cdfx/+asam/+cdfx/+axis/CurveAxis.m
@@ -1,44 +1,22 @@
 classdef CurveAxis<asam.cdfx.axis.AxisContainer
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     properties
-
 
 InstanceReference
 ReferenceSize
     end
 
+
     methods
         function obj=CurveAxis(axisCont)
             obj.Category="CURVE_AXIS";
-
-
             obj.AxisContElement=axisCont;
-
-
-
             obj.InstanceReference=string(obj.AxisContElement.SW_INSTANCE_REF.elementValue);
-
 
             obj.ReferenceSize=[];
             if~isempty(obj.AxisContElement.SW_ARRAYSIZE)
                 obj.ReferenceSize=str2double(obj.AxisContElement.SW_ARRAYSIZE.V(1).elementValue);
             end
-
-
 
             obj.IsReferencedAxis=true;
         end
