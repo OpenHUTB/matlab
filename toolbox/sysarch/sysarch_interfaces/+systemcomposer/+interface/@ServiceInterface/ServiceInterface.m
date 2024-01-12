5b8645f2abdb5ce03fb78ccfc0ff39d4d6f852f7
王海东 <wanghaidong@hutb.edu.cn>
2024-01-12 21:35:11 +0800

    更新 ServiceInterface.m

--- toolbox/sysarch/sysarch_interfaces/+systemcomposer/+interface/@ServiceInterface/ServiceInterface.m
+++ toolbox/sysarch/sysarch_interfaces/+systemcomposer/+interface/@ServiceInterface/ServiceInterface.m
@@ -1,9 +1,5 @@
 classdef ServiceInterface<systemcomposer.base.StereotypableElement&systemcomposer.base.BaseElement
 
-
-
-
-
     properties(SetAccess=private)
 Dictionary
 Name
@@ -27,6 +23,7 @@ Model
         end
     end
 
+
     methods(Static,Hidden)
         function incheck(inval)
             persistent p
@@ -38,12 +35,10 @@ Model
         end
     end
 
+
     methods
 
         function m=get.Model(this)
-
-
-
             m=systemcomposer.arch.Model.empty;
             catalog=this.getImpl().getCatalog();
             catalogOwnerName=catalog.getStorageSource;
@@ -53,33 +48,37 @@ Model
             end
         end
 
+
         function dictionary=get.Dictionary(this)
             dictionary=systemcomposer.internal.getWrapperForImpl(this.getImpl().getCatalog(),'systemcomposer.interface.Dictionary');
         end
+
 
         function name=get.Name(this)
             name=this.getImpl().getName;
         end
 
+
         function setName(this,name)
             systemcomposer.interface.ServiceInterface.incheck(name);
-
             isModelContext=isempty(this.Dictionary.ddConn);
             sourceName=this.Dictionary.getSourceName;
             systemcomposer.BusObjectManager.RenameInterface(sourceName,isModelContext,this.Name,name);
         end
 
+
         function desc=get.Description(this)
             desc=this.getImpl().getDescription;
         end
 
+
         function setDescription(this,desc)
             systemcomposer.interface.ServiceInterface.incheck(desc);
-
             isModelContext=isempty(this.Dictionary.ddConn);
             sourceName=this.Dictionary.getSourceName;
             systemcomposer.BusObjectManager.SetInterfaceDescription(sourceName,isModelContext,this.Name,desc);
         end
+
 
         function elements=get.Elements(this)
             interfaceImpl=this.getImpl();
@@ -90,34 +89,32 @@ Model
             end
         end
 
+
         function element=addElement(this,elementName,varargin)
             p=inputParser;
             addRequired(p,'elementName',@(x)ischar(x)||isstring(x));
             addParameter(p,'FunctionPrototype','y = f0(u)',@(x)ischar(x)&&~isempty(x)||isstring(x)&&~isequal(x,""));
             parse(p,elementName,varargin{:});
             results=p.Results;
-
             isModelContext=isempty(this.Dictionary.ddConn);
             sourceName=this.Dictionary.getSourceName;
 
             if nargin>2
-
                 systemcomposer.BusObjectManager.AddFunctionElement(sourceName,isModelContext,this.Name,elementName,results);
             else
                 systemcomposer.BusObjectManager.AddFunctionElement(sourceName,isModelContext,this.Name,elementName);
             end
-
-
-
             elementImpl=this.getImpl.getElement(elementName);
             element=systemcomposer.internal.getWrapperForImpl(elementImpl,'systemcomposer.interface.FunctionElement');
         end
+
 
         function removeElement(this,elementName)
             isModelContext=isempty(this.Dictionary.ddConn);
             sourceName=this.Dictionary.getSourceName;
             systemcomposer.BusObjectManager.DeleteInterfaceElement(sourceName,isModelContext,this.Name,elementName);
         end
+
 
         function element=getElement(this,elementName)
             elementImpl=this.getImpl().getElement(elementName);
