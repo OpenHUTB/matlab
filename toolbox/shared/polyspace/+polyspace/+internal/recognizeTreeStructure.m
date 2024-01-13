a52b0c57dd68b3d79b0f368ea96f68ea65e6c7ea
王海东 <wanghaidong@hutb.edu.cn>
2024-01-13 18:02:32 +0800

    更新 recognizeTreeStructure.m

--- toolbox/shared/polyspace/+polyspace/+internal/recognizeTreeStructure.m
+++ toolbox/shared/polyspace/+polyspace/+internal/recognizeTreeStructure.m
@@ -1,18 +1,12 @@
-
-
 function[recognizedPrefix,matchingRoots]=...
     recognizeTreeStructure(inputPaths,from)
 
     narginchk(2,2);
 
-
     currDir=pwd;
-
 
     validateattributes(inputPaths,{'cell'},{});
     validateattributes(from,{'char'},{});
-
-
 
     FILESYSTEM_RECOGNIZE_TREE_STRUCTURE=3;
     [recognizedPrefix,matchingRoots]=...
