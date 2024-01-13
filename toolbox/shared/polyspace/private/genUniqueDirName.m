2fc40f02811054a158b1ef845224fde351b50c41
王海东 <wanghaidong@hutb.edu.cn>
2024-01-13 17:30:06 +0800

    更新 genUniqueDirName.m

--- toolbox/shared/polyspace/private/genUniqueDirName.m
+++ toolbox/shared/polyspace/private/genUniqueDirName.m
@@ -1,21 +1,20 @@
+% 获得以索引数字结尾的唯一目录
 function[dirName,existingDir]=genUniqueDirName(dirName)
-
-
-
 
     rootDir=dirName;
     existingDir='';
     index=0;
 
+    % windows 下如果以 \ 开头则表示是相对路径，前面需要补充当前路径
     if ispc&&startsWith(rootDir,filesep)
         rootDir=fullfile(pwd,rootDir);
     end
 
+    % 后面添加表示第几个文件的数字，如果存在则索引+1
     while exist(rootDir,'dir')==7
-
         existingDir=rootDir;
-
         index=index+1;
         rootDir=sprintf('%s_%d',dirName,index);
     end
+
     dirName=rootDir;
