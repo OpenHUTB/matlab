cf3223eb78d611961788e785e39079376dfaa5a2
王海东 <wanghaidong@hutb.edu.cn>
2024-01-13 17:23:22 +0800

    更新 extractMdlRefs.m

--- toolbox/shared/polyspace/private/extractMdlRefs.m
+++ toolbox/shared/polyspace/private/extractMdlRefs.m
@@ -1,42 +1,20 @@
 function mdlRefList=extractMdlRefs(systemName,stopLevel)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     mdlRefList={};
     if stopLevel==0
         return
     end
 
-
-
     mdlRefSet=containers.Map({'fake'},{false});
     mdlRefSet.remove('fake');
 
-
-
-
-
     modelName=bdroot(systemName);
     try
-
-
         fcn=@()find_mdlrefs(modelName,'MatchFilter',@Simulink.match.internal.filterOutCodeInactiveVariantSubsystemChoices,'AllLevels',false);
         [unused,unused1,mdlRefBlks]=evalc('fcn()');%#ok<ASGLU>
     catch Me
         rethrow(Me);
     end
-
 
     if~strcmp(modelName,systemName)
         badIdx=[];
@@ -49,13 +27,11 @@ function mdlRefList=extractMdlRefs(systemName,stopLevel)
         mdlRefBlks(badIdx)=[];
     end
 
-
     for ii=1:numel(mdlRefBlks)
         mdlRefName=get_param(mdlRefBlks{ii},'ModelName');
         mdlRefSet(mdlRefName)=true;
         iFindMdlRefs(mdlRefSet,mdlRefName,1,stopLevel)
     end
-
 
     mdlRefList=mdlRefSet.keys();
     mdlRefList=mdlRefList(:);
@@ -65,22 +41,16 @@ end
 
 function iFindMdlRefs(mdlRefSet,currModelName,currLevel,stopLevel)
 
-
     if currLevel>=stopLevel
         return
     end
 
-
-
     try
-
-
         fcn=@()find_mdlrefs(currModelName,'MatchFilter',@Simulink.match.internal.filterOutCodeInactiveVariantSubsystemChoices,'AllLevels',false);
         [unused,currMdlRefs]=evalc('fcn()');%#ok<ASGLU>
     catch Me
         rethrow(Me);
     end
-
 
     currLevel=currLevel+1;
     for ii=1:numel(currMdlRefs)-1
