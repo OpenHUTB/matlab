4ff4f8e77e9a4dce4b0b2cbb1581b7b81599906c
王海东 <wanghaidong@hutb.edu.cn>
2024-01-13 17:56:37 +0800

    更新 isInSubDirectory.m

--- toolbox/shared/polyspace/+polyspace/+internal/isInSubDirectory.m
+++ toolbox/shared/polyspace/+polyspace/+internal/isInSubDirectory.m
@@ -1,19 +1,8 @@
 function res=isInSubDirectory(path1,path2)
-
-
-
-
-
-
-
-
-
     path1=polyspace.internal.getAbsolutePath(path1);
     path2=polyspace.internal.getAbsolutePath(path2);
 
-
     currDir=pwd;
-
 
     FILESYSTEM_IS_IN_DIRECTORY=2;
     res=filesystem_mex(FILESYSTEM_IS_IN_DIRECTORY,currDir,path1,path2);
