128342eda8d317a5520b507c5692a5cea58a0b21
王海东 <wanghaidong@hutb.edu.cn>
2024-01-13 17:52:42 +0800

    更新 getAbsolutePath.m

--- toolbox/shared/polyspace/+polyspace/+internal/getAbsolutePath.m
+++ toolbox/shared/polyspace/+polyspace/+internal/getAbsolutePath.m
@@ -1,7 +1,4 @@
-
-
 function filePath=getAbsolutePath(filePath,currDir)
-
 
     narginchk(1,2);
 
@@ -10,16 +7,13 @@ function filePath=getAbsolutePath(filePath,currDir)
         currDir=pwd;
     end
 
-
     if isempty(filePath)
         filePath=currDir;
         return
     end
 
-
     validateattributes(filePath,{'char'},{'row'},mfilename('class'),'',1)
     validateattributes(currDir,{'char'},{'row'},mfilename('class'),'',1)
-
 
     if isunix&&strncmp(filePath,'~/',2)
         res=deblank(getenv('HOME'));
@@ -27,8 +21,6 @@ function filePath=getAbsolutePath(filePath,currDir)
             filePath=[res,filePath(2:end)];
         end
     end
-
-
 
     FILESYSTEM_GETABSOLUTEPATH=1;
     filePath=filesystem_mex(FILESYSTEM_GETABSOLUTEPATH,currDir,filePath);
