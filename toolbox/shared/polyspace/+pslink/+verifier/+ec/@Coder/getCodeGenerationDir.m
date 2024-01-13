2b31f505087217cbed28d6bb1af2709f477c974d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-13 19:24:47 +0800

    更新 getCodeGenerationDir.m

--- toolbox/shared/polyspace/+pslink/+verifier/+ec/@Coder/getCodeGenerationDir.m
+++ toolbox/shared/polyspace/+pslink/+verifier/+ec/@Coder/getCodeGenerationDir.m
@@ -1,26 +1,15 @@
-
-
-
-
 function cgDirInfo=getCodeGenerationDir(systemName)
 
-
     cgDirInfo=cell(0,2);
-
     [codeGenDir,codeGenName]=targetsprivate('targets_get_build_dir',systemName);
     if~isempty(codeGenDir)&&~isempty(codeGenName)
         cgDirInfo={codeGenDir,codeGenName};
     else
-
-
         systemsNames=split(systemName,'/');
         if strcmpi(systemsNames{1},systemsNames{end})
             hdl=get_param(systemName,'Handle');
             t=Simulink.ModelReference.Conversion.NameUtils;
             validName=t.getValidModelName(hdl);
-
-
-
             [codeGenDir,codeGenName]=targetsprivate('targets_get_build_dir',bdroot(systemName));
             if~isempty(codeGenDir)&&~isempty(codeGenName)
                 [beginIdx,endIdx]=regexp(codeGenDir,bdroot(systemName));
