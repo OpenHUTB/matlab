a33c59f4cc860a07a16b87952fe014ea8a22df1b
王海东 <wanghaidong@hutb.edu.cn>
2024-01-13 17:32:32 +0800

    更新 getOrCreateDir.m

--- toolbox/shared/polyspace/private/getOrCreateDir.m
+++ toolbox/shared/polyspace/private/getOrCreateDir.m
@@ -1,16 +1,8 @@
 function aDir=getOrCreateDir(aDir)
 
-
-
-
-
     rtw_checkdir();
-
     if~polyspace.internal.isAbsolutePath(aDir)
         aDir=fullfile(pwd,aDir);
     end
     aDir=polyspace.internal.getAbsolutePath(aDir);
-
-
-
     aDir=rtwprivate('rtw_create_directory_path',aDir,'');
