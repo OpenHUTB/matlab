645c71e01097791db97150c510013670b0a5836e
王海东 <wanghaidong@hutb.edu.cn>
2024-01-14 13:08:48 +0800

    更新 mustBeNonEmptyString.m

--- toolbox/shared/tdms/+matlab/+io/+tdms/+internal/+validator/mustBeNonEmptyString.m
+++ toolbox/shared/tdms/+matlab/+io/+tdms/+internal/+validator/mustBeNonEmptyString.m
@@ -1,9 +1,4 @@
 function mustBeNonEmptyString(text)
-
-
-
-
-
     if matlab.io.tdms.internal.utility.isEmptyString(text)
         throwAsCaller(MException(message("MATLAB:validators:mustBeNonempty")));
     end
