709931bbdd2fd0295034e8cc35333815ed7e4e6d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-14 13:10:20 +0800

    更新 mustBeRegularTimeTable.m

--- toolbox/shared/tdms/+matlab/+io/+tdms/+internal/+validator/mustBeRegularTimeTable.m
+++ toolbox/shared/tdms/+matlab/+io/+tdms/+internal/+validator/mustBeRegularTimeTable.m
@@ -1,8 +1,4 @@
 function mustBeRegularTimeTable(value)
-
-
-
-
     if~(istimetable(value)&&isregular(value))
         eid="tdms:TDMS:mustBeRegularTimeTable";
         throwAsCaller(MException(eid,message(eid,class(value))));
