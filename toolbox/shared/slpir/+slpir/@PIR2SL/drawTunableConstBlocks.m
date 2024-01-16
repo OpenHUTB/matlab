bd613349c35166518f1b3e7246af3c1e5975f327
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 22:41:38 +0800

    更新 drawTunableConstBlocks.m

--- toolbox/shared/slpir/+slpir/@PIR2SL/drawTunableConstBlocks.m
+++ toolbox/shared/slpir/+slpir/@PIR2SL/drawTunableConstBlocks.m
@@ -1,30 +1,19 @@
 function drawTunableConstBlocks(tunablePorts,DUTName,subsystemOnDUT)
 
-
-
-
-
-
     if~isempty(tunablePorts)
 
         portsOnDUT=keys(tunablePorts);
-
         posOfDUT=get_param([subsystemOnDUT,'/',DUTName],'Position');
 
         constBlkSize=[30,30];
         moveDown=[0,50];
-
         posOfBlk=[posOfDUT(1)-60,posOfDUT(2)-30];
 
         for ii=1:numel(portsOnDUT)
 
             portName=char(portsOnDUT(ii));
 
-
-
-
             portInfo=tunablePorts(portName);
-
             constBlkName=[subsystemOnDUT,'/','tunable_const'];
             constBlkHandle=add_block('built-in/Constant',constBlkName,'MakeNameUnique','on','Value',portName);
 
@@ -36,7 +25,6 @@ function drawTunableConstBlocks(tunablePorts,DUTName,subsystemOnDUT)
 
             posOfBlk=posOfBlk+moveDown;
             set_param(constBlkHandle,'Position',[posOfBlk,posOfBlk+constBlkSize]);
-
             srcPort=[get_param(constBlkHandle,'Name'),'/','1'];
             dstPort=[DUTName,'/',get_param(portInfo.SLPortHandle,'Port')];
             add_line(subsystemOnDUT,srcPort,dstPort,'autorouting','on');
