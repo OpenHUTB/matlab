2b9b4ef4bc6671d2966a07ad57b4f8cc09ab9f35
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:12:28 +0800

    更新 getCurrentRelease.m

--- toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/getCurrentRelease.m
+++ toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/getCurrentRelease.m
@@ -1,33 +1,9 @@
 function relealseStr=getCurrentRelease()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     relealseStr=getenv('SUPPORTPACKAGEROOT_MATLAB_RELEASE');
 
     if isempty(relealseStr)
         relealseStr=matlabshared.supportpkg.internal.util.getCurrentReleaseInternal();
     end
-
-
 
     token=regexpi(relealseStr,'R\d{4,4}[ab][^\)]*','match','once');
     relealseStr=['(',token,')'];
