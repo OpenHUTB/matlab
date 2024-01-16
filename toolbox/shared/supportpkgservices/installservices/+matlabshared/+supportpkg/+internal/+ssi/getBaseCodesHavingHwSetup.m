480adc6ddb57623089b6b1cb6729c9884ddaa8cb
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:22:05 +0800

    更新 getBaseCodesHavingHwSetup.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/getBaseCodesHavingHwSetup.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/getBaseCodesHavingHwSetup.m
@@ -1,22 +1,11 @@
 function filteredBaseCodes=getBaseCodesHavingHwSetup(baseCodeArray)
 
-
-
-
-
-
-
-
-
-
-
     baseCodeArray=cell(baseCodeArray);
     assert(iscellstr(baseCodeArray),'Expected baseCodeArray to be a cell array of strings');
 
     filteredBaseCodes={};
     for i=1:length(baseCodeArray)
         currentBaseCode=baseCodeArray{i};
-
         spPkgInfo=matlabshared.supportpkg.internal.getSpPkgInfoForBaseCode(currentBaseCode);
         if isempty(spPkgInfo)
             continue;
