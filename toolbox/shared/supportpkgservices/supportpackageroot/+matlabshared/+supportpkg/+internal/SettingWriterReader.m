720827b6b835386d429b10cfeae9f6ed8131b9d3
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:02:19 +0800

    更新 SettingWriterReader.m

--- toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/SettingWriterReader.m
+++ toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/SettingWriterReader.m
@@ -1,21 +1,20 @@
 classdef SettingWriterReader<handle
 
-
-
-
-
     properties(Access=protected)
 SettingDir
     end
+
 
     properties(SetAccess=immutable)
 SettingFileFullPath
     end
 
+
     properties(Access=protected,Constant)
         SETTING_FILE_NAME='supportpackagerootsetting.xml';
         DEFAULT_TOKEN='__DEFAULT__';
     end
+
 
     methods(Access=public)
 
@@ -26,9 +25,8 @@ SettingFileFullPath
             obj.SettingFileFullPath=fullfile(obj.SettingDir,obj.SETTING_FILE_NAME);
         end
 
+
         function writeRootSetting(obj,settingValue)
-
-
 
             validateattributes(settingValue,{'char','string'},{'nonempty','scalartext'},'matlabshared.supportpkg.internal.SettingWriterReader','settingValue');
             if~logical(exist(obj.SettingFileFullPath,'file'))
@@ -45,28 +43,20 @@ SettingFileFullPath
                 currentSetting='';
             end
 
-
             if strcmp(currentSetting,settingValue)
                 return;
             end
-
             obj.writeSettingXMLFile(settingValue);
             obj.pollForSettingFileWrite();
         end
 
+
         function settingValue=readRootSetting(obj)
-
-
-
-
-
             settingValue=matlabshared.supportpkg.internal.util.readSprootSettingFile(obj.SettingFileFullPath);
         end
 
+
         function writeSettingXMLFile(obj,spRoot)
-
-
-
             [status,~]=obj.writeSettingXMLFileImpl(spRoot);
             if status<0
                 error(message('supportpkgservices:supportpackageroot:NoWritePermissions',spRoot));
@@ -74,10 +64,6 @@ SettingFileFullPath
         end
 
         function[status,stdOut]=writeSettingXMLFileImpl(obj,spRoot)
-
-
-
-
 
             validateattributes(spRoot,{'char','string'},{'nonempty','scalartext'},'matlabshared.supportpkg.internal.SettingWriterReader','spRoot');
             writerExecutableJar=fullfile(matlabroot,'java','jar','toolbox','shared','supportpkgservices','sprootsettingwriter.jar');
@@ -87,30 +73,17 @@ SettingFileFullPath
                 [status,stdOut]=system(cmd,'-runAsAdmin');
             else
 
-
-
-
-
                 stdOut='';
                 if logical(exist(obj.SettingFileFullPath,'file'))
                     [~,sFileAttrib,~]=fileattrib(obj.SettingFileFullPath);
                     isWriteable=sFileAttrib.UserWrite;
                     if~isWriteable
-
-
-
                         [isWriteable,stdOut,~]=fileattrib(obj.SettingFileFullPath,'+w','a');
                     end
                 else
-
-
                     [~,sDirAttrib,~]=fileattrib(obj.SettingDir);
                     isWriteable=sDirAttrib.UserWrite;
                 end
-
-
-
-
 
                 if~isWriteable
                     status=-1;
@@ -121,14 +94,14 @@ SettingFileFullPath
         end
     end
 
+
     methods(Access={?matlabshared.supportpkg.internal.SingleRootHandler,?MockSettingWriterReader})
         function createDefaultSettingFile(obj)
-
-
             obj.writeSettingXMLFile(matlabshared.supportpkg.internal.SettingWriterReader.DEFAULT_TOKEN)
 
             obj.pollForSettingFileWrite();
         end
+
 
         function pollForSettingFileWrite(obj)
             while(1)
