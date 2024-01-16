f1c2af6feba5b12af9e41818a407772871d5f7a3
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 22:44:40 +0800

    更新 generateOrigModel.m

--- toolbox/shared/slpir/+slpir/@PIR2SL/generateOrigModel.m
+++ toolbox/shared/slpir/+slpir/@PIR2SL/generateOrigModel.m
@@ -1,8 +1,5 @@
 function generateOrigModel(this)
 
-
-
-
     if this.isDutWholeModel
         return;
     end
@@ -12,40 +9,25 @@ function generateOrigModel(this)
     gmStartNodeName=regexprep(this.RootNetworkName,['^',this.InModelFile],gmName);
 
     try
-
         junkModelName=getGeneratedModelName('tempHDLC_',gmName);
         new_system(junkModelName,'model');
         junkSS=add_block('built-in/SubSystem',[junkModelName,'/tempHDLC']);
         Simulink.BlockDiagram.copyContentsToSubSystem(this.InModelFile,junkSS);
 
-
-
-
         new_system(gmName,'model');
         Simulink.SubSystem.copyContentsToBlockDiagram(junkSS,gmName);
-
-
-
-
-
         slInternal('convertAllSSRefBlocksToSubsystemBlocks',...
         get_param(gmName,'handle'));
         blk=gmStartNodeName;
         while~isempty(blk)
             if strcmp(get_param(blk,'Type'),'block')&&...
                 strcmp(get_param(blk,'StaticLinkStatus'),'resolved')
-
                 set_param(blk,'LinkStatus','inactive');
             end
             blk=get_param(blk,'Parent');
         end
         close_system(junkModelName,0);
-
-
         slpir.PIR2SL.initOutputModel(this.InModelFile,gmName);
-
-
-
         if(strncmp(get_param(gmName,'SignalResolutionControl'),'TryResolve',10))
             set_param(gmName,'SignalResolutionControl','UseLocalSettings');
         end
