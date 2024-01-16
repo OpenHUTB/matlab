89072962cae4623c6640aa7a39b0ad662abb4c5b
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 20:51:43 +0800

    更新 IOServerBlockInfo.m

--- toolbox/shared/transportclients/+matlabshared/+transportclients/+internal/+IOServerClient/IOServerBlockInfo.m
+++ toolbox/shared/transportclients/+matlabshared/+transportclients/+internal/+IOServerClient/IOServerBlockInfo.m
@@ -1,25 +1,13 @@
 classdef(Hidden)IOServerBlockInfo<event.EventData
 
-
-
-
-
-
-
-
-
     properties(SetAccess=private)
-
-
 Data
-
 AbsTime
     end
 
 
     methods(Hidden)
         function obj=IOServerBlockInfo(data)
-
             obj.Data=data;
             obj.AbsTime=datetime;
         end
