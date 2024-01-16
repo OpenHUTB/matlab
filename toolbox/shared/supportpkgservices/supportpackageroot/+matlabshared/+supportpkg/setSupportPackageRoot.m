d5969fa52d47bbd9275f1bdac6ab0935f2f3eff7
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:01:15 +0800

    更新 setSupportPackageRoot.m

--- toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/setSupportPackageRoot.m
+++ toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/setSupportPackageRoot.m
@@ -1,42 +1,7 @@
 function location=setSupportPackageRoot(spRoot)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     validateattributes(spRoot,{'char','string'},{'nonempty','scalartext'},'matlabshared.supportpkg.setSupportPackageRoot','SPROOT');
-
-
     rootHandler=matlabshared.supportpkg.internal.SupportPackageRootHandler.getHandler();
-
-
 
     try
         rootHandler.setInstallRoot(spRoot);
