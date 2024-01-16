406b20f91efde01d3f3a382e8f8ff74b5c512d0d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:25:27 +0800

    更新 ensurePlatformAppropriatePath.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/ensurePlatformAppropriatePath.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/ensurePlatformAppropriatePath.m
@@ -1,9 +1,4 @@
 function pathEntries=ensurePlatformAppropriatePath(pathEntries)
-
-
-
-
-
     assert(iscellstr(pathEntries),'ensurePlatformAppropriatePath: Expected "pathEntries" to be a cell array of strings');
     pathEntries=fullfile(pathEntries);
 end
