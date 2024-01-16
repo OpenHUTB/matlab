2711394d02cf9e60675c7333bc4729a87f5f2ec6
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:14:49 +0800

    更新 isDefaultSetting.m

--- toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/+util/isDefaultSetting.m
+++ toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/+util/isDefaultSetting.m
@@ -1,6 +1,5 @@
 function isDefault=isDefaultSetting(settingValue)
 
-
     validateattributes(settingValue,{'char','string'},{'nonempty','scalartext'});
     isDefault=strcmp(settingValue,'__DEFAULT__');
 end
