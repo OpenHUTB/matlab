578473b490baadc141bbfe3a76f1b29664e659ad
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:27:52 +0800

    更新 getFullNameForBaseCode.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/getFullNameForBaseCode.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/getFullNameForBaseCode.m
@@ -1,10 +1,5 @@
 function spNameFromInstallData=getFullNameForBaseCode(baseCode,spRoot)
 
-
-
-
-
-
     validateattributes(baseCode,{'char'},{'nonempty'},'getFullNameForBaseCode','baseCode',1);
     validateattributes(spRoot,{'char'},{'nonempty'},'getFullNameForBaseCode','spRoot',2);
     assert(logical(exist(spRoot,'dir')),sprintf('spRoot directory: %s does not exist',spRoot));
