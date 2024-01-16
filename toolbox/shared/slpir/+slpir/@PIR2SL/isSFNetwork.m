064452976d0ff0a459687f6124d2ee4e41743ae0
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 22:50:55 +0800

    更新 isSFNetwork.m

--- toolbox/shared/slpir/+slpir/@PIR2SL/isSFNetwork.m
+++ toolbox/shared/slpir/+slpir/@PIR2SL/isSFNetwork.m
@@ -1,7 +1,4 @@
 function isSF=isSFNetwork(~,slbh)
-
-
-
     isSF=slbh~=-1&&isprop(slbh,'BlockType')&&...
     strcmpi(get_param(slbh,'BlockType'),'SubSystem')&&...
     ~strcmpi(get_param(slbh,'SFBlockType'),'NONE');
