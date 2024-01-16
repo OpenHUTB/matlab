d146b5ca5a522758f7e0f5a4049c6346d3d9cc02
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:27:04 +0800

    更新 getExamplesFcnAndArgsForBaseCode.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/getExamplesFcnAndArgsForBaseCode.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/getExamplesFcnAndArgsForBaseCode.m
@@ -1,17 +1,10 @@
 function fcnHandle=getExamplesFcnAndArgsForBaseCode(baseCode,spRoot)
 
-
-
-
-
-
-
     validateattributes(baseCode,{'char'},{'nonempty'},'getExamplesFcnAndArgsForBaseCode','baseCode',1);
     validateattributes(spRoot,{'char'},{'nonempty'},'getExamplesFcnAndArgsForBaseCode','spRoot',2);
     assert(logical(exist(spRoot,'dir')),sprintf('spRoot directory: %s does not exist',spRoot));
 
     fcnHandle=function_handle.empty;
-
 
     try
         installedPackages=matlabshared.supportpkg.internal.InstalledDocUtils.getInstalledDocInfo(spRoot);
@@ -22,32 +15,18 @@ function fcnHandle=getExamplesFcnAndArgsForBaseCode(baseCode,spRoot)
     end
 
     spPkgHelpRoot=[];
-
-
-
-
-
     spNameFromInstallData=matlabshared.supportpkg.internal.ssi.util.getFullNameForBaseCode(baseCode,spRoot);
-
-
-
     pkgIdx=ismember(allFullNamesUnderHelpRoot,{spNameFromInstallData});
 
     if any(pkgIdx)
         spPkgHelpRoot=char(installedPackages(pkgIdx).SupportPackageHelpRoot);
     end
-
     featuredExamplesIndex=fullfile(spPkgHelpRoot,'examples.html');
-
 
     if~isempty(spPkgHelpRoot)&&logical(exist(featuredExamplesIndex,'file'))
         fcnHandle=@()web(featuredExamplesIndex,'-helpbrowser');
         return;
     end
-
-
-
-
     pluginInfo=matlabshared.supportpkg.internal.getSpPkgInfoForBaseCode(baseCode);
     if~isempty(pluginInfo)&&~isempty(pluginInfo.ExtraInfoCheckBoxCmd)
         fcnHandle=@()matlabshared.supportpkg.internal.ssi.util.evaluateCmd(pluginInfo.ExtraInfoCheckBoxCmd);
