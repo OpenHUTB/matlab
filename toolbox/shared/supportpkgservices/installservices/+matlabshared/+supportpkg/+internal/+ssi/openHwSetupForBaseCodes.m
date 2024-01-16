afe2a7684dc576ab1af21c3e42cb16c74393d313
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:23:01 +0800

    更新 openHwSetupForBaseCodes.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/openHwSetupForBaseCodes.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/openHwSetupForBaseCodes.m
@@ -1,17 +1,5 @@
 function openHwSetupForBaseCodes(baseCodeArray)
-
-
-
-
-
-
-
-
-
     baseCodeArray=cellstr(string(baseCodeArray));
-
-
-
 
     try
         matlab.internal.msgcat.setAdditionalResourceLocation(...
@@ -19,35 +7,22 @@ function openHwSetupForBaseCodes(baseCodeArray)
     catch
     end
 
-
-
     if numel(baseCodeArray)>1
 
         entryPoint='ssiSetupNow';
         matlabshared.supportpkg.internal.ssi.util.openAddOnsManager(entryPoint);
 
-
-
     else
-
         sppkgObj=matlabshared.supportpkg.internal.getSpPkgInfoForBaseCode(...
         baseCodeArray{1});
 
-
         fwUpdateClassName=sppkgObj.FwUpdate;
-
         assert(~isempty(fwUpdateClassName),...
         ['No Hardware Setup available for the specified base code: ',baseCodeArray{1}]);
-
-
-
         fwUpdateSuperClassList=superclasses(fwUpdateClassName);
-
-
 
         if any(ismember(fwUpdateSuperClassList,'hwconnectinstaller.FirmwareUpdate'))
             matlabshared.supportpkg.internal.sppkglegacy.launchTargetupdaterForBaseCodes(baseCodeArray);
-
 
         elseif any(ismember(fwUpdateSuperClassList,...
             'matlab.hwmgr.internal.hwsetup.Workflow'))
