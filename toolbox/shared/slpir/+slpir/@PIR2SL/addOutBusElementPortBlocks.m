e212564d750883b0462ceda14f75ccaecb9f9ced
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:34:53 +0800

    更新 addOutBusElementPortBlocks.m

--- toolbox/shared/slpir/+slpir/@PIR2SL/addOutBusElementPortBlocks.m
+++ toolbox/shared/slpir/+slpir/@PIR2SL/addOutBusElementPortBlocks.m
@@ -1,9 +1,4 @@
-
 function portAdded=addOutBusElementPortBlocks(this,hP,slBlockName,tgtParentPath,hNtwkSlHandle)
-
-
-
-
 
     portAdded=false;
     BusPortHandle=find_system(get_param(hNtwkSlHandle,'Handle'),...
@@ -19,14 +14,9 @@ function portAdded=addOutBusElementPortBlocks(this,hP,slBlockName,tgtParentPath,
     uniqueName=slpir.PIR2SL.getUniqueName(slBlockName);
     if nPorts==1&&...
         ~strcmp(get_param(BusPortHandle,'IsBusElementPort'),'on')
-
-
         gmHandle=add_block(getfullname(BusPortHandle),uniqueName);
         hP.setGMHandle(gmHandle);
-
     else
-
-
         gmPortHandle=add_block('simulink/Ports & Subsystems/Out Bus Element',uniqueName,'CreateNewPort','on','PortName',[get_param(BusPortHandle(1),'PortName'),'OutBus'],'Element','');
         hP.setGMHandle(gmPortHandle);
     end
