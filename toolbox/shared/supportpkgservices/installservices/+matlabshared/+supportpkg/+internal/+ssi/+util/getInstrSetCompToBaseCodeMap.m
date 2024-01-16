e77683a2cce65dd5310262e744a314b099435662
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:29:37 +0800

    更新 getInstrSetCompToBaseCodeMap.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/getInstrSetCompToBaseCodeMap.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/getInstrSetCompToBaseCodeMap.m
@@ -1,18 +1,9 @@
 function compMap=getInstrSetCompToBaseCodeMap(pluginMetaClasses)
 
-
-
-
-
-
     validateattributes(pluginMetaClasses,{'meta.class'},{'nonempty'});
 
     allPluginObjects=matlabshared.supportpkg.internal.sppkglegacy.SupportPackageRegistryPluginBase.constructPluginClasses(pluginMetaClasses);
-
-
     assert(isa(allPluginObjects,'matlabshared.supportpkg.internal.sppkglegacy.SupportPackageRegistryPluginBase'),...
     'Expected the meta classes to be of the matlabshared.supportpkg.internal.sppkglegacy.SupportPackageRegistryPluginBase type');
-
-
     compMap=allPluginObjects.getTpCompNameToBaseCodeMap();
 end
