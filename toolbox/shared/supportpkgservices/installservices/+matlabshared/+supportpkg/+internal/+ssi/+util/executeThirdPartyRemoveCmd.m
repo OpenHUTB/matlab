8682223973c441879c8c1e51ab5c10d99a94d967
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:26:24 +0800

    更新 executeThirdPartyRemoveCmd.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/executeThirdPartyRemoveCmd.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/+util/executeThirdPartyRemoveCmd.m
@@ -1,45 +1,18 @@
 function executeThirdPartyRemoveCmd(spBaseCode)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     allPluginMetaClasses=matlabshared.supportpkg.internal.findAllRegistryPlugins;
     if isempty(allPluginMetaClasses)
         return;
     end
-
     pluginMetaClass=matlabshared.supportpkg.internal.sppkglegacy.SupportPackageRegistryPluginBase.findSpPkgPluginForBaseCode(spBaseCode,allPluginMetaClasses);
     if isempty(pluginMetaClass)
         return;
     end
-
     pluginObj=matlabshared.supportpkg.internal.sppkglegacy.SupportPackageRegistryPluginBase.constructPluginClasses(pluginMetaClass);
-
     compNamesFromPlugin=pluginObj.getTpCompNames();
-
-
     baseCodesMap=matlabshared.supportpkg.internal.ssi.util.getInstrSetCompToBaseCodeMap(allPluginMetaClasses);
     for i=1:numel(compNamesFromPlugin)
-
-
-
-
         compName=compNamesFromPlugin{i};
         if(numel(baseCodesMap(compName))>1)
-
-
-
             continue;
         end
         assert(strcmp(baseCodesMap(compName),spBaseCode),...
@@ -50,10 +23,7 @@ function executeThirdPartyRemoveCmd(spBaseCode)
                 fcnHandle();
             end
         catch ex
-
-
             warning(message('supportpkgservices:installservices:ExecuteRemoveCmdError',spBaseCode,ex.message));
         end
-
     end
 end
