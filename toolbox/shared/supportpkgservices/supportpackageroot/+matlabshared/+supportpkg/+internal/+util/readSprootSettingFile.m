bae7ee7c27b5ac97f01a5866e53165cd294af211
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:15:20 +0800

    更新 readSprootSettingFile.m

--- toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/+util/readSprootSettingFile.m
+++ toolbox/shared/supportpkgservices/supportpackageroot/+matlabshared/+supportpkg/+internal/+util/readSprootSettingFile.m
@@ -1,13 +1,8 @@
 function settingVal=readSprootSettingFile(settingFileFullPath)
 
-
-
     if~logical(exist(settingFileFullPath,'file'))
         error(message('supportpkgservices:supportpackageroot:NoSettingFile'));
     end
-
-
-
     settingVal=matlabshared.supportpkg.internal.biReadSettingXMLFile(settingFileFullPath);
 
     if isempty(settingVal)
