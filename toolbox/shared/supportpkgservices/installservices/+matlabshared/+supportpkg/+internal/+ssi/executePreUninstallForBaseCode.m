f2d43e14709170918433fa6a14e327a68ac963e4
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:21:36 +0800

    更新 executePreUninstallForBaseCode.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/executePreUninstallForBaseCode.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/executePreUninstallForBaseCode.m
@@ -1,28 +1,14 @@
 function executePreUninstallForBaseCode(baseCode)
 
-
-
-
-
-
-
-
-
-
-
-
-
     validateattributes(baseCode,{'char'},{'nonempty'},'executePreUninstallForBaseCode','baseCode',1);
 
     spPkgInfo=matlabshared.supportpkg.internal.getSpPkgInfoForBaseCode(baseCode);
-
 
     if isempty(spPkgInfo)
         return;
     end
     try
         if~isempty(spPkgInfo.PreUninstallCmd)
-
             matlabshared.supportpkg.internal.ssi.util.evaluateCmd(spPkgInfo.PreUninstallCmd);
         end
         matlabshared.supportpkg.internal.ssi.util.executeThirdPartyRemoveCmd(baseCode);
