2e5f190e784f59cd9a8bc924abd1d81019a71cd9
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:23:51 +0800

    更新 refreshMatlab.m

--- toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/refreshMatlab.m
+++ toolbox/shared/supportpkgservices/installservices/+matlabshared/+supportpkg/+internal/+ssi/refreshMatlab.m
@@ -1,40 +1,15 @@
 function refreshMatlab()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     rehash pathreset;
-
-
-
     skipSLRefresh=getenv('SSI_MLREFRESH_SKIP_SIMULINK_REFRESH');
 
     if isempty(skipSLRefresh)...
         &&matlabshared.supportpkg.internal.ssi.util.isProductInstalled('Simulink')
         sl_refresh_customizations;
 
-
         lb=slLibraryBrowser('noshow');
         lb.refresh;
     else
-
-
-
         if exist('RTW.TargetRegistry','class')
             RTW.TargetRegistry.getInstance('reset');
         end
@@ -42,23 +17,9 @@ function refreshMatlab()
     end
 
     try %#ok<TRYNC> 
-
         matlab.internal.doc.invalidateSupportPackageCache();
-
-
-
-
-
-
-
         matlab.internal.reference.cache.ClearAllCaches();
     end
-
-
-
-
-
-
     spRoot=matlabshared.supportpkg.internal.getSupportPackageRootNoCreate();
     if(not(isempty(spRoot)))
         matlabshared.supportpkg.internal.ssi.util.loadMsgCatResourcesIfAvailable(spRoot);
