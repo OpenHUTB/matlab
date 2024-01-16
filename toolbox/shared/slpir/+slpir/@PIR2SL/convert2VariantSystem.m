228c34a9c4921d8b8e1e696b6d40e6e6314f5807
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 21:38:35 +0800

    更新 convert2VariantSystem.m

--- toolbox/shared/slpir/+slpir/@PIR2SL/convert2VariantSystem.m
+++ toolbox/shared/slpir/+slpir/@PIR2SL/convert2VariantSystem.m
@@ -4,7 +4,6 @@ function convert2VariantSystem(this,hC,blockNameWithFullPath)
         return;
     end
 
-
     addBlock(this,[],'built-in/Inport',[blockNameWithFullPath,'/Variant_In1']);
     addBlock(this,[],'built-in/Outport',[blockNameWithFullPath,'/Variant_Out1']);
     add_line(blockNameWithFullPath,'Variant_In1/1','Variant_Out1/1');
