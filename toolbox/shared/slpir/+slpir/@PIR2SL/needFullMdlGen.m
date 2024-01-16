a6808f5a030dfa57e3cf721b472a559a83179428
王海东 <wanghaidong@hutb.edu.cn>
2024-01-16 22:51:15 +0800

    更新 needFullMdlGen.m

--- toolbox/shared/slpir/+slpir/@PIR2SL/needFullMdlGen.m
+++ toolbox/shared/slpir/+slpir/@PIR2SL/needFullMdlGen.m
@@ -1,27 +1,16 @@
 function needMdlGenForDut=needFullMdlGen(this)
 
-
-
-
-
-
-
     if this.genmodelgetparameter('CriticalPathEstimation')
         needMdlGenForDut=true;
     elseif this.genmodelgetparameter('StaticLatencyPathAnalysis')
         needMdlGenForDut=true;
     elseif~this.genmodelgetparameter('optimizemdlgen')
 
-
         needMdlGenForDut=true;
     elseif isDutWholeModel(this)
 
         needMdlGenForDut=true;
     else
-
-
-
-
         needMdlGenForDut=this.needsFullModelGenForDut;
     end
 
