725f2aadaf9c1dc41c708fd8b607ff364f0ba805
王海东 <wanghaidong@hutb.edu.cn>
2024-01-17 12:57:45 +0800

    更新 getTuningData.m

--- toolbox/control/ctrlmodels/@genss/getTuningData.m
+++ toolbox/control/ctrlmodels/@genss/getTuningData.m
@@ -14,7 +14,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
     end
     nsysTuned=numel(TunedModels);
 
-
     nspec=0;
     for ct=1:nreqs
         R=Reqs(ct);
@@ -39,14 +38,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
         error(message('Control:tuning:systune43'))
     end
 
-
-
-
-
-
-
-
-
     FixedInteg=struct('Check',[],'Active',[],'Shift',[],...
     'U1',[],'U2',[],'V1',[],'V2',[],'A11',[],'A22',[]);
     SYSDATA(1:nsys,1)=struct(...
@@ -59,9 +50,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
     'Acl',[],'Bcl',[],'Ccl',[],'Dcl',[],...
     'LFTData',[],'Scaling',[],'sInfo',[]);
 
-
-
-
     DS=struct('Static',false,'Dynamic',0);
     SPECDATA(1:nspec,1)=struct(...
     'Type',[],'Soft',[],'Stabilize',true,'Uncertain',false,...
@@ -73,17 +61,10 @@ function[SYSDATA,SPECDATA,tInfo]=...
     'xPerf',[],'Acl',[],'Bcl',[],'Ccl',[],'Dcl',[],'Ucl',[],...
     'PeakFreq',[],'HamEigs',[],'RY',[],'Flag',[]);
 
-
-
-
-
-
-
     Data=T.Data_;
     CplxUFlag=false;
     for ct=1:nsysTuned
         iM=TunedModels(ct);
-
 
         D=normalizeBlocks(Data(iM));
         B=D.Blocks;
@@ -102,7 +83,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
         [~,bpermT]=sort(getBlockName(B(iT)));
         iN=find(~(isAnalysis|isUncty|isTuned));
         bperm=[iA(bpermA);iU(bpermU);iT(bpermT);iN];
-
         [rperm,cperm]=getRowColPerm(B,bperm);
         D.IC=ioperm(D.IC,[1:nz,nz+cperm],[1:nw,nw+rperm]);
         D.Blocks=B(bperm);
@@ -118,13 +98,11 @@ function[SYSDATA,SPECDATA,tInfo]=...
         WarnFcn(message('Control:tuning:systune27'))
     end
 
-
     BlockSet=getBlocks(T);
     BN=fieldnames(BlockSet);
     BV=struct2cell(BlockSet);
     isP=cellfun(@isParametric,BV);
     isU=cellfun(@isUncertain,BV);
-
 
     TunedBlocks=struct('Data',BV(isP,:),...
     'nx',[],'nu',[],'ny',[],'np',[],'npf',[],...
@@ -135,13 +113,10 @@ function[SYSDATA,SPECDATA,tInfo]=...
     'nx',0,'nu',1,'ny',1,'np',1,'npf',1);
     UBlockNames=BN(isU);
 
-
-
     isAP=~(isP|isU);
     AnalysisBlocks=struct('Data',BV(isAP,:),...
     'nch',[],'ich',[],'chID',[]);
     AnalysisBlockNames=BN(isAP);
-
 
     nAnalysis=numel(AnalysisBlocks);
     OpenAP=false;
@@ -156,14 +131,10 @@ function[SYSDATA,SPECDATA,tInfo]=...
         ich=ich+nch;
     end
     if OpenAP
-
-
         WarnFcn(message('Control:tuning:Tuning1'))
     end
     nL=sum([AnalysisBlocks.nch]);
     LoopConfigs=false(nL,0);
-
-
     p0=zeros(0,1);pMin=zeros(0,1);pMax=zeros(0,1);
     iFree=zeros(0,1);iBlock=zeros(0,1);
     ip=0;ix=0;
@@ -196,19 +167,9 @@ function[SYSDATA,SPECDATA,tInfo]=...
         ip=ip+np;ix=ix+npf;
     end
 
-
-
-
-
-
-
-
-
     nxL=zeros(nL,1);
     nsL=zeros(nL,1);
     ISSGraph=eye(nL);
-
-
 
     DL0=struct('a',[],'b',zeros(0,nL+nz),'c',zeros(nL+nz,0),'d',eye(nL+nz),'Poles',zeros(0,1));
     DR0=struct('a',[],'b',zeros(0,nL+nw),'c',zeros(nL+nw,0),'d',eye(nL+nw),'Poles',zeros(0,1));
@@ -223,10 +184,8 @@ function[SYSDATA,SPECDATA,tInfo]=...
     'LoopScalings',[],'DL',DL0,'DR',DR0,...
     'SpecEvalOrder',[],'Ts',Ts,'TU',T.TimeUnit);
 
-
     for ct=1:nsysTuned
         iM=TunedModels(ct);
-
         [ABInfo,UBInfo,TBInfo]=SYSTUNE_BlockInfo(Data(iM).Blocks,...
         AnalysisBlockNames,UBlockNames,TunedBlockNames);
 
@@ -252,12 +211,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
         SYSDATA(iM).TunedBlocks=TBInfo;
     end
 
-
-
-
-
-
-
     uNames=T.InputName;
     yNames=T.OutputName;
     if~(localCheckSignalList(uNames)&&localCheckSignalList(yNames))
@@ -267,7 +220,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
     if~localCheckSignalList(aNames)
         error(message('Control:tuning:Tuning6'))
     end
-
 
     isActive=false(nsys,1);
     nspec=0;S0=SPECDATA(1);
@@ -298,8 +250,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
 
                 iM=intersect(R.Models,TunedModels,'stable');
                 if S.Type==3
-
-
                     iM=localCheckRelevance(iM,S.Input-nw,SYSDATA);
                 end
 
@@ -310,7 +260,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
 
                 isActive(iM,S.Config)=true;
             else
-
                 nspec=nspec+1;SPECDATA(nspec)=getSpecData(R,S,TunedBlockNames);
             end
         end
@@ -321,9 +270,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
     SPECDATA=SPECDATA(1:nspec);
     tInfo.SpecEvalOrder=1:nspec;
     tInfo.LoopConfigs=LoopConfigs;
-
-
-
 
     for ct=1:numel(SPECDATA)
         S=SPECDATA(ct);
@@ -372,31 +318,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
     tInfo.iBlock=iBlock;
     tInfo.LoopScalings=struct('nc',1,'ns',num2cell(nsL),'nx',num2cell(nxL),'np',num2cell(npL));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     SYSDATA=repmat(SYSDATA,[1,size(LoopConfigs,2)]);
     for ct=1:nsysTuned
 
@@ -417,10 +338,8 @@ function[SYSDATA,SPECDATA,tInfo]=...
 
         iL=SYSDATA(iM,1).iL;
         nch=numel(iL);
-
         [Aus,Bus,Cus,Dus]=localUStruct(SYSDATA(iM,1),tInfo);
         nxU=size(Aus,1);
-
         [Acs,Bcs,Ccs,Dcs]=localCStruct(SYSDATA(iM,1),tInfo);
         nxC=size(Acs,1);
 
@@ -431,23 +350,19 @@ function[SYSDATA,SPECDATA,tInfo]=...
             SYSDATA(iM,iSC).Active=true;
             SYSDATA(iM,iSC).sInfo=localCLStruct(...
             IC,LSC,Aus,Bus,Cus,Dus,Acs,Bcs,Ccs,Dcs);
-
             D=feedback(IC,createGain(IC,LSC),nw+1:nw+nch,nz+1:nz+nch,+1);
             SYSDATA(iM,iSC).A=D.a;
             SYSDATA(iM,iSC).B=D.b;
             SYSDATA(iM,iSC).C=D.c;
             SYSDATA(iM,iSC).D=D.d;
             if nch<nL
-
                 SYSDATA(iM,iSC)=localInsertZeros(SYSDATA(iM,iSC),nw,nz,nL,iL');
             end
-
             SYSDATA(iM,iSC).Scaling=struct('sx',[],'px',[]);
             SYSDATA(iM,iSC).xStab=false(nxP+nxC+nxU,1);
             SYSDATA(iM,iSC).xPerf=false(nxP+nxC+nxU,1);
         end
     end
-
 
     for ct=1:nspec
         S=SPECDATA(ct);
@@ -455,13 +370,11 @@ function[SYSDATA,SPECDATA,tInfo]=...
         iC=S.Config;
         if iC>0
 
-
             SI=SYSDATA(iM,iC).sInfo;
             if isempty(S.Input)
 
                 xPerf=true(SI.nx,1);
             else
-
                 [~,~,~,~,xkeep]=smreal(SI.a,SI.b(:,S.Input),SI.c(S.Output,:),[]);
                 xPerf=xkeep(1:SI.nx,:);
             end
@@ -476,7 +389,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
             end
         end
     end
-
 
     for ct=1:numel(SYSDATA)
         if isActive(ct)
@@ -519,7 +431,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
             SYSDATA.sInfo=sInfo;
 
             function isOK=localCheckSignalList(NameList)
-
                 NameList=NameList(~strcmp(NameList,''));
                 isOK=(numel(unique(NameList))==numel(NameList));
 
@@ -580,8 +491,6 @@ function[SYSDATA,SPECDATA,tInfo]=...
 
                         function sInfo=localCLStruct(IC,APConfig,Au,Bu,Cu,Du,Ac,Bc,Cc,Dc)
 
-
-
                             A=(IC.a~=0);
                             B=(IC.b~=0);
                             C=(IC.c~=0);
@@ -596,30 +505,17 @@ function[SYSDATA,SPECDATA,tInfo]=...
                             ic=[nw+1:cs,1:nw,nw+1:nw+nL];
                             B=B(:,ic);C=C(ir,:);D=D(ir,ic);
                             nws=nw+nL;nzs=nz+nL;
-
-
-
-
-
-
-
-
                             s1=nx+nxu+nxc;s2=s1+ny+nzu+nL;s3=s2+nu+nwu+nL;
-
                             ir=[1:nx,s1+1:s2,s3+1:s3+nzs];
                             ic=[1:nx,s2+1:s3+nws];
                             MS(ir,ic)=[A,B;C,D];
-
                             MS(s2+1:s2+nL,s1+1:s1+nL)=APConfig;
-
                             ir=[nx+1:nx+nxu,s2+nL+1:s2+nL+nwu];
                             ic=[nx+1:nx+nxu,s1+nL+1:s1+nL+nzu];
                             MS(ir,ic)=[Au,Bu;Cu,Du];
-
                             ir=[nx+nxu+1:nx+nxu+nxc,s2+nL+nwu+1:s3];
                             ic=[nx+nxu+1:nx+nxu+nxc,s1+nL+nzu+1:s2];
                             MS(ir,ic)=[Ac,Bc;Cc,Dc];
-
                             sInfo=struct('a',MS(1:s3,1:s3),'b',MS(1:s3,s3+1:s3+nws),...
                             'c',MS(s3+1:s3+nzs,1:s3),'d',MS(s3+1:s3+nzs,s3+1:s3+nws),...
                             'nx',nx+nxc+nxu);
