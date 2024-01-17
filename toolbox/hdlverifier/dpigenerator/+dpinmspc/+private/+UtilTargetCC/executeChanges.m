97d61168b93181ac3b486fd2265bbaeecf61224c
王海东 <wanghaidong@hutb.edu.cn>
2024-01-17 13:07:37 +0800

    更新 executeChanges.m

--- toolbox/hdlverifier/dpigenerator/+dpinmspc/+private/+UtilTargetCC/executeChanges.m
+++ toolbox/hdlverifier/dpigenerator/+dpinmspc/+private/+UtilTargetCC/executeChanges.m
@@ -1,27 +1,22 @@
 function tgt=executeChanges(hObj,chg,tgt)
 
-
-
     vTF=isfield(chg,{'vis','en','val'});
-
 
     if(isa(tgt,'struct'))
         if(vTF(1)),tgt=l_executeCellChange(chg.vis,tgt,'Visible');end
         if(vTF(2)),tgt=l_executeCellChange(chg.en,tgt,'Enabled');end
         if(vTF(3)),l_executeCellChange(chg.val,hObj,'Value');end
 
-
-
     elseif(isa(tgt,'DAStudio.Dialog'))
         if(vTF(1)),l_executeDlgChange(hObj,chg.vis,@tgt.setVisible,'Visible');end
         if(vTF(2)),l_executeDlgChange(hObj,chg.en,@tgt.setEnabled,'Enabled');end
         if(vTF(3)),l_executeDlgChange(hObj,chg.val,@tgt.setWidgetValue,'Value');end
 
-
     else
 
     end
 end
+
 
 function tgt=l_executeCellChange(chg,tgt,tgtField)
     if(~isempty(chg)),propNames=fieldnames(chg);
@@ -37,6 +32,7 @@ function tgt=l_executeCellChange(chg,tgt,tgtField)
         end
     end
 end
+
 
 function l_executeDlgChange(hObj,chg,tgtFcn,tgtField)
     if(~isempty(chg)),propNames=fieldnames(chg);
