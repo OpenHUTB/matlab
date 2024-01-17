b282b93c79cb5bb8fd83f71a570a4ee20e9309a1
王海东 <wanghaidong@hutb.edu.cn>
2024-01-17 13:06:39 +0800

    更新 zpk_minreal_inf.m

--- toolbox/control/ctrlutil/zpk_minreal_inf.m
+++ toolbox/control/ctrlutil/zpk_minreal_inf.m
@@ -4,7 +4,6 @@ function[z,p,k]=zpk_minreal_inf(am,bm,cm,dm,em,Ts)
     if n==0
         z=zeros(0,1);p=zeros(0,1);k=dm-bm*cm/am;
     else
-
         [v,w,tau]=ltipack.householder(bm);
         p=-tau;
         aa=am-v*(w'*am);
@@ -13,21 +12,15 @@ function[z,p,k]=zpk_minreal_inf(am,bm,cm,dm,em,Ts)
         m=ee(1,1:n);e=ee(2:n+1,1:n);
         c=cm(1,1:n);q=cm(1,n+1);
 
-
         dN=dm*h-p*q;cN=dm*g-p*c;
         [W,~]=qr([e;dm*m]);
         w12=W(1:n,n+1);w22=W(n+1,n+1);
         [z,kN]=ltipack.sszero(a,f,w22'*cN+w12'*a,w22'*dN+w12'*f,e,Ts,'norefine');
         kN=kN/w22';
 
-
-
         q22=-w(n+1)*v(1)';
         [p,kD]=ltipack.sszero(a,f,am(n+1,1:n),am(n+1,n+1),e,Ts,'norefine');
         kD=kD/q22;
-
-
-
 
         if nargout>2
             k=kN/kD;
