32cf74343df66318cf4f0a07cf8d246b689bf5b4
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:46:02 +0800

    更新 splitHarnessId.m

--- toolbox/shared/reqmgt/+rmisl/splitHarnessId.m
+++ toolbox/shared/reqmgt/+rmisl/splitHarnessId.m
@@ -1,16 +1,11 @@
 function[hID,sID]=splitHarnessId(origId)
 
-
-
-
-
     colons=strfind(origId,':');
     if length(colons)==3
 
         hID=origId;
         sID='';
     else
-
         hID=origId(1:colons(4)-1);
         sID=origId(colons(4):end);
     end
