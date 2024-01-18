a45e550c957b7eb5a54aefd21e4b9a00a360cf6d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:41:47 +0800

    更新 modelHasEmbeddedReqInfo.m

--- toolbox/shared/reqmgt/+rmisl/modelHasEmbeddedReqInfo.m
+++ toolbox/shared/reqmgt/+rmisl/modelHasEmbeddedReqInfo.m
@@ -1,18 +1,9 @@
 function result=modelHasEmbeddedReqInfo(modelH)
 
-
-
-
-
-
-
-
     if strcmp(get_param(modelH,'hasReqInfo'),'on')
         result=true;
         return;
     end
-
-
     bdReqInfo=get_param(modelH,'requirementInfo');
     if~isempty(bdReqInfo)&&contains(bdReqInfo,'{''')
         set_param(modelH,'hasReqInfo','on');
