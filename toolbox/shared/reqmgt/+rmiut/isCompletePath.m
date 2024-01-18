e4ee83ba186f0676bbf492b462e84a9ce5761eac
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 21:50:28 +0800

    更新 isCompletePath.m

--- toolbox/shared/reqmgt/+rmiut/isCompletePath.m
+++ toolbox/shared/reqmgt/+rmiut/isCompletePath.m
@@ -1,16 +1,10 @@
 function yesno=isCompletePath(fPath)
 
-
-
-
     fPath=convertStringsToChars(fPath);
-
     if~isempty(regexp(fPath,'^https?:','once'))
 
         yesno=true;
-
     else
-
         yesno=length(fPath)>=2&&...
         (fPath(2)==':'||fPath(1)=='/'||fPath(1)=='\'||fPath(1)==filesep);
     end
