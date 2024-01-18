79071a3c2191d23dbb7516c2d5a462f03b483983
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 21:57:30 +0800

    更新 trimWithDots.m

--- toolbox/shared/reqmgt/+rmiut/trimWithDots.m
+++ toolbox/shared/reqmgt/+rmiut/trimWithDots.m
@@ -1,7 +1,5 @@
 function out=trimWithDots(in,limit,trim)
 
-
-
     if nargin<3
         trim=limit;
     end
@@ -17,17 +15,11 @@ function out=trimWithDots(in,limit,trim)
     end
 
     if length(in)<=limit
-
         out=in;
-
     elseif length(trim)==1||trim(2)==0
-
         out=[in(1:trim),'...'];
-
     elseif trim(1)==0
-
         out=['...',in(end-trim(2)+1:end)];
-
     else
 
         out=[in(1:trim(1)),'...',in(end-trim(2)+1:end)];
