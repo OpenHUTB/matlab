48908c02d95ecb44fe4809923c4353bd8c37c022
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:24:49 +0800

    更新 getData.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/getData.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/getData.m
@@ -1,10 +1,6 @@
 function linkData=getData(this,srcName,elementId)
 
-
-
-
     linkData=[];
-
 
     if~ischar(srcName)
 
@@ -13,28 +9,18 @@ function linkData=getData(this,srcName,elementId)
     srcRoot=rmimap.RMIRepository.getRoot(this.graph,srcName);
 
     if isempty(srcRoot)
-
-
         return;
     else
-
-
         elt=rmimap.RMIRepository.getNode(srcRoot,elementId);
 
         if~isempty(elt)
             links=elt.dependeeLinks;
 
             if links.size>0
-
-
-
-
-
                 emptyReqs=rmi.createEmptyReqs(links.size);
                 linkData=emptyReqs(:);
 
                 for i=1:links.size
-
                     linkData(i)=rmimap.RMIRepository.populateReqData(links.at(i),linkData(i));
                 end
             end
