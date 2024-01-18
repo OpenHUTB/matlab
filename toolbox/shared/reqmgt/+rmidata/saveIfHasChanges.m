1ea85b5f0c3922b120aaea80974b948b1bdfa5a6
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 13:58:40 +0800

    更新 saveIfHasChanges.m

--- toolbox/shared/reqmgt/+rmidata/saveIfHasChanges.m
+++ toolbox/shared/reqmgt/+rmidata/saveIfHasChanges.m
@@ -1,9 +1,5 @@
 function wasSaved=saveIfHasChanges(mdl)
 
-
-
-
-
     if ischar(mdl)&&exist(mdl,'file')==4&&rmidata.isExternal(mdl)
         wasSaved=saveReqFileIfHasChanges(mdl);
     else
@@ -11,10 +7,10 @@ function wasSaved=saveIfHasChanges(mdl)
     end
 end
 
+
 function wasSaved=saveReqFileIfHasChanges(mdl)
     modelH=get_param(mdl,'Handle');
     if slreq.hasChanges(modelH)
-
         wasSaved=rmidata.promptToSave(modelH);
     else
         wasSaved=false;
