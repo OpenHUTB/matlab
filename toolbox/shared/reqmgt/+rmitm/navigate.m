f011e417517bc32c4bf5b41d918bd4ae2266c314
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 22:05:57 +0800

    更新 navigate.m

--- toolbox/shared/reqmgt/+rmitm/navigate.m
+++ toolbox/shared/reqmgt/+rmitm/navigate.m
@@ -1,17 +1,11 @@
 function navigate(varargin)
 
-
-
-
-
     if dig.isProductInstalled('Simulink Test')&&license('test','simulink_test')
         [testFile,testCaseId]=rmitm.resolve(varargin{:});
 
         if isempty(testFile)
             error(message('Slvnv:rmitm:UnableToLocate',strtok(varargin{1},'|')));
         end
-
-
         callback=@()stm.internal.openTestCase(testFile,testCaseId);
         sltest.internal.invokeFunctionAfterWindowRenders(callback);
     else
