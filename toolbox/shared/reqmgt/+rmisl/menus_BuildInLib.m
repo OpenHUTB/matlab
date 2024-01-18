c538c5368a5762a738a31962eb8cd6a75fd318c6
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:34:46 +0800

    更新 menus_BuildInLib.m

--- toolbox/shared/reqmgt/+rmisl/menus_BuildInLib.m
+++ toolbox/shared/reqmgt/+rmisl/menus_BuildInLib.m
@@ -1,8 +1,5 @@
 function schema=menus_BuildInLib(varargin)
 
-
-
-
     schema=DAStudio.ActionSchema;
     schema.label=getString(message('Slvnv:reqmgt:BuiltInLibNoRMI'));
     schema.tag='Simulink:RequirementsMenuForBuiltInLib';
