b4aec3ea9eea628905d45497cbd9930a090b4107
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 12:42:02 +0800

    更新 close.m

--- toolbox/shared/reqmgt/+rmidata/close.m
+++ toolbox/shared/reqmgt/+rmidata/close.m
@@ -1,13 +1,9 @@
 function close(modelH)
 
-
-
     if~slreq.data.ReqData.exists()
-
         rmidata.storageModeCache('remove',modelH);
         return;
     end
-
     artifactPath=slreq.resolveArtifactPath(modelH,'linktype_rmi_simulink');
     if~isempty(artifactPath)
         slreq.close(artifactPath);
@@ -15,7 +11,6 @@ function close(modelH)
     if slreq.app.MainManager.exists()
         slreq.app.MainManager.modelCloseCallback(modelH);
     end
-
     rmidata.storageModeCache('remove',modelH);
 
     if rmi.isInstalled()&&strcmp(get_param(modelH,'ReqHilite'),'on')
