041de4b20591d2bb352faeacf04b59165a2443bc
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:19:45 +0800

    更新 correctSimulinkUriAndId.m

--- toolbox/shared/reqmgt/+rmisl/correctSimulinkUriAndId.m
+++ toolbox/shared/reqmgt/+rmisl/correctSimulinkUriAndId.m
@@ -1,22 +1,4 @@
 function[modelUri,objId]=correctSimulinkUriAndId(modelUri,objId)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     [modelUri,subId]=fixSimulinkModelUri(modelUri);
     if~isempty(subId)
         objId=fixSimulinkId(objId,subId);
@@ -26,50 +8,42 @@ end
 
 function[fileName,subId]=fixSimulinkModelUri(storedName)
     if rmisl.isHarnessIdString(storedName)
-
         [mdlName,remainder]=strtok(storedName,':');
         subId=[':',rmisl.harnessTargetIdToSID(remainder)];
     elseif rmisl.isSidString(storedName)
-
         [mdlName,subId]=strtok(storedName,':');
     else
-
         mdlName=storedName;
         subId='';
     end
     fileName=getSimulinkFileName(mdlName);
 end
 
+
 function fileName=getSimulinkFileName(mdlName)
     if dig.isProductInstalled('Simulink')&&is_simulink_loaded()
         try
-
             fullFileName=get_param(mdlName,'FileName');
         catch
 
             fullFileName=which(mdlName);
         end
     else
-
-
         fullFileName=which(mdlName);
     end
     if isempty(fullFileName)
         fileName=[mdlName,'.slx'];
-
-
-
     else
         fileName=slreq.uri.getShortNameExt(fullFileName);
     end
 end
+
 
 function longId=fixSimulinkId(localId,parentId)
 
     if contains(parentId,':urn')
         longId=[parentId,localId];
     else
-
         longId=slreq.utils.getLongIdFromShortId(parentId,localId);
     end
 end
