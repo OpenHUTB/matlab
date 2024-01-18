4cdffca89ce84302ebf179a0ee06f197915c669e
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:32:29 +0800

    更新 isDocBlockPath.m

--- toolbox/shared/reqmgt/+rmisl/isDocBlockPath.m
+++ toolbox/shared/reqmgt/+rmisl/isDocBlockPath.m
@@ -1,23 +1,12 @@
 function out=isDocBlockPath(doc)
 
-
-
-
-
     out=false;
 
     doc=convertStringsToChars(doc);
 
-
     [~,~,dExt]=fileparts(doc);
     if strcmp(dExt,'.rtf')
-
-
-
-
         driveLetterRemoved=regexprep(doc,'^\w\:(/|\\)','\\');
-
-
         if~isempty(strfind(driveLetterRemoved,':'))
             out=true;
         end
