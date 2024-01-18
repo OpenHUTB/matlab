5abde8b7614d240d855b4fc5b8760efcafea9f2d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 21:50:06 +0800

    更新 isBuiltinNoRmi.m

--- toolbox/shared/reqmgt/+rmiut/isBuiltinNoRmi.m
+++ toolbox/shared/reqmgt/+rmiut/isBuiltinNoRmi.m
@@ -1,38 +1,27 @@
 function out=isBuiltinNoRmi(in)
-
-
-
-
-
-
-
     mdlName=getMdlName(convertStringsToChars(in));
     if isempty(mdlName)
         out=true;
         return;
     end
-
     exclusionMgr=rmiut.RMExclusionMgr.getInstance();
 
     out=exclusionMgr.checkCached(mdlName);
     if~isempty(out)
         return;
     end
-
     [mdlPath,inside_mlroot]=getMdlPath(mdlName);
 
     if isempty(mdlPath)
-
         out=exclusionMgr.cache(mdlName,false);
     elseif inside_mlroot
-
         out=exclusionMgr.check(mdlPath,mdlName);
     else
-
         out=exclusionMgr.cache(mdlName,false);
     end
 
 end
+
 
 function mdlName=getMdlName(in)
 
@@ -42,15 +31,12 @@ function mdlName=getMdlName(in)
         try
             mdlName=get_param(in,'Name');
         catch ex
-
-
-
-
             MSLDiagnostic(ex.identifier,'%s',ex.message).reportAsWarning;
             mdlName='';
         end
     end
 end
+
 
 function[mdlPath,inside_mlroot]=getMdlPath(mdlName)
 
@@ -61,7 +47,6 @@ function[mdlPath,inside_mlroot]=getMdlPath(mdlName)
             [mdlPath,inside_mlroot]=Simulink.loadsave.resolveFile(mdlPath);
         end
     else
-
         [mdlPath,inside_mlroot]=Simulink.loadsave.resolveFile(mdlName);
     end
 end
