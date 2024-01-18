08986e71932b7b2fc58bd73eea734c5a332e3dec
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:29:31 +0800

    更新 readGraph.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/readGraph.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/readGraph.m
@@ -1,9 +1,5 @@
 function currentModel=readGraph(this,reqFileName,modelName)
 
-
-
-
-
     rf=M3I.XmiReaderFactory();
     rdr=rf.createXmiReader();
     graphModel=rmidd.Graph();
@@ -11,33 +7,15 @@ function currentModel=readGraph(this,reqFileName,modelName)
     oldGraph=rdr.read(reqFileName);
 
     if oldGraph==this.graph
-
-
         currentModel=[];
-
     elseif oldGraph.roots.size<1
-
         currentModel=[];
-
     else
-
-
-
-
-
-
-
-
         t1=M3I.Transaction(this.graph);
         t2=M3I.Transaction(oldGraph);
-
-
-
         oldModel=rmimap.RMIRepository.oldModelFromGraph(oldGraph);
 
-
         rmimap.RMIRepository.getRoot([],'');
-
 
         loadedModelName=oldModel.url;
         if~strcmp(loadedModelName,modelName)
@@ -48,39 +26,27 @@ function currentModel=readGraph(this,reqFileName,modelName)
             error(message('Slvnv:rmigraph:UnmatchedModelName',modelName));
         end
 
-
         currentModel=rmidd.Root(this.graph);
         this.graph.roots.append(currentModel);
         currentModel.url=modelName;
-
-
-
         copyOldNodeData(this,currentModel,oldModel);
-
-
 
         sourceLinkData=oldModel.linkData;
         if sourceLinkData.size>0
             populateGraphFromOldLinkData(this,currentModel,sourceLinkData);
         end
 
-
         oldGraph.destroy;
-
-
         currentModel.setProperty('source','linktype_rmi_simulink');
 
         t1.commit;
         t2.commit;
     end
 
-
     delete(rdr);
-
 
     rmimap.RMIRepository.getRoot([],'');
 end
-
 
 
 function copyOldNodeData(this,currentModel,oldModel)
@@ -104,9 +70,6 @@ function copyOldNodeData(this,currentModel,oldModel)
     for ndi=1:currentModel.nodeData.size
         nd=currentModel.nodeData.at(ndi);
         if~isempty(nd.getValue('groups'))
-
-
-
             node=rmidd.Node(this.graph);
             currentModel.nodes.append(node);
             node.data=nd;
@@ -115,35 +78,24 @@ function copyOldNodeData(this,currentModel,oldModel)
     end
 end
 
+
 function populateGraphFromOldLinkData(this,currentModel,oldLinkData)
-
-
-
 
     ldi=1;
     linkData=oldLinkData.at(ldi);
 
-
     while ldi<=oldLinkData.size
-
-
         node=oldLinkDataToDependentNode(currentModel,linkData);
 
         if isempty(node)||isa(node,'rmidd.Root')
-
             dependentId=oldLinkDataToDependentId(linkData);
-
 
             if isempty(node)
                 node=this.addNode(currentModel,dependentId);
             end
 
-
-
             while ldi<=oldLinkData.size&&strcmp(node.id,dependentId)
-
                 this.appendLink(currentModel,node,linkData,false);
-
 
                 ldi=ldi+1;
                 if ldi<=oldLinkData.size
@@ -152,33 +104,21 @@ function populateGraphFromOldLinkData(this,currentModel,oldLinkData)
                 end
             end
         else
-
-
             groupString=node.getProperty('groups');
             if~isempty(groupString)
 
                 numbers=textscan(groupString,'%d,');
                 groups=numbers{1};
 
-
-
                 gidx=1;
                 while ldi<=oldLinkData.size&&strcmp(node.id,linkData.getValue('dependentId'))
-
-
                     groupId=[node.id,'.',num2str(groups(gidx))];
-
-
-
                     groupNode=rmimap.RMIRepository.getNode(currentModel,groupId);
 
                     if isempty(groupNode)
                         groupNode=this.addNode(currentModel,groupId);
                     end
-
-
                     this.appendLink(currentModel,groupNode,linkData,false);
-
 
                     ldi=ldi+1;
                     gidx=gidx+1;
@@ -188,22 +128,16 @@ function populateGraphFromOldLinkData(this,currentModel,oldLinkData)
                     end
                 end
 
-
                 node.data.destroy;
                 node.destroy;
-
             else
-
                 ordinaryNode=rmimap.RMIRepository.getNode(currentModel,node.id);
 
                 if isempty(ordinaryNode)
                     warning('readGraph error: missing node %s%s',currentModel.url,node.id);
                     ordinaryNode=this.addNode(currentModel,node.id);
                 end
-
-
                 this.appendLink(currentModel,ordinaryNode,linkData,false);
-
 
                 ldi=ldi+1;
 
@@ -215,6 +149,7 @@ function populateGraphFromOldLinkData(this,currentModel,oldLinkData)
         end
     end
 end
+
 
 function node=oldLinkDataToDependentNode(root,linkData)
     dependentId=linkData.getValue('dependentId');
