63092a89476179f315dd01e95683461f955a2739
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:18:28 +0800

    更新 unhighlight.m

--- toolbox/shared/reqmgt/+rmisf/unhighlight.m
+++ toolbox/shared/reqmgt/+rmisf/unhighlight.m
@@ -1,12 +1,7 @@
 function unhighlight(modelName)
-
-
-
-
     machine=find(sfroot,'-isa','Stateflow.Machine','-and','Name',modelName);%#ok<GTARG>
     if~isempty(machine)
         machineID=machine.id;
-
 
         sf('ClearAltStyles',machineID);
         sf('Redraw',machineID);
