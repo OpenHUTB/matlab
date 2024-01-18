5c4f4fbb2e14b835cefab72f415af8d860b2d0e4
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:19:06 +0800

    更新 addModel.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/addModel.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/addModel.m
@@ -1,11 +1,5 @@
 function result=addModel(this,rootName,reqFileName)
 
-
-
-
-
-
-
     if nargin>1
 
         if~ischar(rootName)
@@ -13,11 +7,6 @@ function result=addModel(this,rootName,reqFileName)
             rootH=rootName;
             if rmisl.isComponentHarness(rootH)
                 [~,rootName]=Simulink.harness.internal.sidmap.getHarnessModelUniqueName(rootH);
-
-
-
-
-
                 systemModelName=strtok(rootName,':');
                 parentRoot=this.ensureRoot(systemModelName);
                 harnessData.id=rootName;
@@ -31,10 +20,6 @@ function result=addModel(this,rootName,reqFileName)
         end
 
         if nargin>2
-
-
-
-
             type=rmimap.validateReqFile(reqFileName);
             switch type
             case 'mdlGraph'
@@ -45,28 +30,20 @@ function result=addModel(this,rootName,reqFileName)
                 error(message('Slvnv:rmigraph:InvalidReqFile',reqFileName));
             end
         else
-
-
             t1=M3I.Transaction(this.graph);
             myRoot=rmidd.Root(this.graph);
             this.graph.roots.append(myRoot);
             myRoot.url=rootName;
-
-
             myRoot.setProperty('source','linktype_rmi_simulink');
 
             t1.commit;
         end
-
     else
-
         warning(message('Slvnv:rmigraph:BadUsage'));
         t1=M3I.Transaction(this.graph);
         myRoot=rmidd.Root(this.graph);
         this.graph.append(myRoot);
         myRoot.url='untitled';
-
-
         myRoot.setProperty('source','linktype_rmi_simulink');
 
         t1.commit;
