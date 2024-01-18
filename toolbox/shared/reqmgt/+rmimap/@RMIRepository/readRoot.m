645c6cd947d194aeda46b4a8525aabed9a2651ce
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:30:20 +0800

    更新 readRoot.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/readRoot.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/readRoot.m
@@ -1,19 +1,10 @@
 function newRoot=readRoot(this,reqFileName,srcName)
 
-
-
-
-
     persistent isInitialized
     if isempty(isInitialized)
         rmi('init');
         isInitialized=true;
     end
-
-
-
-
-
 
     dependentLinks={};
     myRoot=rmimap.RMIRepository.getRoot(this.graph,srcName);
@@ -24,62 +15,44 @@ function newRoot=readRoot(this,reqFileName,srcName)
             newRoot=myRoot;
             return;
         else
-
-
-
-
             [dependentLinks,dependeeIds]=rmimap.RMIRepository.getDependentLinks(myRoot);
             this.removeRoot(srcName,true);
         end
     end
 
-
     rf=M3I.XmiReaderFactory();
     rdr=rf.createXmiReader();
     this.graph=rdr.read(reqFileName,this.graph,this.graph,'roots');
     delete(rdr);
-
-
     newRoot=rmimap.RMIRepository.getRoot(this.graph,srcName);
     if isempty(newRoot)
-
-
         if~isempty(fileparts(srcName))
             [newRoot,matched]=rmimap.RMIRepository.findSource(this.graph,srcName,'',true);
             if~isempty(newRoot)
-
                 t0=M3I.Transaction(this.graph);
                 newRoot.setProperty('lastLoaded',num2str(now));
-
                 filesepName=strrep(newRoot.url,'\',filesep);
                 if~isempty(fileparts(filesepName))
-
-
 
                     newRoot.url=srcName;
                 end
                 t0.commit;
             end
             if~isempty(matched)
-
                 this.proxies(srcName)=matched;
             end
         end
     end
-
 
     if isempty(newRoot)
         error(message('Slvnv:rmigraph:NoModelInFile',srcName,reqFileName));
 
     else
         t1=M3I.Transaction(this.graph);
-
         isSimulink=strcmp(newRoot.getProperty('source'),'linktype_rmi_simulink');
         if isSimulink
-
             [childRoots,subRootIdx]=this.extractSubRoots(newRoot);
         end
-
 
         this.merge(newRoot);
 
@@ -92,8 +65,6 @@ function newRoot=readRoot(this,reqFileName,srcName)
                 this.merge(subRoot);
             end
         end
-
-
 
         if~isempty(dependentLinks)
             for i=1:length(dependentLinks)
@@ -113,8 +84,6 @@ function newRoot=readRoot(this,reqFileName,srcName)
         t1.commit;
     end
 
-
-
     rmimap.RMIRepository.getRoot([],'');
 end
 
@@ -123,7 +92,6 @@ end
 function dealSubRootData(subRoot,nodeDataFromParent)
 
     dataCount=nodeDataFromParent.names.size;
-
 
     if isempty(subRoot.data)
         subRoot.addData();
