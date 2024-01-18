69c5bd0ea7a209f06c6ec19152674a239fd61579
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:27:49 +0800

    更新 guidlookup.m

--- toolbox/shared/reqmgt/+rmisl/guidlookup.m
+++ toolbox/shared/reqmgt/+rmisl/guidlookup.m
@@ -1,19 +1,13 @@
 function out=guidlookup(modelH,guidStr)
 
-
-
     guidtable=get_param(modelH,'GUIDTable');
     if isempty(guidtable)
         isLocked=strcmp(get_param(modelH,'Lock'),'on');
         if isLocked
-
-
-
             hasActiveHarness=Simulink.harness.internal.hasActiveHarness(modelH);
             if hasActiveHarness
                 Simulink.harness.internal.setBDLock(modelH,false);
             else
-
                 set_param(modelH,'Lock','off');
             end
         end
