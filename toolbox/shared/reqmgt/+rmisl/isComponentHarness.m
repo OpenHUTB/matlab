39a5d392c112625d9dfe8e64afb8a2a3f1fae18f
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:32:14 +0800

    更新 isComponentHarness.m

--- toolbox/shared/reqmgt/+rmisl/isComponentHarness.m
+++ toolbox/shared/reqmgt/+rmisl/isComponentHarness.m
@@ -1,15 +1,11 @@
 function yesno=isComponentHarness(mdl,~)
 
-
-
-
     mdl=convertStringsToChars(mdl);
 
     if ischar(mdl)&&any(mdl==':')
 
         mdl=strtok(mdl,':');
     end
-
 
     try
         yesno=Simulink.harness.isHarnessBD(mdl);
