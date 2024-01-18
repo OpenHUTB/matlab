5f5fb4490bfbf9607b8bc958922aba22fa0a0b5a
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:20:07 +0800

    更新 clear.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/clear.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/clear.m
@@ -1,13 +1,8 @@
-
-
-
-
 function clear()
 
     rmidata.RmiSlData.reset();
     rmiml.RmiMlData.reset();
     rmide.RmiDeData.reset();
     rmitm.RmiTmData.reset();
-
     rmimap.RMIRepository.getInstance.reset();
 end
