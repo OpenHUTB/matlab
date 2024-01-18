dc9e932af8d6dd628ecfdd7653fb3f47b635c466
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 22:06:14 +0800

    更新 rename.m

--- toolbox/shared/reqmgt/+rmitm/rename.m
+++ toolbox/shared/reqmgt/+rmitm/rename.m
@@ -1,14 +1,8 @@
 function newReq=rename(oldTestFile,newTestFile)
-
-
-
-
     oldTestFile=convertStringsToChars(oldTestFile);
     newTestFile=convertStringsToChars(newTestFile);
 
     newReq='';
-
-
     linkSet=slreq.utils.getLinkSet(oldTestFile);
     if~isempty(linkSet)
         linkSet.moveArtifact(newTestFile);
