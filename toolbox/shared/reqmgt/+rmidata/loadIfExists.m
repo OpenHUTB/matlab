94eab690f1b749c54c2eb012e80a429b314ef497
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 13:10:29 +0800

    更新 loadIfExists.m

--- toolbox/shared/reqmgt/+rmidata/loadIfExists.m
+++ toolbox/shared/reqmgt/+rmidata/loadIfExists.m
@@ -1,20 +1,11 @@
 function[result,deferredNotification]=loadIfExists(modelH)
 
-
-
-
     modelPath=get_param(modelH,'FileName');
     deferredNotification={};
 
     if isempty(modelPath)
-
-
         result=false;
     else
-
-
-
-
         [result,deferredNotification]=slreq.utils.loadLinkSet(modelPath,false);
     end
 end
