0fbbdac8ba43e938d6e26afc2f008f8ce2684d22
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:31:00 +0800

    更新 inSubsystemReference.m

--- toolbox/shared/reqmgt/+rmisl/inSubsystemReference.m
+++ toolbox/shared/reqmgt/+rmisl/inSubsystemReference.m
@@ -1,17 +1,11 @@
 function result=inSubsystemReference(objH,isSf)
 
-
-
-
-
     if nargin<2||isempty(isSf)
         [isSf,objH,errMsg]=rmi.resolveobj(objH);
         if~isempty(errMsg)
             error(message('Simulink:util:ErrorOfExecutingCommand','rmi.resolveobj()',errMsg));
         end
     end
-
-
     refSid=rmisl.getRefSidFromObjSSRefInstance(objH,isSf,false);
     result=~isempty(refSid);
 end
