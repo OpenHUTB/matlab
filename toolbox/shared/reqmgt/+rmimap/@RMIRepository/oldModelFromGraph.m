98f01b9ad6a3f42c015b0675ee885c6e7e8dc509
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:26:53 +0800

    更新 oldModelFromGraph.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/oldModelFromGraph.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/oldModelFromGraph.m
@@ -1,9 +1,5 @@
 function oldModel=oldModelFromGraph(oldGraph)
 
-
-
-
-
     for r=1:oldGraph.roots.size
         root=oldGraph.roots.at(r);
         rootSrcType=root.getProperty('source');
@@ -15,13 +11,10 @@ function oldModel=oldModelFromGraph(oldGraph)
 
     for i=1:oldModel.links.size
         link=oldModel.links.at(i);
-
         destUrl=link.getProperty('dependeeUrl');
         source=rmimap.RMIRepository.getRoot(oldGraph,destUrl);
-
         destType=source.getProperty('source');
         link.setProperty('source',destType);
-
 
         if strcmp(destType,'linktype_rmi_simulink')&&strcmp(destUrl,'$ModelName$')
             link.setProperty('dependeeUrl',root.url);
