4d00e4435c799403d7f328221c3d6c555d9ebe20
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:45:36 +0800

    更新 sigb_write_info.m

--- toolbox/shared/reqmgt/+rmisl/sigb_write_info.m
+++ toolbox/shared/reqmgt/+rmisl/sigb_write_info.m
@@ -1,16 +1,9 @@
 function sigb_write_info(blkInfo)
-
-
-
-
-
-
     fromWsH=find_system(blkInfo.blockH,'FollowLinks','on'...
     ,'MatchFilter',@Simulink.match.internal.filterOutInactiveVariantSubsystemChoices...
     ,'LookUnderMasks','all'...
     ,'BlockType','FromWorkspace');
     if~strcmp(get_param(fromWsH,'StaticLinkStatus'),'implicit')
-
 
         blkInfo.blockH=[];
         blkInfo.modelH=[];
