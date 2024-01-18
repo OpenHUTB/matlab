244a4a67a79a1bade15f9261778468bb6698ea12
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 22:05:12 +0800

    更新 hasData.m

--- toolbox/shared/reqmgt/+rmitm/hasData.m
+++ toolbox/shared/reqmgt/+rmitm/hasData.m
@@ -1,7 +1,5 @@
 function yesno=hasData(testSuiteFile)
-
     testSuiteFile=convertStringsToChars(testSuiteFile);
-
 
     if~any(testSuiteFile=='.')
         testSuiteFile=rmitm.getFilePath(testSuiteFile);
