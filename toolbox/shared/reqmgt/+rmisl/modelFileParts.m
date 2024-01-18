ba8bb123619e7013db45e386c7cf0cf81240c141
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:41:34 +0800

    更新 modelFileParts.m

--- toolbox/shared/reqmgt/+rmisl/modelFileParts.m
+++ toolbox/shared/reqmgt/+rmisl/modelFileParts.m
@@ -3,8 +3,6 @@ function[mdlLocation,mdlName,mdlExt]=modelFileParts(modelH,resolveHarnessId)
         resolveHarnessId=true;
     end
     if resolveHarnessId&&rmisl.isComponentHarness(modelH)
-
-
         [mdlLocation,mdlName,mdlExt]=Simulink.harness.internal.sidmap.getHarnessModelUniqueName(modelH);
     else
         modelFilePath=get_param(modelH,'FileName');
