c3e8ab0cffd66789dddacf24e810f5af3c80738a
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:20:18 +0800

    更新 clearLinks.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/clearLinks.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/clearLinks.m
@@ -1,18 +1,11 @@
 function clearLinks(this,elt,deleteIncoming)%#ok<INUSL>
 
-
-
-
-
-
-
     links=elt.dependeeLinks;
     linkList={};
 
     for lk=1:links.size
         linkList{end+1}=links.at(lk);%#ok<*AGROW>
     end
-
 
     for lk=1:length(linkList)
         link=linkList{lk};
