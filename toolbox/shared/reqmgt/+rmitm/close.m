6dca676249f7898bc59be4ed29d629e854a438ae
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 22:02:44 +0800

    更新 close.m

--- toolbox/shared/reqmgt/+rmitm/close.m
+++ toolbox/shared/reqmgt/+rmitm/close.m
@@ -1,10 +1,5 @@
 function wasSaved=close(testSuite)
-
-
-
-
     testSuite=convertStringsToChars(testSuite);
-
     testFilePath=rmitm.getFilePath(testSuite);
 
     wasSaved=slreq.close(testFilePath);
