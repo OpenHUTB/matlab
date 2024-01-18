67ead12d9e9c8f9fc6f182de66d1379feb1f3c1d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 12:55:05 +0800

    更新 discard.m

--- toolbox/shared/reqmgt/+rmidata/discard.m
+++ toolbox/shared/reqmgt/+rmidata/discard.m
@@ -1,22 +1,12 @@
 function discard(artifact)
 
-
-
-
     if slreq.data.ReqData.exists()
         slreq.discardLinkSet(artifact,true);
-
-
     end
 
     if isnumeric(artifact)
-
-
         modelH=artifact;
-
-
         rmidata.storageModeCache('remove',modelH);
-
 
         if rmi.isInstalled()&&strcmp(get_param(modelH,'ReqHilite'),'on')
             rmi.Informer.closeModel(get_param(modelH,'Name'));
