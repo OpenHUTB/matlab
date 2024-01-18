7646ee7bcdeafa7dccd7044c5c5a51df132ef9a2
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:21:17 +0800

    更新 destroyChildRoots.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/destroyChildRoots.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/destroyChildRoots.m
@@ -1,23 +1,12 @@
 function destroyChildRoots(this,srcRoot)
 
-
-
-
-
     nodeDataLength=srcRoot.nodeData.size;
     for i=1:nodeDataLength
         ndData=srcRoot.nodeData.at(i);
         if rmimap.RMIRepository.isSimulinkSubroot(ndData)
-
             childId=[srcRoot.url,ndData.getValue('id')];
             childRoot=rmimap.RMIRepository.getRoot(this.graph,childId);
             if isempty(childRoot)
-
-
-
-
-
-
 
             else
                 if strcmp(ndData.getValue('source'),'linktype_rmi_matlab')
