bb6ef9f2a1e71e1293e533b30a803931ad842184
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:15:29 +0800

    更新 parse_trans_path.m

--- toolbox/shared/reqmgt/+rmisf/parse_trans_path.m
+++ toolbox/shared/reqmgt/+rmisf/parse_trans_path.m
@@ -1,15 +1,10 @@
 function[labelStr,fromStr,toStr]=parse_trans_path(transPath)
 
-
-
-
     labelStr=regexp(transPath,'.*?from \"','match','once');
     labelStr=strrep(labelStr,' from "','');
 
-
     fromStr=regexp(transPath,'from \".*?\"','match','once');
     toStr=regexp(transPath,'to \".*?\"','match','once');
-
 
     fromStr=regexp(fromStr,'\".*?\"','match','once');
     toStr=regexp(toStr,'\".*?\"','match','once');
