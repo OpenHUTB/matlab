7219a0f4e2335c24979c449860f2eb21d68522a5
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:14:39 +0800

    更新 highlight.m

--- toolbox/shared/reqmgt/+rmisf/highlight.m
+++ toolbox/shared/reqmgt/+rmisf/highlight.m
@@ -1,11 +1,6 @@
 function highlight(highlightHs,fadeHs,modelName,mode)
-
-
-
     modelName=convertStringsToChars(modelName);
     mode=convertStringsToChars(mode);
-
-
 
     if strcmp(mode,'req')||strcmp(mode,'on')
         style=sf_style('req');
@@ -16,9 +11,7 @@ function highlight(highlightHs,fadeHs,modelName,mode)
         warn(['Unsupported style name in sfHighlight: ',mode]);
         return;
     end
-
     hasActiveHarness=Simulink.harness.internal.hasActiveHarness(modelName);
-
 
     if~strcmp(mode,'off')
         fade_style=sf_style('fade');
@@ -26,7 +19,6 @@ function highlight(highlightHs,fadeHs,modelName,mode)
             highlightOneObject(fadeHs(i),fade_style,hasActiveHarness);
         end
     end
-
 
     allHighlighted=[];
     for i=1:length(highlightHs)
@@ -40,14 +32,13 @@ function highlight(highlightHs,fadeHs,modelName,mode)
             allHighlighted=highlightWithParents(thisId,allHighlighted,style,hasActiveHarness);
         end
     end
-
-
     machine=find(sfroot,'-isa','Stateflow.Machine','-and','Name',modelName);%#ok<GTARG>
     if~isempty(machine)
         machineID=machine.id;
         sf('Redraw',machineID);
     end
 end
+
 
 function allHighlighted=highlightWithParents(myId,allHighlighted,style,hasActiveHarness)
     highlightOneObject(myId,style,hasActiveHarness);
@@ -57,6 +48,7 @@ function allHighlighted=highlightWithParents(myId,allHighlighted,style,hasActive
     end
     allHighlighted(end+1)=myId;
 end
+
 
 function highlightOneObject(objId,style,hasActiveHarness)
     sf_set_style(objId,style);
