161d050581475b2900fd285d0825b0ad82c8af47
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 13:09:30 +0800

    更新 init.m

--- toolbox/shared/reqmgt/+rmidata/init.m
+++ toolbox/shared/reqmgt/+rmidata/init.m
@@ -1,41 +1,24 @@
 function[deferredNotification,isLinkSetLoaded]=init(modelH)
 
-
-
-
     deferredNotification={};
     isLinkSetLoaded=false;
-
 
     if rmiut.isBuiltinNoRmi(modelH)
         rmidata.storageModeCache('set',modelH,false);
         return;
     end
 
-
     if rmidata.storageModeCache('check',modelH)
         return;
     end
-
-
-
     [isHarnessBD,mainModel]=Simulink.harness.internal.sidmap.isHarnessBDPostLoad(modelH);
     if isHarnessBD
-
-
         mainModelMode=rmidata.isExternal(mainModel);
         if mainModelMode
 
-
-
-
-
             if rmipref('StoreDataExternally')&&~rmidata.bdHasExternalData(mainModel)
                 if rmisl.modelHasEmbeddedReqInfo(modelH)
-
-
                     rmidata.storageModeCache('set',mainModel,false);
-
                     rmidata.storageModeCache('set',modelH,false);
                     return;
                 end
@@ -45,7 +28,6 @@ function[deferredNotification,isLinkSetLoaded]=init(modelH)
         return;
     end
 
-
     if rmisl.modelHasEmbeddedReqInfo(modelH)
         if is_slx_format(modelH)
             deferredNotification={message('Slvnv:slreq:DataNeedsUpdating'),message('Slvnv:slreq:UpdateNow')};
@@ -54,20 +36,10 @@ function[deferredNotification,isLinkSetLoaded]=init(modelH)
         end
         rmidata.storageModeCache('set',modelH,false);
 
-
-
-
         slreq.data.ReqData.getInstance();
 
     elseif rmidata.bdHasExternalData(modelH,true)
-
         rmidata.storageModeCache('set',modelH,true);
-
-
-
-
-
-
         artifact=get_param(modelH,'FileName');
         linkSet=slreq.data.ReqData.getInstance.getLinkSet(artifact);
         if slreq.internal.isSharedSlreqInstalled()
@@ -77,7 +49,6 @@ function[deferredNotification,isLinkSetLoaded]=init(modelH)
             lsm.onArtifactLoad(artifact);
         end
 
-
         if slreq.app.MainManager.hasEditor()
             mgr=slreq.app.MainManager.getInstance();
             if mgr.isChangeInformationEnabled()
@@ -85,14 +56,10 @@ function[deferredNotification,isLinkSetLoaded]=init(modelH)
                 chTracker.refresh;
             end
         end
-
     elseif isempty(get_param(modelH,'FileName'))
-
-
         return;
 
     else
-
         [isLinkSetLoaded,deferredNotification]=rmidata.loadIfExists(modelH);
         rmidata.storageModeCache('set',modelH,true);
     end
