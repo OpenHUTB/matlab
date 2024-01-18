9fb80c83cbd654c8d0870647f26713ef0433f410
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 13:12:37 +0800

    更新 save.m

--- toolbox/shared/reqmgt/+rmidata/save.m
+++ toolbox/shared/reqmgt/+rmidata/save.m
@@ -1,21 +1,11 @@
 function savedFilePath=save(modelH,varargin)
 
-
-
-
-
-
-
-
-
-
     modelH=convertStringsToChars(modelH);
     if nargin>1
         [varargin{:}]=convertStringsToChars(varargin{:});
     end
 
     savedFilePath='';
-
 
     if ischar(modelH)
         modelH=get_param(modelH,'Handle');
@@ -25,52 +15,32 @@ function savedFilePath=save(modelH,varargin)
         return;
     end
 
-
-
-
-
-
     if rmisl.isComponentHarness(modelH,true)
         mainModel=Simulink.harness.internal.getHarnessOwnerBD(modelH);
         modelH=get_param(mainModel,'Handle');
     end
-
-
     artifactPath=get_param(modelH,'FileName');
     if isempty(artifactPath)
 
         return;
     end
-
-
     prevFileName=get_param(modelH,'PreviousFileName');
-
-
     linkSet=slreq.data.ReqData.getInstance.getLinkSet(artifactPath);
     if isempty(linkSet)&&~isempty(prevFileName)
-
         linkSet=slreq.data.ReqData.getInstance.getLinkSet(prevFileName);
     end
 
     if isempty(linkSet)
         return;
     end
-
-
-
-
     obs2clean=rmidata.embeddedObjCache('get',modelH);
     if~isempty(obs2clean)
         rmidata.cleanEmbeddedLinks(obs2clean);
     end
-
-
-
     if~isempty(prevFileName)&&~strcmp(prevFileName,artifactPath)
         wasDirty=linkSet.dirty;
         hasChanges=true;
         slreq.utils.renameLinkSet(prevFileName,artifactPath);
-
 
         if~wasDirty
             slreq.data.ReqData.getInstance.forceDirtyFlag(linkSet,false);
@@ -79,7 +49,6 @@ function savedFilePath=save(modelH,varargin)
         hasChanges=linkSet.dirty;
         wasDirty=hasChanges;
     end
-
 
     if~hasChanges
         if~isempty(varargin)&&ischar(varargin{1})
@@ -93,10 +62,6 @@ function savedFilePath=save(modelH,varargin)
 
     if hasChanges
         if wasDirty
-
-
-
-
             unsavedReqSets=linkSet.getUnsavedDependeeReqSets();
             goAhead=isempty(unsavedReqSets)||slreq.utils.saveWithPrompt(unsavedReqSets,linkSet.artifact);
         else
