567f6ba958a0443ee31feb2e0d38b2b9e0cc41f6
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 14:34:31 +0800

    更新 updateRanges.m

--- toolbox/shared/reqmgt/+rmimap/@RMIRepository/updateRanges.m
+++ toolbox/shared/reqmgt/+rmimap/@RMIRepository/updateRanges.m
@@ -1,32 +1,19 @@
 function mdlName=updateRanges(this,srcName,newIds,newStarts,newEnds)
-
-
-
-
-
     srcRoot=rmimap.RMIRepository.getRoot(this.graph,srcName);
     if isempty(srcRoot)
         error('RMIRepository: updateRange called for unknown source %s',srcName);
     end
 
-
     t=M3I.Transaction(this.graph);
 
     if isempty(srcRoot.id)
 
-
-
-
-
         [~,srcRoot.id]=strtok(srcName,':');
         srcRoot.setProperty('id',srcRoot.id);
     end
-
     srcRoot.setProperty('rangeStarts',newStarts);
     srcRoot.setProperty('rangeEnds',newEnds);
     srcRoot.setProperty('rangeLabels',newIds);
-
-
     [isMatlabFunction,mdlName]=rmisl.isSidString(srcName,false);
     cache=rmiut.escapeForXml(rmiml.getText(srcName));
     srcRoot.setProperty('cache',cache);
@@ -34,7 +21,6 @@ function mdlName=updateRanges(this,srcName,newIds,newStarts,newEnds)
         parentRoot=rmimap.RMIRepository.getRoot(this.graph,mdlName);
         this.updateTextNodeData(parentRoot,srcRoot);
     end
-
 
     t.commit();
 end
