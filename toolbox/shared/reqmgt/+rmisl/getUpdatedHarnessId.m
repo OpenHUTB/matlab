71fa208fdc2e5fe91f83cc41a26eafdaafecd3e9
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 19:27:21 +0800

    更新 getUpdatedHarnessId.m

--- toolbox/shared/reqmgt/+rmisl/getUpdatedHarnessId.m
+++ toolbox/shared/reqmgt/+rmisl/getUpdatedHarnessId.m
@@ -1,14 +1,8 @@
 function newId=getUpdatedHarnessId(rootName,id)
 
-
-
-
-
-
     if id(1)==':'
         id(1)=[];
     end
-
     stmUpdate=Simulink.harness.internal.getUUIDChanged(rootName,id);
     if stmUpdate.uuidChanged
         newId=[':',stmUpdate.newUUID];
