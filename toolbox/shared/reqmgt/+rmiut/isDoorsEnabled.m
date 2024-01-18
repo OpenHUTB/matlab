c4304f746ce435da260b5042b60f55c728ec9893
王海东 <wanghaidong@hutb.edu.cn>
2024-01-18 21:50:49 +0800

    更新 isDoorsEnabled.m

--- toolbox/shared/reqmgt/+rmiut/isDoorsEnabled.m
+++ toolbox/shared/reqmgt/+rmiut/isDoorsEnabled.m
@@ -1,14 +1,10 @@
 function tf=isDoorsEnabled()
 
-
-
-
     if~ispc
         tf=false;
 
     else
         [installed,licensed]=rmi.isInstalled();
-
         tf=installed&&licensed&&is_doors_installed();
     end
 end
