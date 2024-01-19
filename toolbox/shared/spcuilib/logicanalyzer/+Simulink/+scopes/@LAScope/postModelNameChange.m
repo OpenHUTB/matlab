402fdef50ac47a67f224ab44a67646431539dcf2
王海东 <wanghaidong@hutb.edu.cn>
2024-01-19 12:50:23 +0800

    更新 postModelNameChange.m

--- toolbox/shared/spcuilib/logicanalyzer/+Simulink/+scopes/@LAScope/postModelNameChange.m
+++ toolbox/shared/spcuilib/logicanalyzer/+Simulink/+scopes/@LAScope/postModelNameChange.m
@@ -1,8 +1,5 @@
 function postModelNameChange(this)
 
-
-
-
     clientID=this.ClientID;
     hModel=str2double(clientID);
     this.WebWindow.Title=getString(message('Spcuilib:logicanalyzer:WebWindowTitle',get_param(hModel,'Name')));
