741326ef0bfe80a9dd6e0b944ac0101c61df26f2
王海东 <wanghaidong@hutb.edu.cn>
2024-01-19 13:11:35 +0800

    更新 PublishScopes.m

--- toolbox/shared/spcuilib/unifiedscopes/+internal/+scopes/+publish/PublishScopes.m
+++ toolbox/shared/spcuilib/unifiedscopes/+internal/+scopes/+publish/PublishScopes.m
@@ -1,35 +1,23 @@
 classdef PublishScopes<internal.matlab.publish.PublishFigures
 
-
-
-
     methods
         function obj=PublishScopes(options)
             obj=obj@internal.matlab.publish.PublishFigures(options);
         end
     end
 
+
     methods(Static)
         function imgFilename=snapFigure(f,varargin)
-
-
-
-
 
             scope=f.UserData;
             if(isa(scope,'matlabshared.scopes.UnifiedScope')||...
                 isa(scope,'uiscopes.Framework'))&&...
                 ~isSynchronous(scope.DataSource)
 
-
                 try
                     convertToSynchronous(scope.DataSource.Updater);
                 catch me %#ok<NASGU>
-
-
-
-
-
 
                 end
             end
