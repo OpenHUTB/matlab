f0c55f9e660f0fb490108bbb7bb84f61c4227adc
王海东 <wanghaidong@hutb.edu.cn>
2024-01-19 12:41:13 +0800

    更新 getURL.m

--- toolbox/shared/spcuilib/logicanalyzer/+Simulink/+scopes/@LAScope/getURL.m
+++ toolbox/shared/spcuilib/logicanalyzer/+Simulink/+scopes/@LAScope/getURL.m
@@ -1,8 +1,5 @@
 function URL=getURL(clientID)
 
-
-
-
     URL='/toolbox/shared/spcuilib/logicanalyzer/web/logicanalyzer/logicanalyzer-simulink';
     postFix=['.html?ClientID=',clientID];
     feature=slfeature('slLogicAnalyzerApp');
@@ -10,7 +7,6 @@ function URL=getURL(clientID)
     if feature<2||feature>3
         URL=[URL,'-debug'];
     end
-
 
     URL=connector.getUrl([URL,postFix]);
 
