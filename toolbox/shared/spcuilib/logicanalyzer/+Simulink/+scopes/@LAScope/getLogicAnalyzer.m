be4caeddd330d4c4a00ba3a63cf4e3e832fad372
王海东 <wanghaidong@hutb.edu.cn>
2024-01-19 12:41:00 +0800

    更新 getLogicAnalyzer.m

--- toolbox/shared/spcuilib/logicanalyzer/+Simulink/+scopes/@LAScope/getLogicAnalyzer.m
+++ toolbox/shared/spcuilib/logicanalyzer/+Simulink/+scopes/@LAScope/getLogicAnalyzer.m
@@ -1,12 +1,4 @@
 function laScope=getLogicAnalyzer(modelName)
-
-
-
-
-
-
-
-
     modelHandle=get_param(modelName,'Handle');
     laScope=Simulink.scopes.LAScope.GetNonEmptyInstanceForModel(modelHandle,modelName);
 end
