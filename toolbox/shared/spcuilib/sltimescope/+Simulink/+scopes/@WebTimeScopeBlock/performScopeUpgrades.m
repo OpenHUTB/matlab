a2af080f8d42d3dc9ba1bf4ffc3581e6de3b70a8
王海东 <wanghaidong@hutb.edu.cn>
2024-01-19 13:10:37 +0800

    更新 performScopeUpgrades.m

--- toolbox/shared/spcuilib/sltimescope/+Simulink/+scopes/@WebTimeScopeBlock/performScopeUpgrades.m
+++ toolbox/shared/spcuilib/sltimescope/+Simulink/+scopes/@WebTimeScopeBlock/performScopeUpgrades.m
@@ -1,29 +1,13 @@
 function performScopeUpgrades(this)
 
-
-
-
-
-
-
-
-
-
-
     block=get_param(this.FullPath,'Handle');
     block_diagram_handle=bdroot(block);
-
-
-
-
 
     if any(strcmp(get_param(block_diagram_handle,'Name'),...
         {'dspsnks4','simulink','simviewers'}))||...
         strcmp(get_param(block_diagram_handle,'Lock'),'on')
         return;
     end
-
-
 
     ud=get_param(block,'UserData');
     if(isstruct(ud)&&isfield(ud,'Scope'))
@@ -36,7 +20,6 @@ function performScopeUpgrades(this)
                 scopeSpec=[];
             end
 
-
             if isempty(scopeSpec)
                 scopeSpec=Simulink.scopes.TimeScopeBlockCfg;
                 warning(message('Spcuilib:scopes:EmptyConfig'));
@@ -46,39 +29,26 @@ function performScopeUpgrades(this)
 
         set_param(block,'UserData',[],'UserDataPersistent','off');
     else
-
-
         scopeSpec=get_param(block,'ScopeSpecification');
         if isempty(scopeSpec)
             scopeSpec=get_param(block,'ScopeSpecificationObject');
         end
 
         if isempty(scopeSpec)
-
-
             scopeCfgString=get_param(block,'DefaultConfigurationName');
         else
             scopeCfgString=scopeSpec.toString(false,true);
         end
     end
 
-
-
     if~isempty(scopeSpec)
-
-
-
         if~isempty(scopeSpec.Scope)
             delete(scopeSpec.Scope);
             scopeSpec.Scope=[];
             scopeSpec.Block=[];
         end
 
-
         scopeSpec.Block=this;
-
-
-
 
         set_param(block,...
         'DefaultConfigurationName',class(scopeSpec),...
@@ -87,8 +57,6 @@ function performScopeUpgrades(this)
     end
 
     set_param(block,'ScopeSpecificationString',scopeCfgString);
-
-
 
     resetCallbacks(block,'OpenFcn');
     resetCallbacks(block,'DeleteFcn');
@@ -102,11 +70,6 @@ end
 
 
 function resetCallbacks(blk,uMethod)
-
-
-
-
-
     uCallback=get_param(blk,uMethod);
     if strncmp(uCallback,'scopeext',8)||strncmp(uCallback,'simscope',8)
         set(blk,uMethod,'');
