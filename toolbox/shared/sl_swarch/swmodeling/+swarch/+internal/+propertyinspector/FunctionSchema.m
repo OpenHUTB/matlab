dbb0701155c78ccd8c8549b52631140af4424339
王海东 <wanghaidong@hutb.edu.cn>
2024-01-19 19:27:06 +0800

    更新 FunctionSchema.m

--- toolbox/shared/sl_swarch/swmodeling/+swarch/+internal/+propertyinspector/FunctionSchema.m
+++ toolbox/shared/sl_swarch/swmodeling/+swarch/+internal/+propertyinspector/FunctionSchema.m
@@ -1,33 +1,29 @@
 classdef FunctionSchema<swarch.internal.propertyinspector.SoftwareElementPropertySchema
 
-
-
-
     properties(Access=private)
 RootMFModel
     end
+
 
     methods
         function this=FunctionSchema(studio,rootFunc)
             this=this@swarch.internal.propertyinspector.SoftwareElementPropertySchema(studio,rootFunc);
             this.RootMFModel=mf.zero.Model.empty;
-
-
             protoMFModel=mf.zero.getModel(this.PrototypableZCModel);
             rootMFModel=mf.zero.getModel(rootFunc);
             if protoMFModel~=rootMFModel
-
-
                 this.RootMFModel=rootMFModel;
                 this.RootMFModel.addObservingListener(this.RefreshPIListener);
             end
         end
+
 
         function delete(this)
             if~isempty(this.RootMFModel)&&isvalid(this.RootMFModel)
                 this.RootMFModel.removeListener(this.RefreshPIListener);
             end
         end
+
 
         function typeStr=getObjectType(this)
             switch this.getPrototypable().type
@@ -42,17 +38,17 @@ RootMFModel
             end
         end
 
+
         function setPrototypableName(this,value)
-
-
-
             swarch.utils.setFunctionAndRootInportBlockName(this.ElementImpl,value);
         end
+
 
         function name=getPrototypableName(this)
             name=this.ElementImpl.getName();
         end
     end
+
 
     methods(Access=protected)
         function functionElem=getPrototypable(this)
