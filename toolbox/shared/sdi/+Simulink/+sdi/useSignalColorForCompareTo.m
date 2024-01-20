253942ec08dbfdd4592bfa0b12c62b5f0409c918
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 21:25:38 +0800

    更新 useSignalColorForCompareTo.m

--- toolbox/shared/sdi/+Simulink/+sdi/useSignalColorForCompareTo.m
+++ toolbox/shared/sdi/+Simulink/+sdi/useSignalColorForCompareTo.m
@@ -1,13 +1,5 @@
 function out=useSignalColorForCompareTo(varargin)
 
-
-
-
-
-
-
-
-
     persistent compareToColor;
     currentCompareToColor=Simulink.sdi.getComparisonColor('compareToColor');
     if nargin==0
@@ -16,13 +8,11 @@ function out=useSignalColorForCompareTo(varargin)
         if~isempty(currentCompareToColor)
             compareToColor=currentCompareToColor;
         elseif isempty(compareToColor)
-
             compareToColor=[217/255,83/255,25/255];
         end
         boolValue=varargin{1};
         if boolValue
             out=true;
-
             Simulink.sdi.setComparisonColor('compareToColor',[-1.0,-1.0,-1.0]);
         else
             out=false;
