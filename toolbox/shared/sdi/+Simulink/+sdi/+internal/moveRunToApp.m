046644872932eb74b20623e1e099358903c07c0d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 21:41:09 +0800

    更新 moveRunToApp.m

--- toolbox/shared/sdi/+Simulink/+sdi/+internal/moveRunToApp.m
+++ toolbox/shared/sdi/+Simulink/+sdi/+internal/moveRunToApp.m
@@ -1,12 +1,4 @@
 function moveRunToApp(runID,newAppName,varargin)
-
-
-
-
-
-
-
-
     Simulink.sdi.internal.flushStreamingBackend();
     eng=Simulink.sdi.Instance.engine;
     moveRunToApp(eng,runID,newAppName,varargin{:});
