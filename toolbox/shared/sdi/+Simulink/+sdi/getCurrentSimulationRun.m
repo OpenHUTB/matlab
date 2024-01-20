c7000bddaac15c318c47e895d36f6ee49337ff4a
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 21:06:11 +0800

    更新 getCurrentSimulationRun.m

--- toolbox/shared/sdi/+Simulink/+sdi/getCurrentSimulationRun.m
+++ toolbox/shared/sdi/+Simulink/+sdi/getCurrentSimulationRun.m
@@ -1,12 +1,5 @@
 function ret=getCurrentSimulationRun(mdl,varargin)
 
-
-
-
-
-
-
-
     try
         [varargin{:}]=convertStringsToChars(varargin{:});
         mdl=convertStringsToChars(mdl);
@@ -15,13 +8,10 @@ function ret=getCurrentSimulationRun(mdl,varargin)
         if(nargin==3&&varargin{2}==false)
 
             varargin={varargin{1}};
-
             Simulink.sdi.internal.partialFlushStreamingBackend(mdl,varargin{:});
         else
-
             Simulink.sdi.internal.flushStreamingBackend();
         end
-
 
         eng=Simulink.sdi.Instance.engine;
         ret=eng.safeTransaction(@locGetRun,mdl,varargin{:});
@@ -30,12 +20,12 @@ function ret=getCurrentSimulationRun(mdl,varargin)
     end
 end
 
+
 function run=locGetRun(mdl,varargin)
     run=Simulink.sdi.Run.empty();
     eng=Simulink.sdi.Instance.engine;
     runID=eng.getCurrentStreamingRunID(mdl,varargin{:});
     if runID
-
         run=Simulink.sdi.Instance.engine.getRun(runID);
     end
 end
