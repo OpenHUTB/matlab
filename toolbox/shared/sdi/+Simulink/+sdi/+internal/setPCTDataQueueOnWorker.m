ce2e0dfaafca97a2f49e35bdc69ee24262e89339
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 21:45:36 +0800

    更新 setPCTDataQueueOnWorker.m

--- toolbox/shared/sdi/+Simulink/+sdi/+internal/setPCTDataQueueOnWorker.m
+++ toolbox/shared/sdi/+Simulink/+sdi/+internal/setPCTDataQueueOnWorker.m
@@ -1,33 +1,20 @@
 function setPCTDataQueueOnWorker(dc,mode)
 
-
-
     eng=Simulink.sdi.Instance.engine;
-
-
     Simulink.sdi.internal.flushStreamingBackend();
 drawnow
 
-
     eng.PCTSupportMode=mode;
-
 
     if isempty(dc)
         return
     end
-
-
-
     bWasEnabled=Simulink.sdi.internal.isParallelPoolSetup();
     Simulink.sdi.internal.isParallelPoolSetup(true);
     eng.PCTDataQueueFromWorker=dc;
 
-
-
     eng.PCTDataQueueToWorker=parallel.pool.DataQueue();
     afterEach(eng.PCTDataQueueToWorker,@(x)onPCTWorkerMsg(eng,x));
-
-
 
     if~bWasEnabled
         try
@@ -38,17 +25,13 @@ drawnow
         end
     end
 
-
-
     msg.Type='new_worker';
     msg.DataQueue=eng.PCTDataQueueToWorker;
     Simulink.sdi.internal.sendMsgFromPCTWorker(msg,false,eng);
 end
 
+
 function locCleanupWorker()
-
-
-
     sdi.Repository.closeRepositoryOnWorker();
     Simulink.sdi.Instance.setEngine([]);
 end
