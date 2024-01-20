ddb3e7fcc775920551bdad60b9e27cdeba03fed2
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 20:40:43 +0800

    更新 copyRun.m

--- toolbox/shared/sdi/+Simulink/+sdi/copyRun.m
+++ toolbox/shared/sdi/+Simulink/+sdi/copyRun.m
@@ -1,11 +1,4 @@
 function[runID,runIndex,signalIDs]=copyRun(varargin)
-
-
-
-
-
-
-
     Simulink.sdi.internal.flushStreamingBackend();
     [runID,runIndex,signalIDs]=Simulink.sdi.Instance.engine.copyRun(varargin{:});
 end
