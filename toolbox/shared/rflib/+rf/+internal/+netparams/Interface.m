873b9cceb4acc637a3ceca179720ef9922fbf746
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 17:50:31 +0800

    更新 Interface.m

--- toolbox/shared/rflib/+rf/+internal/+netparams/Interface.m
+++ toolbox/shared/rflib/+rf/+internal/+netparams/Interface.m
@@ -1,47 +1,13 @@
 classdef(HandleCompatible=true)Interface
 
-
-
-
-
-
-
-
-
-
-
     properties(Abstract,Constant,Hidden)
 NetworkParameterNarginchkInputs
 
-
-
-
-
-
-
-
-
-
     end
+
 
     methods(Abstract,Access=protected)
         [str,data,freq,z0]=networkParameterInfo(obj,varargin)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     end
 
