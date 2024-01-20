41c777f354031c1527ed55a1c5e9198d4b7e0737
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 21:38:56 +0800

    更新 getSetWorkerRunSentToClient.m

--- toolbox/shared/sdi/+Simulink/+sdi/+internal/getSetWorkerRunSentToClient.m
+++ toolbox/shared/sdi/+Simulink/+sdi/+internal/getSetWorkerRunSentToClient.m
@@ -1,15 +1,11 @@
 function ret=getSetWorkerRunSentToClient(varargin)
 
-
-
     persistent WAS_SENT
     if isempty(WAS_SENT)
         WAS_SENT=false;
     end
 
-
     ret=WAS_SENT;
-
 
     if~isempty(varargin)
         WAS_SENT=varargin{1};
