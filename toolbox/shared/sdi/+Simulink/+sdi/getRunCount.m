29dfa0f14539aca8ca3394e461d7c1e83ae2cc77
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 21:07:22 +0800

    更新 getRunCount.m

--- toolbox/shared/sdi/+Simulink/+sdi/getRunCount.m
+++ toolbox/shared/sdi/+Simulink/+sdi/getRunCount.m
@@ -1,17 +1,10 @@
 function runCount=getRunCount(varargin)
 
-
-
-
-
-
-
     args={'SDIRuns'};
     if nargin>0
         [varargin{:}]=convertStringsToChars(varargin{:});
         args=varargin;
     end
-
     Simulink.sdi.internal.flushStreamingBackend();
     runCount=int32(Simulink.sdi.Instance.engine.getRunCount(args{:}));
 end
