def0bd29576d03c6a38dcfd3cc4c1848a3b7c554
王海东 <wanghaidong@hutb.edu.cn>
2024-01-20 21:39:17 +0800

    更新 handleRunAppMoved.m

--- toolbox/shared/sdi/+Simulink/+sdi/+internal/handleRunAppMoved.m
+++ toolbox/shared/sdi/+Simulink/+sdi/+internal/handleRunAppMoved.m
@@ -1,20 +1,16 @@
 function handleRunAppMoved(runID,curApp)
 
-
     eng=Simulink.sdi.Instance.engine;
     [uniqueDbIDs,runIDs,signalsIDInfo]=eng.sigRepository.getUniqueRunAndSignalIDs(runID);
     signalIDsToClear=eng.sigRepository.getCheckedSignalsFromDbIDs(uniqueDbIDs);
-
 
     if strcmpi(curApp,'SDIComparison')
         eng.DiffRunResult=Simulink.sdi.DiffRunResult(0,eng);
     end
 
-
     if~isempty(signalIDsToClear)
         Simulink.sdi.clearSignalsFromCanvas(signalIDsToClear);
     end
-
 
     notify(eng,...
     'runsAndSignalsDeleteEvent',...
