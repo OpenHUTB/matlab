ab194190f85598aac6c1cf8669579a6bd2b93878
王海东 <wanghaidong@hutb.edu.cn>
2024-01-21 23:17:38 +0800

    更新 parseOperationPrototype.m

--- toolbox/rtw/targets/AUTOSAR/AUTOSAR/+arblk/parseOperationPrototype.m
+++ toolbox/rtw/targets/AUTOSAR/AUTOSAR/+arblk/parseOperationPrototype.m
@@ -1,36 +1,11 @@
 function[fcn,success,err_id,err_msg]=parseOperationPrototype(fcn_str,maxShortNameLength)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     success=true;
     err_id='';
     err_msg='';
     fcn.name='UNDEFINED';
 
-
-
-
     output=regexp(fcn_str,'^(?<name>\w+)\s*\((?<arg_str>[\s\w,\[\]]*)\)\s*$|^(?<name>\w+)\s*$','names');
-
-
 
     if nargin<2
         maxShortNameLength=255;
@@ -45,8 +20,6 @@ function[fcn,success,err_id,err_msg]=parseOperationPrototype(fcn_str,maxShortNam
 
     fcn.name=output.name;
     arg_str=output.arg_str;
-
-
     [isvalid,msg]=autosarcore.checkIdentifier(fcn.name,'shortName',maxShortNameLength);
     if~isvalid
         success=false;
@@ -56,10 +29,6 @@ function[fcn,success,err_id,err_msg]=parseOperationPrototype(fcn_str,maxShortNam
     end
 
     if~isempty(arg_str)
-
-
-
-
         arg_str(end+1)=',';
         arg_str=regexp(arg_str,'\s*(?<arg_str>[^,]*),','names');
 
@@ -74,8 +43,6 @@ function[fcn,success,err_id,err_msg]=parseOperationPrototype(fcn_str,maxShortNam
             end
 
             fcn.args(ii)=argInfo;
-
-
             [isvalid,msg]=autosarcore.checkIdentifier(fcn.args(ii).name,'shortName',maxShortNameLength);
             if~isvalid
                 success=false;
@@ -84,14 +51,12 @@ function[fcn,success,err_id,err_msg]=parseOperationPrototype(fcn_str,maxShortNam
                 return;
             end
 
-
             if strcmp(fcn.args(ii).dir,'INOUT')
                 success=false;
                 err_id='RTW:autosar:parseOperationPrototypeINOUTNotSupported';
                 err_msg=DAStudio.message(err_id);
                 return;
             end
-
 
             o=regexp(fcn.args(ii).dims,'\[(?<width>\d+)\]','names');
             if isempty(o)
@@ -104,9 +69,7 @@ function[fcn,success,err_id,err_msg]=parseOperationPrototype(fcn_str,maxShortNam
             end
         end
 
-
         uNames=unique({fcn.args.name});
-
 
         if numel(uNames)~=numel({fcn.args.name})
             success=false;
@@ -114,8 +77,6 @@ function[fcn,success,err_id,err_msg]=parseOperationPrototype(fcn_str,maxShortNam
             err_msg=DAStudio.message(err_id);
             return;
         end
-
-
         dupNames=intersect(uNames,unique({fcn.args.dt}));
         if~isempty(dupNames)
             success=false;
