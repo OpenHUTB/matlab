7c295e9e4a0c77827e93f7c0e671591ad5599e64
王海东 <wanghaidong@hutb.edu.cn>
2024-01-21 23:24:09 +0800

    更新 MessageReporter.m

--- toolbox/rtw/targets/AUTOSAR/AUTOSAR/+autosar/+mm/+util/MessageReporter.m
+++ toolbox/rtw/targets/AUTOSAR/AUTOSAR/+autosar/+mm/+util/MessageReporter.m
@@ -1,23 +1,15 @@
-
-
-
-
 classdef MessageReporter<handle
-
-
-
     properties(Hidden=true,GetAccess=public,SetAccess=private)
         useNagCtrlr;
     end
 
-    properties(Hidden=true,Access=public)
 
+    properties(Hidden=true,Access=public)
         DisplayInfoMsgs=false;
     end
 
+
     methods(Access=public)
-
-
 
         function self=MessageReporter(nag)
             if nargin<1
@@ -27,22 +19,16 @@ classdef MessageReporter<handle
         end
 
 
-
         function useNagController(self,nag)
             self.useNagCtrlr=nag;
         end
-
 
 
         function clear(~,~)
         end
 
 
-
         function ret=flush(self,msgs,overrideHeadMsgId)
-
-
-
             exception=MSLException('RTW:fcnClass:finish',...
             DAStudio.message('RTW:fcnClass:finish','Multiple causes'));
 
@@ -73,8 +59,6 @@ classdef MessageReporter<handle
                     end
                 end
 
-
-
                 optTitle='AUTOSAR Target Diagnostic Viewer';
                 dv=find_dv();
                 dv.setTitle(optTitle);
@@ -93,8 +77,6 @@ classdef MessageReporter<handle
                         elseif isa(msg.details,'MException')
                             exception=exception.addCause(msg.details);
                         else
-
-
                             exception=exception.addCause(MException(msgId,strrep(msg.details,'\','\\')));
                         end
                     case 'Warning'
@@ -113,7 +95,6 @@ classdef MessageReporter<handle
                 case 0
 
                 case 1
-
                     exception.cause{1}.throw();
                 otherwise
                     exception.throw();
@@ -122,10 +103,8 @@ classdef MessageReporter<handle
         end
     end
 
+
     methods(Access=private)
-
-
-
 
         function msg=tweakMessage(~,msg)
             strs=regexp(msg,' ','split');
@@ -137,8 +116,8 @@ classdef MessageReporter<handle
         end
     end
 
-    methods(Static)
 
+    methods(Static)
 
         function show(msgs,nag)
             if nargin<2
@@ -154,29 +133,20 @@ classdef MessageReporter<handle
         end
     end
 
+
     methods(Static,Access=public)
-
-
-
-
 
         function hyperlinkFile(filename,line)
             matlab.desktop.editor.openAndGoToLine(filename,line);
         end
 
-        function hyperlinkUri(~)
 
+        function hyperlinkUri(~)
         end
+
 
         function select(~)
-
         end
-
-
-
-
-
-
 
 
         function createWarning(msgID,varargin)
@@ -185,27 +155,13 @@ classdef MessageReporter<handle
         end
 
 
-
-
-
-
-
-
         function print(msg)
-
-
             matlab.internal.display.printWrapped(msg);
             drawnow;
         end
 
 
-
-
-
         function throwException(exceptionObj)
-
-
-
             assert(isa(exceptionObj,'MException'),'Expected an exception object');
             if~autosar.utils.Debug.showStackTrace()
                 throwAsCaller(exceptionObj);
@@ -215,12 +171,10 @@ classdef MessageReporter<handle
         end
 
 
-
         function cleanupObj=suppressWarningTrace()
 
             cleanupObj=[];
             if~autosar.utils.Debug.showStackTrace()
-
                 backtrace_status=warning('query','backtrace');
                 if strcmp(backtrace_status.state,'on')
                     cleanupObj=onCleanup(@()warning(backtrace_status));
