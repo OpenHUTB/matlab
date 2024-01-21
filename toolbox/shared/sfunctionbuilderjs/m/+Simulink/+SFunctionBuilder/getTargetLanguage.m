1444adc3365a7c06ebbabae38feb094ae3a472ca
王海东 <wanghaidong@hutb.edu.cn>
2024-01-21 11:18:48 +0800

    更新 getTargetLanguage.m

--- toolbox/shared/sfunctionbuilderjs/m/+Simulink/+SFunctionBuilder/getTargetLanguage.m
+++ toolbox/shared/sfunctionbuilderjs/m/+Simulink/+SFunctionBuilder/getTargetLanguage.m
@@ -1,8 +1,4 @@
 function language=getTargetLanguage(blockHandle)
-
-
-
-
     appdata=Simulink.SFunctionBuilder.internal.getApplicationData(blockHandle);
     language=appdata.SfunWizardData.LangExt;
 
