58057d385cadebada19b67956fe6517921f47072
王海东 <wanghaidong@hutb.edu.cn>
2024-01-21 23:22:06 +0800

    更新 DictionaryLinkUtils.m

--- toolbox/rtw/targets/AUTOSAR/AUTOSAR/+autosar/+dictionary/+internal/DictionaryLinkUtils.m
+++ toolbox/rtw/targets/AUTOSAR/AUTOSAR/+autosar/+dictionary/+internal/DictionaryLinkUtils.m
@@ -1,14 +1,7 @@
 classdef DictionaryLinkUtils<handle
 
-
-
-
-
-
     methods(Static)
         function[isLinkedToDict,dictFiles]=isModelLinkedToAUTOSARInterfaceDictionary(modelName)
-
-
             [isLinkedToDict,dictFiles]=Simulink.interface.dictionary.internal.DictionaryClosureUtils.isModelLinkedToInterfaceDict(...
             modelName,WithPlatformMapping='AUTOSARClassic');
         end
