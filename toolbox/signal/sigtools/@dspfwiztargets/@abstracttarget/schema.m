2491002921dff8c6e83432d22b1b3685215e0a67
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:39:27 +0800

    更新 schema.m

--- toolbox/signal/sigtools/@dspfwiztargets/@abstracttarget/schema.m
+++ toolbox/signal/sigtools/@dspfwiztargets/@abstracttarget/schema.m
@@ -1,22 +1,14 @@
 function schema
 
-
-
-
     pk=findpackage('dspfwiztargets');
     c=schema.class(pk,'abstracttarget');
     c.Description='Abstract';
 
-
-
     definetypes;
-
-
     p=schema.prop(c,'Destination','ustring');
     p.Factoryvalue='Current';
     schema.prop(c,'Blockname','ustring');
     schema.prop(c,'OverwriteBlock','on/off');
-
     p=schema.prop(c,'IsInputProcessingSpecified','bool');
     p.FactoryValue=false;
     p=schema.prop(c,'IsRateOptionSpecified','bool');
@@ -25,14 +17,12 @@ function schema
     schema.prop(c,'InputProcessing','FDDlgInputProcessing');
     schema.prop(c,'RateOption','FDDlgRateOption');
 
-
     p=schema.prop(c,'system','ustring');
     p.AccessFlags.PublicSet='off';
     p.Visible='off';
 
+
     function definetypes
-
-
 
         if isempty(findtype('FDDlgInputProcessing'))
             schema.EnumType('FDDlgInputProcessing',{'columnsaschannels',...
