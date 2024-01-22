0bbaa5bee47567643ac7ed1a07657a79d1af4bf3
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:38:47 +0800

    更新 fdhdlcInstallCheck.m

--- toolbox/signal/sigtools/fdhdlcInstallCheck.m
+++ toolbox/signal/sigtools/fdhdlcInstallCheck.m
@@ -1,16 +1,11 @@
 function fdhdlcInstallCheck
 
-
-
-
-
     persistent fdhdlinstalled;
 
     if isempty(fdhdlinstalled)
 
         privgenhdl_available=(exist('privgeneratehdl','file')==6)...
         ||(exist('privgeneratehdl','file')==2);
-
         fdhdlinstalled=builtin('license','test','Filter_Design_HDL_Coder')...
         &&builtin('license','checkout','Filter_Design_HDL_Coder')...
         &&~isempty(ver('hdlfilter'))...
