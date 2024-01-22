a876750d94e8dd6c1e3c3dd749cc108d6a70138f
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:37:28 +0800

    更新 testFeature.m

--- toolbox/si/si/+si/+utilities/testFeature.m
+++ toolbox/si/si/+si/+utilities/testFeature.m
@@ -1,17 +1,8 @@
 function ok=testFeature(feature)
 
-
-
-
-
-
-
     ok=builtin('license','test',feature);
 
     ok=logical(ok);
-
-
-
     if(ok&&strcmpi('distrib_computing_toolbox',feature))
         ok=matlab.internal.parallel.isPCTInstalled;
     end
