91d9516769e8b9a22fd0eebee924fe9d262657ef
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:39:50 +0800

    更新 schema.m

--- toolbox/signal/sigtools/@dspfwiztargets/@blocktarget/schema.m
+++ toolbox/signal/sigtools/@dspfwiztargets/@blocktarget/schema.m
@@ -1,11 +1,6 @@
 function schema
 
-
-
-
     pk=findpackage('dspfwiztargets');
-
-
     c=schema.class(pk,'blocktarget',pk.findclass('abstracttarget'));
     c.Description='Abstract';
 
@@ -15,14 +10,10 @@ function schema
 
     schema.prop(c,'MapCoeffsToPorts','on/off');
     schema.prop(c,'CoeffNames','mxArray');
-
-
-
     p=schema.prop(c,'BlockHandle','mxArray');
     p.Visible='off';
 
     schema.prop(c,'CoeffNames','mxArray');
-
     p=schema.prop(c,'privCoefficients','mxArray');
     p.AccessFlags.PublicSet='off';
     p.Visible='off';
