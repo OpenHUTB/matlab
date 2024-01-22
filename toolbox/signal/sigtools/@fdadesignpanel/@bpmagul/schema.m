5d259181c6f295c70a988ef30285122ba1e4ef4d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:44:04 +0800

    更新 schema.m

--- toolbox/signal/sigtools/@fdadesignpanel/@bpmagul/schema.m
+++ toolbox/signal/sigtools/@fdadesignpanel/@bpmagul/schema.m
@@ -1,15 +1,9 @@
 function schema
 
-
-
-
-
     pk=findpackage('fdadesignpanel');
     c=schema.class(pk,'bpmagul',pk.findclass('abstractmagul'));
-
     p=schema.prop(c,'Dstop1Upper','ustring');
     set(p,'FactoryValue','.1','Description','spec');
-
     p=schema.prop(c,'Dstop1Lower','ustring');
     set(p,'FactoryValue','.1','Description','spec');
 
@@ -18,10 +12,8 @@ function schema
 
     p=schema.prop(c,'DpassLower','ustring');
     set(p,'FactoryValue','.1','Description','spec');
-
     p=schema.prop(c,'Dstop2Upper','ustring');
     set(p,'FactoryValue','.1','Description','spec');
-
     p=schema.prop(c,'Dstop2Lower','ustring');
     set(p,'FactoryValue','.1','Description','spec');
 
