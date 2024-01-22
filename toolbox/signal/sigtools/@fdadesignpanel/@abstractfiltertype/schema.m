1d4e17efb131670d86a4a2abb78906522abafef7
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:40:41 +0800

    更新 schema.m

--- toolbox/signal/sigtools/@fdadesignpanel/@abstractfiltertype/schema.m
+++ toolbox/signal/sigtools/@fdadesignpanel/@abstractfiltertype/schema.m
@@ -1,13 +1,7 @@
 function schema
 
-
-
-
-
     pk=findpackage('fdadesignpanel');
     spk=findpackage('siggui');
-
-
     c=schema.class(pk,'abstractfiltertype',findclass(spk,'sigcontainer'));
     c.Description='abstract';
 
