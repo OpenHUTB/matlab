6ac27ee5833d0e0e9b4e5740adc8941181ee6008
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:45:11 +0800

    更新 schema.m

--- toolbox/signal/sigtools/@fdadesignpanel/@freqcutoff1/schema.m
+++ toolbox/signal/sigtools/@fdadesignpanel/@freqcutoff1/schema.m
@@ -1,14 +1,7 @@
 function schema
 
-
-
-
-
     pk=findpackage('fdadesignpanel');
-
-
     c=schema.class(pk,'freqcutoff1',findclass(pk,'abstractfiltertypewfs'));
-
 
     p=schema.prop(c,'Fc','ustring');
     p.FactoryValue='10800';
