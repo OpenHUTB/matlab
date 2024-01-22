6a3c61fa1a0055a9354a3ed8bf99be43eecc4f6c
王海东 <wanghaidong@hutb.edu.cn>
2024-01-22 18:41:45 +0800

    更新 schema.m

--- toolbox/signal/sigtools/@fdadesignpanel/@abstractmagc/schema.m
+++ toolbox/signal/sigtools/@fdadesignpanel/@abstractmagc/schema.m
@@ -1,14 +1,9 @@
 function schema
 
-
-
-
-
     pk=findpackage('fdadesignpanel');
 
     c=schema.class(pk,'abstractmagc',findclass(pk,'abstractmagframe'));
     c.Description='abstract';
-
     p=schema.prop(c,'ConstrainedBands','double_vector');
     set(p,'FactoryValue',[],'Description','spec');
 
