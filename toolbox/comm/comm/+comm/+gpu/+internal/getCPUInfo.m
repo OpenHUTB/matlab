abf9dbe512bd6264a711e98fe028eba00c4f185e
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 22:27:56 +0800

    更新 getCPUInfo.m

--- toolbox/comm/comm/+comm/+gpu/+internal/getCPUInfo.m
+++ toolbox/comm/comm/+comm/+gpu/+internal/getCPUInfo.m
@@ -1,20 +1,5 @@
 function info=getCPUInfo()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     if isunix
         if ismac
             info=cpuInfoMac();
@@ -24,7 +9,6 @@ function info=getCPUInfo()
     else
         info=cpuInfoWindows();
     end
-
 
 
     function info=cpuInfoWindows()
@@ -57,22 +41,16 @@ function info=getCPUInfo()
                     fields{ff}=strtrim(fields{ff}(1:idx-1));
                 end
             end
-
-
-
             numResults=sum(strcmpi(fields,fields{1}));
             if numResults>1
-
                 numCoresEntries=find(strcmpi(fields,'NumberOfCores'));
                 if~isempty(numCoresEntries)
                     cores=cellfun(@str2double,values(numCoresEntries));
                     values(numCoresEntries)={num2str(sum(cores))};
                 end
-
                 [fields,idx]=unique(fields,'first');
                 values=values(idx);
             end
-
 
             info=cell2struct(values,fields);
 
@@ -116,7 +94,6 @@ function info=getCPUInfo()
 
 
                     function vernum=getOSXVersion()
-
                         ver=evalc('system(''sw_vers'')');
                         vernum=regexp(ver,'ProductVersion:\s([1234567890.]*)','tokens','once');
                         vernum=strtrim(vernum{1});
@@ -125,11 +102,8 @@ function info=getCPUInfo()
                         function info=cpuInfoUnix()
                             txt=readCPUInfo();
                             cpuinfo=parseCPUInfoText(txt);
-
                             txt=readOSInfo();
                             osinfo=parseOSInfoText(txt);
-
-
                             info=cell2struct([struct2cell(cpuinfo);struct2cell(osinfo)],...
                             [fieldnames(cpuinfo);fieldnames(osinfo)]);
 
@@ -161,18 +135,13 @@ function info=getCPUInfo()
                                         continue;
                                     end
 
-
                                     idx=find(strcmpi(lookup(:,1),fieldName));
                                     if~isempty(idx)
                                         newName=lookup{idx,2};
                                         info.(newName)=fieldValue;
                                     end
                                 end
-
-
                                 info.Clock=[info.Clock,' MHz'];
-
-
                                 info.NumProcessors=str2double(info.NumProcessors);
 
 
@@ -192,19 +161,14 @@ function info=getCPUInfo()
                                             error('cpuinfo:BadPROCCPUInfo','Could not open /proc/cpuinfo for reading');
                                         end
                                         onCleanup(@()fclose(fid));
-
                                         txt=textscan(fid,'%s','Delimiter','\n');
                                         txt=txt{1};
 
-
-                                        function txt=readOSInfo()
-
-                                            fid=fopen('/proc/version','rt');
+                                        function txt=readOSInfo()                                            fid=fopen('/proc/version','rt');
                                             if fid<0
                                                 error('cpuinfo:BadProcVersion','Could not open /proc/version for reading');
                                             end
                                             onCleanup(@()fclose(fid));
-
                                             txt=textscan(fid,'%s','Delimiter','\n');
                                             txt=txt{1};
 
