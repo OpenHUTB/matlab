b1c34a476076a02ff949e597cee3da9a9a8ac8bc
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 22:24:04 +0800

    更新 GPUBase.m

--- toolbox/comm/comm/+comm/+gpu/+internal/GPUBase.m
+++ toolbox/comm/comm/+comm/+gpu/+internal/GPUBase.m
@@ -1,9 +1,5 @@
 classdef GPUBase<matlab.System
 
-
-
-
-
     properties(Access=private)
 pInputIsGPUArray
 pCPUOutputMode
@@ -14,33 +10,38 @@ pGPUArrayClassCache
 pGPUArrayComplexityCache
     end
 
+
     properties(GetAccess=protected,Constant)
         kGPUArrayString='gpuArray';
         kMaxGridSize=65535;
     end
 
+
     methods
         function obj=GPUBase(varargin)
-
             if~(builtin('license','checkout','Distrib_Computing_Toolbox'))||...
                 isempty(ver('parallel'))
                 error(message('comm:system:gpuBase:noPCT'));
             end
-
-
             if~parallel.gpu.GPUDevice.isAvailable()
                 error(message('comm:system:gpuBase:badGPU'));
             end
         end
     end
+
+
     methods(Access=protected)
 
         function flag=isInputSizeMutableImpl(~,~)
             flag=false;
         end
+
+
         function flag=isInputComplexityMutableImpl(~,~)
             flag=false;
         end
+
+
         function detectGPUInputs(obj,varargin)
             nIn=numel(varargin);
             obj.pInputIsGPUArray=false(1,nIn);
@@ -61,6 +62,8 @@ pGPUArrayComplexityCache
             obj.pCPUOutputMode=~any(obj.pInputIsGPUArray);
             obj.pGPUInputVectorSet=true;
         end
+
+
         function varargout=moveInputsToGPU(obj,varargin)
             if~obj.pGPUInputVectorSet
                 error(message('comm:system:gpuBase:incorrectSetup'));
@@ -72,29 +75,19 @@ pGPUArrayComplexityCache
 
                         theSize=size(varargin{ix});
                         oldSize=obj.pGPUArraySizeCache{ix};
-
-
-
                         if~isInputSizeMutableImpl(obj,ix)
                             if(numel(theSize)~=numel(oldSize))||...
                                 (~all(theSize==obj.pGPUArraySizeCache{ix}))
                                 error(message('comm:system:gpuBase:inputSizeChanged',num2str(ix)));
                             end
                         end
-
-
                         if~strcmp(underlyingType(varargin{ix}),...
                             obj.pGPUArrayClassCache{ix})
                             error(message('comm:system:gpuBase:inputDataTypeChanged',num2str(ix)));
                         end
-
-
                         if~isInputComplexityMutableImpl(obj,ix)
                             if obj.pGPUArrayComplexityCache{ix}&&...
                                 ~isreal(varargin{ix})
-
-
-
                                 error(message('comm:system:gpuBase:inputComplexityChanged',num2str(ix)));
                             end
                         end
@@ -104,23 +97,30 @@ pGPUArrayComplexityCache
                 end
             end
         end
+
+
         function varargout=moveOutputsToCPU(obj,varargin)
 
             for ix=1:obj.getNumOutputs()
                 varargout{ix}=gather(varargin{ix});
             end
         end
+
+
         function flag=isInputGPUArray(obj,idx)
             if~obj.pGPUInputVectorSet
                 error(message('comm:system:gpuBase:incorrectSetup'));
             end
             flag=obj.pInputIsGPUArray(idx);
         end
+
+
         function flag=isOutputCPUArray(obj)
             flag=obj.pCPUOutputMode;
         end
-        function varargout=moveOutputsIfNeeded(obj,varargin)
 
+
+        function varargout=moveOutputsIfNeeded(obj,varargin)
 
             if obj.pCPUOutputMode
 
@@ -134,6 +134,8 @@ pGPUArrayComplexityCache
                 end
             end
         end
+
+
         function s=saveObjectImpl(obj)
             s=saveObjectImpl@matlab.System(obj);
             s.SaveLockedData=false;
@@ -152,19 +154,20 @@ pGPUArrayComplexityCache
         end
     end
 
+
     methods(Static,Hidden)
 
         function tf=isfloat(x)
             tf=isfloat(x)&&~isa(x,'embedded.fi');
         end
 
+
         function tf=islogical(x)
             tf=islogical(x);
         end
 
+
         function tf=isRealBuiltinFloat(x)
-
-
             tf=(comm.gpu.internal.GPUBase.isfloat(x)&&...
             isreal(x));
         end
