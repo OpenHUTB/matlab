361dffe8cd8cdd2072fdab8903c99a1ea46c31b2
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 21:46:56 +0800

    更新 MPSKModulatorAutoscaler.m

--- toolbox/comm/commutilities/+commautoscaler/@MPSKModulatorAutoscaler/MPSKModulatorAutoscaler.m
+++ toolbox/comm/commutilities/+commautoscaler/@MPSKModulatorAutoscaler/MPSKModulatorAutoscaler.m
@@ -1,12 +1,5 @@
 classdef MPSKModulatorAutoscaler<commautoscaler.ModulatorAutoscaler
 
-
-
-
-
-
-
-
     methods(Hidden)
         [outputPortIndex,outputValueMax,outputValueMin]=getModelRequiredMinMaxOutputValues(h,blkObj)
     end
