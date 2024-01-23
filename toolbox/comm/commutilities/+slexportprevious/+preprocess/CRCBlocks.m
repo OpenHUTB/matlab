4cc11ba88b632b70e824fa0c978c33f729045409
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 22:17:10 +0800

    更新 CRCBlocks.m

--- toolbox/comm/commutilities/+slexportprevious/+preprocess/CRCBlocks.m
+++ toolbox/comm/commutilities/+slexportprevious/+preprocess/CRCBlocks.m
@@ -1,22 +1,11 @@
 function CRCBlocks(obj)
 
-
-
-
-
-
     if isR2015aOrEarlier(obj.ver)
-
 
         crcBlocks=findCRCblocks(obj);
 
-
         for i=1:length(crcBlocks)
             blk=crcBlocks{i};
-
-
-
-
             hConvertStringPolysToNum(blk,'genPoly','descending');
         end
     end
@@ -24,12 +13,7 @@ function CRCBlocks(obj)
 end
 
 
-
-
 function crcBlocks=findCRCblocks(obj)
-
-
-
     crcBlocks=find_system(obj.modelName,'LookUnderMasks','on',...
     'IncludeCommented','on','regexp','on',...
     'MatchFilter',@Simulink.match.internal.filterOutInactiveVariantSubsystemChoices,...
