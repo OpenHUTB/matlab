9e4d4b552547a809ca0869dbaba704697c71d95c
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 21:49:27 +0800

    更新 RectQAMModulatorAutoscaler.m

--- toolbox/comm/commutilities/+commautoscaler/@RectQAMModulatorAutoscaler/RectQAMModulatorAutoscaler.m
+++ toolbox/comm/commutilities/+commautoscaler/@RectQAMModulatorAutoscaler/RectQAMModulatorAutoscaler.m
@@ -1,14 +1,5 @@
 classdef RectQAMModulatorAutoscaler<dvautoscaler.SPCUniDTAutoscaler
 
-
-
-
-
-
-
-
-
-
     methods(Hidden)
         [outputPortIndex,outputValueMax,outputValueMin]=getModelRequiredMinMaxOutputValues(h,blkObj)
     end
