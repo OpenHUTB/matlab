ddff46bb4f991c743b802232af5d66175c76b284
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 21:47:27 +0800

    更新 ModulatorAutoscaler.m

--- toolbox/comm/commutilities/+commautoscaler/@ModulatorAutoscaler/ModulatorAutoscaler.m
+++ toolbox/comm/commutilities/+commautoscaler/@ModulatorAutoscaler/ModulatorAutoscaler.m
@@ -1,14 +1,5 @@
 classdef ModulatorAutoscaler<dvautoscaler.SPCBlocksetAutoscaler
 
-
-
-
-
-
-
-
-
-
     methods
         [DTConInfo,comments,paramNames]=gatherSpecifiedDT(h,blkObj,pathItem)
         [signValStr,wlValueStr,flValueStr,specifiedDTStr,flDlgStr,modeDlgStr,wlDlgStr]=getDataTypeInfoForPathItem(h,blkObj,pathItem)
