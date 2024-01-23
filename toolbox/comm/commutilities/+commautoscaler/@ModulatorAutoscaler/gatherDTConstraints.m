f0f174ea9cb7f8a68829e1e2cf0a707e26c713a4
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 21:47:47 +0800

    更新 gatherDTConstraints.m

--- toolbox/comm/commutilities/+commautoscaler/@ModulatorAutoscaler/gatherDTConstraints.m
+++ toolbox/comm/commutilities/+commautoscaler/@ModulatorAutoscaler/gatherDTConstraints.m
@@ -1,23 +1,11 @@
 function[hasDTConstraints,DTConstraintsSet]=gatherDTConstraints(h,blkObj)
 
-
-
-
     hasDTConstraints=true;
-
-
-
     curInputPorts=SimulinkFixedPoint.AutoscalerUtils.getSignalDrivingPort(h,blkObj,-1,[]);
-
-
     curOutputPorts=SimulinkFixedPoint.AutoscalerUtils.getSignalDrivingPort(h,blkObj,[],-1);
-
     DTConstraintsSet=cell(numel(curInputPorts)+numel(curOutputPorts),1);
 
-
     for idx=1:numel(curInputPorts)
-
-
         uniqueId=SimulinkFixedPoint.AutoscalerUtils.getUniqueId(curInputPorts{idx}.blkObj,curInputPorts{idx}.pathItem);
         constraint=SimulinkFixedPoint.AutoscalerConstraints.SpecificConstraint([],[8,16,32],0);
         constraint.setSourceInfo(blkObj,[DAStudio.message('SimulinkFixedPoint:autoscaling:ConstraintFromInport'),' 1']);
