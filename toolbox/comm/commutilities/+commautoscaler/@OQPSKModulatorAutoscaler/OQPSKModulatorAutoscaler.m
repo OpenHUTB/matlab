b8a49511c0b05dbcc85b523372e9b04e2e766c8a
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 21:48:38 +0800

    更新 OQPSKModulatorAutoscaler.m

--- toolbox/comm/commutilities/+commautoscaler/@OQPSKModulatorAutoscaler/OQPSKModulatorAutoscaler.m
+++ toolbox/comm/commutilities/+commautoscaler/@OQPSKModulatorAutoscaler/OQPSKModulatorAutoscaler.m
@@ -1,12 +1,5 @@
 classdef OQPSKModulatorAutoscaler<commautoscaler.ModulatorAutoscaler
 
-
-
-
-
-
-
-
     methods(Hidden)
         [outputPortIndex,outputValueMax,outputValueMin]=getModelRequiredMinMaxOutputValues(h,blkObj)
     end
