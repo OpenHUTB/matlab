b35edf0ed7794192f5d80ee7557b139df9c21802
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 22:23:02 +0800

    更新 CUDAKernelSystemBase.m

--- toolbox/comm/comm/+comm/+gpu/+internal/CUDAKernelSystemBase.m
+++ toolbox/comm/comm/+comm/+gpu/+internal/CUDAKernelSystemBase.m
@@ -1,11 +1,5 @@
 classdef CUDAKernelSystemBase<comm.gpu.internal.GPUBase
 
-
-
-
-
-
-
     properties(Constant,GetAccess=protected)
         MaxThreadBlockSize=256;
 
@@ -22,18 +16,14 @@ ptxext
         function this=CUDAKernelSystemBase(varargin)
             this@comm.gpu.internal.GPUBase(varargin);
 
-
             this.ptxext=parallel.gpu.ptxext;
 
         end
     end
 
+
     methods(Access=protected)
         function ker=makeKernel(this,proto,func,dt,kernelFile)
-
-
-
-
             kerFile=makePTXFilename(this,kernelFile);
             kerName=comm.gpu.internal.makePTXFunction(func,dt);
             ker=parallel.gpu.CUDAKernel(kerFile,proto,kerName);
@@ -44,11 +34,7 @@ ptxext
 
     methods(Access=protected)
         function fname=makePTXFilename(this,filebasename)
-
-
-
             ptxfile=[filebasename,'.',this.ptxext];
-
             fname=fullfile(toolboxdir('comm'),'gpu','commptx',dct_arch,ptxfile);
         end
     end
