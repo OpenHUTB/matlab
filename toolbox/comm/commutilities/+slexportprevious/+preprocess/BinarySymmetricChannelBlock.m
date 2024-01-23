a19ce4dd418a8d6f439d7a628338156e0027b6cc
王海东 <wanghaidong@hutb.edu.cn>
2024-01-23 22:14:46 +0800

    更新 BinarySymmetricChannelBlock.m

--- toolbox/comm/commutilities/+slexportprevious/+preprocess/BinarySymmetricChannelBlock.m
+++ toolbox/comm/commutilities/+slexportprevious/+preprocess/BinarySymmetricChannelBlock.m
@@ -1,20 +1,10 @@
 function BinarySymmetricChannelBlock(obj)
 
-
-
-
-
-
-
     if isR2018aOrEarlier(obj.ver)
-
-
         bscBlocks=obj.findBlocksWithMaskType('Binary Symmetric Channel');
-
 
         for i=1:length(bscBlocks)
             blk=bscBlocks{i};
-
 
             if strcmp(get_param(blk,'OutputDataTypeSL'),'single')
                 set_param(blk,'OutputDataTypeSL','double')
