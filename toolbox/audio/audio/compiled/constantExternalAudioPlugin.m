c59025abcf4f8df7e262dcbb3b16f3cbdbfaf457
王海东 <wanghaidong@hutb.edu.cn>
2024-01-24 21:50:52 +0800

    更新 constantExternalAudioPlugin.m

--- toolbox/audio/audio/compiled/constantExternalAudioPlugin.m
+++ toolbox/audio/audio/compiled/constantExternalAudioPlugin.m
@@ -1,6 +1,5 @@
 classdef constantExternalAudioPlugin<audioPlugin
 
-
 %#codegen
 
     properties(Hidden)
@@ -10,28 +9,34 @@ PluginInstance
         MaxSamplesPerFrame=65536
     end
 
+
     methods
         function obj=constantExternalAudioPlugin
             coder.allowpcode('plain');
         end
 
+
         function out=process(plugin,in)
             out=externalAudioPluginSimulink.process(plugin,in);
         end
+
 
         function setSampleRate(plugin,rate)
             setSampleRate@audioPlugin(plugin,rate);
             externalAudioPluginSimulink.setSampleRate(plugin);
         end
 
+
         function setMaxSamplesPerFrame(plugin,rate)
             plugin.MaxSamplesPerFrame=double(rate);
             externalAudioPluginSimulink.setMaxSamplesPerFrame(plugin);
         end
 
+
         function rate=getMaxSamplesPerFrame(plugin)
             rate=plugin.MaxSamplesPerFrame;
         end
+
 
         function s=info(plugin)
             s=externalAudioPluginSimulink.getInfo(plugin);
