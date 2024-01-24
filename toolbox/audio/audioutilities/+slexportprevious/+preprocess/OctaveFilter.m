d72146d74dd9dfe09c5492dade442ecc981b1b1f
王海东 <wanghaidong@hutb.edu.cn>
2024-01-24 21:23:30 +0800

    更新 OctaveFilter.m

--- toolbox/audio/audioutilities/+slexportprevious/+preprocess/OctaveFilter.m
+++ toolbox/audio/audioutilities/+slexportprevious/+preprocess/OctaveFilter.m
@@ -1,24 +1,14 @@
 function OctaveFilter(obj)
 
-
-
-
     verobj=obj.ver;
 
     blocks={};
     if isR2016bOrEarlier(verobj)
-
-
-
-
         blocks=obj.findBlocksWithMaskType('audio.simulink.OctaveFilter',...
         'BandwidthType','2/3 octave');
     end
 
     if isR2017aOrEarlier(verobj)
-
-
-
 
         blocks=[blocks;...
         obj.findBlocksWithMaskType('audio.simulink.OctaveFilter',...
@@ -28,7 +18,6 @@ function OctaveFilter(obj)
     for blkIdx=1:numel(blocks)
         blk=blocks{blkIdx};
         obj.replaceWithEmptySubsystem(blk,getString(message('audio:octave:BlockTitle')));
-
         msgStr=DAStudio.message('audio:octave:NewFeaturesNotAvailable');
         set_param(blk,'InitFcn',sprintf('error(''%s'')',msgStr));
     end
