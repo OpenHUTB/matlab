280f4d38d882d828095a54209bbd36e5100947bc
王海东 <wanghaidong@hutb.edu.cn>
2024-01-24 21:21:10 +0800

    更新 AudioDeviceReader.m

--- toolbox/audio/audioutilities/+slexportprevious/+preprocess/AudioDeviceReader.m
+++ toolbox/audio/audioutilities/+slexportprevious/+preprocess/AudioDeviceReader.m
@@ -1,19 +1,12 @@
 function AudioDeviceReader(obj)
 
-
-
-
     if isR2016bOrEarlier(obj.ver)
-
-
-
 
         blocks=obj.findBlocksWithMaskType('Audio Device Reader',...
         'Driver','WASAPI');
 
         for blkIdx=1:numel(blocks)
             blk=blocks{blkIdx};
-
             obj.replaceWithEmptySubsystem(blk,...
             getString(message('dsp:audioDeviceIO:WASAPI_ADR')));
 
