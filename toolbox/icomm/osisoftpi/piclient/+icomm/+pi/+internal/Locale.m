1515ca9059007dba87787b2a482f822ce9e21d15
王海东 <wanghaidong@hutb.edu.cn>
2024-01-25 18:38:59 +0800

    更新 Locale.m

--- toolbox/icomm/osisoftpi/piclient/+icomm/+pi/+internal/Locale.m
+++ toolbox/icomm/osisoftpi/piclient/+icomm/+pi/+internal/Locale.m
@@ -1,10 +1,9 @@
 classdef Locale
 
-
-
     properties(Constant)
         DateFormat(1,1)string='dd-MMMM-yyyy'
         DatetimeFormat(1,1)string='dd-MMMM-yyyy HH:mm:ss'
         TimeFormat(1,1)string='HH:mm:ss'
     end
+
 end
