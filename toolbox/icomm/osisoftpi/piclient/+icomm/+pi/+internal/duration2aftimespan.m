b216c5c3f1d963246d31a99f99b8faa3960f3a91
王海东 <wanghaidong@hutb.edu.cn>
2024-01-25 18:41:15 +0800

    更新 duration2aftimespan.m

--- toolbox/icomm/osisoftpi/piclient/+icomm/+pi/+internal/duration2aftimespan.m
+++ toolbox/icomm/osisoftpi/piclient/+icomm/+pi/+internal/duration2aftimespan.m
@@ -1,7 +1,4 @@
 function span=duration2aftimespan(interval)
-
-
-
     if isa(interval,'duration')
         span=OSIsoft.AF.Time.AFTimeSpan(0,0,0,0,0,0,milliseconds(interval));
     elseif isa(interval,'calendarDuration')
