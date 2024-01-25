b9a9b15718b4813e59597f1532f456cf23b7268f
王海东 <wanghaidong@hutb.edu.cn>
2024-01-25 13:57:36 +0800

    更新 Chart.m

--- toolbox/icomm/osisoftpi/piclient/+icomm/+pi/+app/+charts/Chart.m
+++ toolbox/icomm/osisoftpi/piclient/+icomm/+pi/+app/+charts/Chart.m
@@ -1,10 +1,5 @@
 classdef(Abstract)Chart<icomm.pi.app.Container
 
-
-
-
-
-
     properties(GetAccess=public,SetAccess=public,Dependent)
         Data timetable
         Overlay(1,1)logical
@@ -23,20 +18,24 @@ classdef(Abstract)Chart<icomm.pi.app.Container
         AxesLayout icomm.pi.app.AxesLayout
     end
 
+
     methods
 
         function value=get.Data(this)
             value=this.Data_;
         end
 
+
         function set.Data(this,value)
             this.Data_=value;
             this.update();
         end
 
+
         function value=get.Overlay(this)
             value=this.Overlay_;
         end
+
 
         function set.Overlay(this,value)
             if~isequal(value,this.Overlay_)
@@ -45,15 +44,18 @@ classdef(Abstract)Chart<icomm.pi.app.Container
             end
         end
 
+
         function value=get.NumAxes(this)
             value=this.AxesLayout.NumAxes;
         end
+
 
         function set.NumAxes(this,value)
             this.AxesLayout.NumAxes=value;
         end
 
     end
+
 
     methods(Access=public)
 
@@ -66,15 +68,14 @@ classdef(Abstract)Chart<icomm.pi.app.Container
             inputs=parser.Results;
             thisLayout=icomm.pi.app.(inputs.GraphicsType).AxesLayout(...
             'Parent',[]);
-
             propIndex=find(cellfun(@(x)isequal(convertCharsToStrings(x),"GraphicsType"),varargin),1,'first');
             if~isempty(propIndex)
                 varargin([propIndex,propIndex+1])=[];
             end
-
             this@icomm.pi.app.Container(thisLayout.UiContainer,varargin{:});
             this.AxesLayout=thisLayout;
         end
+
 
         function delete(this)
             delete(this.UiContainer);
@@ -82,11 +83,11 @@ classdef(Abstract)Chart<icomm.pi.app.Container
 
     end
 
+
     methods(Abstract,Access=protected)
-
         update(this)
-
     end
+
 
     methods(Access=protected)
 
