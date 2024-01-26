dc943d4283250aa4aa9dd069cab8b9c51f57dc9d
王海东 <wanghaidong@hutb.edu.cn>
2024-01-26 19:36:00 +0800

    更新 ifpConicIndex.m

--- toolbox/shared/controllib/engine/+ltipack/ifpConicIndex.m
+++ toolbox/shared/controllib/engine/+ltipack/ifpConicIndex.m
@@ -1,40 +1,9 @@
 function[tMax,fMax,heigs]=ifpConicIndex(a,b,c,d,e,Ts,tol,tStop,fTest,fBand,RealFlag)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     Ts=abs(Ts);
     nx=size(a,1);
     DescFlag=~isempty(e);
     heigs=zeros(0,1);
-
 
     ni=nargin;
     if ni<8
@@ -57,12 +26,9 @@ function[tMax,fMax,heigs]=ifpConicIndex(a,b,c,d,e,Ts,tol,tStop,fTest,fBand,RealF
     tMax=Inf;
     fMax=fBand(1);
 
-
     if nx==0
         tMax=min(eig(d+d'))/2;return
     end
-
-
 
     aa=a;bb=b;cc=c;ee=e;
     if norm(tril(aa,-2),1)+norm(tril(ee,-2),1)>0
@@ -79,14 +45,11 @@ function[tMax,fMax,heigs]=ifpConicIndex(a,b,c,d,e,Ts,tol,tStop,fTest,fBand,RealF
         end
     end
 
-
     if RealFlag
         fTest=[fBand(:);fTest(fTest>fBand(1)&fTest<fBand(2))];
     else
         fTest=[fBand(:);-fBand(:);fTest(abs(fTest)>fBand(1)&abs(fTest)<fBand(2))];
     end
-
-
     [h,fTest]=evalFreqResp(aa,bb,cc,d,ee,Ts,fTest,fBand);
     for ct=1:numel(fTest)
 
@@ -99,41 +62,28 @@ function[tMax,fMax,heigs]=ifpConicIndex(a,b,c,d,e,Ts,tol,tStop,fTest,fBand,RealF
         end
     end
 
-
-
     if tMax==Inf
         return
     end
 
-
-
     tau=norm(d,1)+norm(b,1)^2/(1+norm(a,1));
-
 
     OFFSET=0.001;
     if isempty(e)
         e=eye(nx);
     end
     for iter=1:30
-
         tMax0=tMax;
         dt=tol*tau*(OFFSET+abs(tMax/tau));
         tH=tMax-dt;
-
-
         [heigs,w_acc]=localHamTest(a,b,c,d,e,Ts,tH);
         if RealFlag
-
             heigs=heigs(imag(heigs)>=0);
         end
-
-
         ws=pickTestFreqs(heigs,w_acc,fBand,fMax);
         if isempty(ws)
             break
         end
-
-
         [h,ws]=evalFreqResp(aa,bb,cc,d,ee,Ts,ws,fBand);
         for ct=1:numel(ws)
             hw=h(:,:,ct);
@@ -145,13 +95,10 @@ function[tMax,fMax,heigs]=ifpConicIndex(a,b,c,d,e,Ts,tol,tStop,fTest,fBand,RealF
             end
         end
 
-
-
         if tMax>tMax0-dt/10
             break
         end
     end
-
 
 
     function[heigs,w_acc]=localHamTest(a,b,c,d,e,Ts,t)
@@ -159,8 +106,6 @@ function[tMax,fMax,heigs]=ifpConicIndex(a,b,c,d,e,Ts,tol,tStop,fTest,fBand,RealF
         nx=size(a,1);nu=size(d,1);
         R=d+d'-(2*t)*eye(nu);
         if Ts==0
-
-
             [ta,td]=ltipack.scalePencil(norm(a,1),norm(b,1),norm(R,1));
             [alpha,beta,w_acc]=ltipack.eigSHH(ta^2*a,ta^2*e,zeros(nx),zeros(nx),...
             td^2*R,(ta*td)*b,(ta*td)*c');
