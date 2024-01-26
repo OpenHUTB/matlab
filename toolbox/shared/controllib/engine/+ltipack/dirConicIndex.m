2af41ea46a24b911a3a837c299c1f3912ea84284
王海东 <wanghaidong@hutb.edu.cn>
2024-01-26 18:51:08 +0800

    更新 dirConicIndex.m

--- toolbox/shared/controllib/engine/+ltipack/dirConicIndex.m
+++ toolbox/shared/controllib/engine/+ltipack/dirConicIndex.m
@@ -1,46 +1,12 @@
 function[tMax,fMax,heigs]=...
     dirConicIndex(a,b,c,d,e,Ts,M,W,tol,tStop,fTest,fBand,RealFlag)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     Ts=abs(Ts);
     nx=size(a,1);
     DescFlag=~isempty(e);
     heigs=zeros(0,1);
     zeroTol=1e3*eps;
     N=W*W';
-
 
     ni=nargin;
     if ni<10
@@ -64,14 +30,9 @@ function[tMax,fMax,heigs]=...
     tMax=Inf;
     fMax=fBand(1);
 
-
-
-
     if nx==0||norm(b,1)==0||(norm(W'*c,1)==0&&norm([c,d]'*M*c,1)==0)
         tMax=ltipack.getTmax(M,W*W',d);return
     end
-
-
 
     aa=a;bb=b;cc=c;ee=e;
     if norm(tril(aa,-2),1)+norm(tril(ee,-2),1)>0
@@ -81,21 +42,17 @@ function[tMax,fMax,heigs]=...
             bb=q*bb;
             cc=cc*z;
         else
-
             [u,aa]=hess(aa);
             bb=u'*bb;
             cc=cc*u;
         end
     end
 
-
     if RealFlag
         fTest=[fBand(:);fTest(fTest>fBand(1)&fTest<fBand(2))];
     else
         fTest=[fBand(:);-fBand(:);fTest(abs(fTest)>fBand(1)&abs(fTest)<fBand(2))];
     end
-
-
     [h,fTest,hmag,hscale]=evalFreqResp(aa,bb,cc,d,ee,Ts,fTest,fBand);
     for ct=1:numel(fTest)
 
@@ -113,24 +70,15 @@ function[tMax,fMax,heigs]=...
         end
     end
 
-
-
     if tMax==Inf
         return
     end
 
-
     OFFSET=0.001;
     for iter=1:30
-
-
-
         tMax0=tMax;
         dt=tol*(OFFSET+abs(tMax));
         tH=tMax-dt;
-
-
-
 
         if tH>0
             sgn=+1;r=sqrt(tH);
@@ -142,14 +90,10 @@ function[tMax,fMax,heigs]=...
 
             heigs=heigs(imag(heigs)>=0);
         end
-
-
         ws=pickTestFreqs(heigs,w_acc,fBand,fMax);
         if isempty(ws)
             break
         end
-
-
         [h,ws,hmag,hscale]=evalFreqResp(aa,bb,cc,d,ee,Ts,ws,fBand);
         for ct=1:numel(ws)
             if hmag(ct)<zeroTol*hscale(ct)
@@ -165,8 +109,6 @@ function[tMax,fMax,heigs]=...
                 tMax=tw;fMax=ws(ct);
             end
         end
-
-
 
         if tMax>tMax0-dt/10
             break
