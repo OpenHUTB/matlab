17a0ed91dad58a70c749a4c54cd8a9cc547e6b66
王海东 <wanghaidong@hutb.edu.cn>
2024-01-26 19:35:02 +0800

    更新 hasMatchingType.m

--- toolbox/shared/controllib/engine/+ltipack/hasMatchingType.m
+++ toolbox/shared/controllib/engine/+ltipack/hasMatchingType.m
@@ -1,12 +1,5 @@
 function boo=hasMatchingType(op,varargin)
-
-
-
-
-
-
     InputClasses=cellfun(@class,varargin,'UniformOutput',false);
-
 
     boo=isequal(InputClasses{:})&&...
     strcmp(InputClasses{1},feval([InputClasses{1},'.toClosed'],op));
