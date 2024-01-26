c1fe07da3bdea7041feab847cfece11af799d698
王海东 <wanghaidong@hutb.edu.cn>
2024-01-26 14:13:55 +0800

    更新 FddDLChannel.m

--- toolbox/lte/umts/private/FddDLChannel.m
+++ toolbox/lte/umts/private/FddDLChannel.m
@@ -1,18 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function out=FddDLChannel(data,modulation,sf,sprdcodes,scrmcode,varargin)
 
     if isempty(data)
@@ -26,14 +11,11 @@ function out=FddDLChannel(data,modulation,sf,sprdcodes,scrmcode,varargin)
     end
 
     md=FddDLModulation(data,modulation)/sqrt(2);
-
     sd=FddSpreading(md,sf*ones(1,length(sprdcodes),class(sf)),sprdcodes,1);
     if~isvector(sd)
         sd=sum(sd,2);
     end
-
     out=FddScrambling(sd,1,scrmcode,scramoffset)/sqrt(2);
-
 
     out=circshift(out(:),scramoffset);
 
