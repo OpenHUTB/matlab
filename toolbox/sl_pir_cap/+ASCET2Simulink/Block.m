f8627a0859932fcab4964a9be2ac4058c8e036cf
王海东 <wanghaidong@hutb.edu.cn>
2024-01-27 20:34:53 +0800

    更新 Block.m

--- toolbox/sl_pir_cap/+ASCET2Simulink/Block.m
+++ toolbox/sl_pir_cap/+ASCET2Simulink/Block.m
@@ -1,24 +1,24 @@
-
-
-
 classdef Block<handle
-
-
 
     properties
         path=''
     end
+
+
     methods
         function this=Block(path)
             this.path=path;
         end
 
+
         function blockPath=getPath(this)
             blockPath=this.path;
         end
 
+
         function priority=getSequenceNumber(this)
             priority=ASCET2Simulink.BlockSequenceNumber(this);
         end
+
     end
 end
