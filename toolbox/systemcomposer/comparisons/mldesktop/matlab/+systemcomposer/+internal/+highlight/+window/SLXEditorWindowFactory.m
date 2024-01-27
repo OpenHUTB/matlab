44c9d5993cbe8aa3af619451727327e43da10ce7
王海东 <wanghaidong@hutb.edu.cn>
2024-01-27 20:49:04 +0800

    更新 SLXEditorWindowFactory.m

--- toolbox/systemcomposer/comparisons/mldesktop/matlab/+systemcomposer/+internal/+highlight/+window/SLXEditorWindowFactory.m
+++ toolbox/systemcomposer/comparisons/mldesktop/matlab/+systemcomposer/+internal/+highlight/+window/SLXEditorWindowFactory.m
@@ -1,21 +1,21 @@
 classdef SLXEditorWindowFactory<comparisons.internal.highlight.WindowFactory
 
-
-
-
     properties(Access=private)
 ContentId
     end
+
 
     methods(Access=public)
         function obj=SLXEditorWindowFactory(contentId)
             obj.ContentId=contentId;
         end
 
+
         function canHandle=canDisplay(~,location)
             import systemcomposer.internal.highlight.window.isSupportedBySLXEditor
             canHandle=isSupportedBySLXEditor(location.Type);
         end
+
 
         function window=create(obj,location)
             window=systemcomposer.internal.highlight.window.SLXEditorHighlightWindow(...
