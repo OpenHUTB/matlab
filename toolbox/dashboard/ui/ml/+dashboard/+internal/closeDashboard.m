a437e03d24a9e4b9ef46914819d1eae9e17a24a9
王海东 <wanghaidong@hutb.edu.cn>
2024-01-28 13:30:59 +0800

    更新 closeDashboard.m

--- toolbox/dashboard/ui/ml/+dashboard/+internal/closeDashboard.m
+++ toolbox/dashboard/ui/ml/+dashboard/+internal/closeDashboard.m
@@ -1,10 +1,8 @@
 function closeDashboard()
 
-    uiService=dashboard.UiService.get();
+    uiService = dashboard.UiService.get();
     if~isempty(uiService.Windows)
         for i=1:numel(uiService.Windows)
-
-
             uiService.Windows(1).close;
         end
     end
