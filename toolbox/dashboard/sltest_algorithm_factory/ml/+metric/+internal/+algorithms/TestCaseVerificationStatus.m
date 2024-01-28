1cebb1f714ba32164262c43022e933e6cd78f75b
王海东 <wanghaidong@hutb.edu.cn>
2024-01-28 12:53:15 +0800

    更新 TestCaseVerificationStatus.m

--- toolbox/dashboard/sltest_algorithm_factory/ml/+metric/+internal/+algorithms/TestCaseVerificationStatus.m
+++ toolbox/dashboard/sltest_algorithm_factory/ml/+metric/+internal/+algorithms/TestCaseVerificationStatus.m
@@ -1,8 +1,5 @@
 classdef TestCaseVerificationStatus<metric.SimpleMetric
 
-
-
-
     methods
 
         function obj=TestCaseVerificationStatus()
@@ -11,15 +8,10 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
             obj.Version=1;
         end
 
+
         function result=algorithm(this,resultFactory,artifacts)
-
-
-
-
             import metric.internal.algorithms.TestCaseVerificationStatusEnum;
             import sltest.testmanager.TestResultOutcomes;
-
-
 
             if length(artifacts)<=1
                 val=TestCaseVerificationStatusEnum.NO_RESULT;
@@ -29,18 +21,12 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
             end
 
             resultArtifact=artifacts(2);
-
             testObj=sltest.testmanager.TestResult.getResultFromUUID(resultArtifact.Address);
-
-
             if~isempty(testObj)&&isequal(testObj.Outcome,'Disabled')
                 val=TestCaseVerificationStatusEnum.NO_RESULT;
 
             elseif~isempty(testObj)
                 val=TestCaseVerificationStatusEnum.UNVERIFIED;
-
-
-
 
                 testObj=testObj(1);
 
@@ -59,10 +45,10 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
         end
     end
 
+
     methods(Access=private)
         function val=analyzeSimulationTest(this,testObj)
             import metric.internal.algorithms.TestCaseVerificationStatusEnum;
-
             val=this.analyzeAssessments(testObj);
 
             if val==TestCaseVerificationStatusEnum.UNVERIFIED
@@ -74,13 +60,10 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
             end
         end
 
+
         function val=analyzeEquivalenceTest(this,testObj)
             import metric.internal.algorithms.TestCaseVerificationStatusEnum;
             val=TestCaseVerificationStatusEnum.UNVERIFIED;
-
-
-
-
             itrObj=testObj.getIterationResults;
             if~isempty(itrObj)
                 itrsHaveBaseline=true;
@@ -95,7 +78,6 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
                 val=TestCaseVerificationStatusEnum.VERIFIED;
             end
 
-
             if val==TestCaseVerificationStatusEnum.UNVERIFIED
                 val=this.analyzeAssessments(testObj);
 
@@ -109,17 +91,13 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
             end
         end
 
+
         function val=analyzeAssessments(~,testObj)
             import metric.internal.algorithms.TestCaseVerificationStatusEnum;
 
             val=TestCaseVerificationStatusEnum.UNVERIFIED;
-
-
-
             if~isempty(getAssessmentResults(testObj))
                 results=getAssessmentResults(testObj);
-
-
 
                 atleastOneIsTested=false;
                 for i=1:numel(results)
@@ -136,11 +114,7 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
                     return
                 end
             end
-
-
-
             iterations=testObj.getIterationResults;
-
 
             allItrsHaveAssessment=true;
             containsAssessment=false;
@@ -152,7 +126,6 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
                     containsAssessment=true;
                     assessments=res{1};
                     for i=1:numel(assessments)
-
                         if~strcmpi(assessments(i).Outcome,'Untested')&&~strcmpi(assessments(i).Outcome,'Disabled')
                             thisItrHasAssessment=true;
                         end
@@ -166,13 +139,10 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
             end
         end
 
+
         function val=analyzeVerifyStatements(~,testObj)
             import metric.internal.algorithms.TestCaseVerificationStatusEnum;
             val=TestCaseVerificationStatusEnum.UNVERIFIED;
-
-
-
-
             s=stm.internal.getTestCaseResultDetail(testObj.getID);
             if~isempty(s.verifyResults)
                 s.verifyResults.verifyRunOutcome=sltest.internal.Helper.cppOutcomeToMatlabOutcome(s.verifyResults.verifyRunOutcome);
@@ -180,8 +150,6 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
                     val=TestCaseVerificationStatusEnum.VERIFIED;
                 end
             end
-
-
             iterations=testObj.getIterationResults;
             allItrsVerified=true;
             hasVerifyStatements=false;
@@ -190,7 +158,6 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
                 if~isempty(s.verifyResults)
                     hasVerifyStatements=true;
                     s.verifyResults.verifyRunOutcome=sltest.internal.Helper.cppOutcomeToMatlabOutcome(s.verifyResults.verifyRunOutcome);
-
 
                     if strcmpi(s.verifyResults.verifyRunOutcome,'Untested')||strcmpi(s.verifyResults.verifyRunOutcome,'Disabled')
                         allItrsVerified=false;
@@ -203,17 +170,15 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
             end
         end
 
+
         function val=analyzeCustomCriteria(~,testObj)
             import metric.internal.algorithms.TestCaseVerificationStatusEnum;
             val=TestCaseVerificationStatusEnum.UNVERIFIED;
-
             if~isempty(testObj.getCustomCriteriaResult)&&~strcmpi(testObj.getCustomCriteriaResult.Outcome,'Untested')...
                 &&~strcmpi(testObj.getCustomCriteriaResult.Outcome,'Disabled')
                 val=TestCaseVerificationStatusEnum.VERIFIED;
                 return
             end
-
-
 
             allItrsHaveCustomCriteria=true;
             containsCustomCriteria=false;
@@ -223,7 +188,6 @@ classdef TestCaseVerificationStatus<metric.SimpleMetric
                 res=getCustomCriteriaResult(iterations(idx));
                 thisItrHasCustomCriteria=false;
                 if~isempty(res)
-
                     containsCustomCriteria=true;
                     if~strcmpi(res.Outcome,'Untested')&&~strcmpi(res.Outcome,'Disabled')
                         thisItrHasCustomCriteria=true;
